a3986b7074b845174788dbd7e0a0ab4db14bdb71
David A. Mellis <d.mellis@arduino.cc>
2005-10-04 09:12:20 +0000

    Removed build.bat; use the Makefile instead.

--- bootloader/build.bat
+++
@@ -1,6 +0,0 @@
-avr-gcc -g -Wall -O3 -mmcu=atmega8 -DCRUMB8 -c -o ATmegaBOOT.o ATmegaBOOT.c
-avr-gcc -g -Wall -O3 -mmcu=atmega8 -DCRUMB8 -Wl,-Map,ATmegaBOOT.map,--section-start=.text=0x1800 -o ATmegaBOOT.elf ATmegaBOOT.o 
-avr-objdump -S -h ATmegaBOOT.elf > ATmegaBOOT.lst
-avr-objcopy -j .text -j .data -O ihex ATmegaBOOT.elf ATmegaBOOT.hex
-avr-objcopy -j .text -j .data -O binary ATmegaBOOT.elf ATmegaBOOT.bin
-avr-objcopy -j .text -j .data -O srec ATmegaBOOT.elf ATmegaBOOT.srec
