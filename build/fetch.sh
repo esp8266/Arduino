5ede9c8d2e6b82d624680bd139cd910aff650286
David A. Mellis <d.mellis@arduino.cc>
2006-01-11 11:18:10 +0000

    Added a really hacky script to grab the documentation from the website.

---
+++ build/fetch.sh
@@ -0,0 +1,6 @@
+curl http://arduino.berlios.de/index.php/Reference/HomePage -o index.html
+curl http://arduino.berlios.de/pub/skins/arduino/arduino.css -o arduino.css
+for i in `grep -o "http://arduino.berlios.de/index.php/Reference/[^']*" index.html | sort -u | grep -v '?' | cut -d '/' -f 6`; do curl http://arduino.berlios.de/index.php/Reference/$i -o $i.html; done
+perl -i -pe "s|http://arduino.berlios.de/index.php/Reference/[^?\"']*\?[^'\"]*|#|g" *.html
+perl -i -pe "s|http://arduino.berlios.de/index.php/Reference/([^']*)|\1.html|g" *.html
+perl -i -pe "s|http://arduino.berlios.de/pub/skins/arduino/arduino.css|arduino.css|g" *.html
