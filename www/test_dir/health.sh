5915029090f40bc7fbd1d2969d1e116c2655f811
cameronrich <cameronrich@9a5d90b5-6617-0410-8a86-bb477d3ed2e3>
2006-07-12 08:57:34 +0000

    health.sh tweak
    git-svn-id: svn://svn.code.sf.net/p/axtls/code/trunk@21 9a5d90b5-6617-0410-8a86-bb477d3ed2e3

--- www/test_dir/health.sh
+++ www/test_dir/health.sh
@@ -3,19 +3,13 @@
 echo "Content-type: text/html"
 echo
 
-echo "<HTML>"
-echo "<TITLE>System Health</TITLE><BODY>"
+echo "<HTML><TITLE>System Health</TITLE><BODY>"
 
 echo "<H1>System Health for '`hostname`'</H1>"
-echo "<H2>Processes</H2>"
-echo "<TABLE BORDEr=\"2\">"
+echo "<H2>Processes</H2><TABLE BORDER=\"2\">"
 ps -ef | sed -e "s/\(.*\)/<tr><td>\1<\/td><\/tr>/"
-echo "</TABLE>"
 
-echo "<H2>Free FileSystem Space</H2>"
-echo "<TABLE border=\"2\">"
+echo "</TABLE><H2>Free FileSystem Space</H2>"
+echo "<TABLE BORDER=\"2\">"
 df -h . | sed -e "s/\(.*\)/<tr><td>\1<\/td><\/tr>/"
-echo "</TABLE>"
-
-echo "</BODY></HTML>"
-
+echo "</TABLE></BODY></HTML>"
