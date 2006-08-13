cb676dfa2a1d6bca985418ba255d36285a639cf6
David A. Mellis <d.mellis@arduino.cc>
2006-08-13 09:59:06 +0000

    Adding Wiring-compatible random(min) and random(min, max) functions (except operating on longs instead of floats).

--- targets/arduino/WProgram.h
+++ targets/arduino/WProgram.h
@@ -5,5 +5,12 @@
 #include <avr/signal.h>
 
 #include "wiring.h"
+
+#ifdef __cplusplus
 #include "HardwareSerial.h"
 
+// random prototypes
+long random(long);
+long random(long, long);
+void randomSeed(unsigned int);
+#endif
