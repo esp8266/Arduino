4659e60810c3252e641a7da49ea4846bc84f9dd1
David A. Mellis <d.mellis@arduino.cc>
2007-07-18 22:11:27 +0000

    One bootloader for the Diecimila and NG - just different makefile targets to pass in slightly different defines.

--- bootloader168-diecimila/ATmegaBOOT_168.c
+++ bootloader168-diecimila/ATmegaBOOT_168.c
@@ -78,7 +78,8 @@
 /* 20060803: hacked by DojoCorp */
 /* 20070626: hacked by David A. Mellis to decrease waiting time for auto-reset */
 /* set the waiting time for the bootloader */
-#define MAX_TIME_COUNT (F_CPU>>4)
+/* get this from the Makefile instead */
+/* #define MAX_TIME_COUNT (F_CPU>>4) */
 
 /* 20070707: hacked by David A. Mellis - after this many errors give up and launch application */
 #define MAX_ERROR_COUNT 5
@@ -357,9 +358,9 @@ int main(void)
     /* flash onboard LED to signal entering of bootloader */
 #ifdef __AVR_ATmega128__
     // 4x for UART0, 5x for UART1
-    flash_led(3 + bootuart);
+    flash_led(NUM_LED_FLASHES + bootuart);
 #else
-    flash_led(1);
+    flash_led(NUM_LED_FLASHES);
 #endif
     
     /* 20050803: by DojoCorp, this is one of the parts provoking the
