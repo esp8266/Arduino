e5b24cc48bb73c22fa10ffbe5bffe0ed73f07cb7
David A. Mellis <d.mellis@arduino.cc>
2010-05-22 20:25:43 +0000

    Removing old shell scripts.

--- build/windows/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-CLASSPATH=lib\\pde.jar\;lib\\core.jar\;lib\\jna.jar\;lib\\ecj.jar\;lib\\antlr.jar\;lib\\oro.jar\;lib\\RXTXcomm.jar\;java\\lib\\tools.jar
-export CLASSPATH
-
-cd work && ./java/bin/java processing.app.Base
