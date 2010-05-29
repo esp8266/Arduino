0979ed050b875a7f31a5690d5f6269a60c432c25
David A. Mellis <d.mellis@arduino.cc>
2010-05-29 17:01:43 +0000

    Updating version number to 0019. Some revisions.

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -41,9 +41,9 @@ import processing.core.*;
  * files and images, etc) that comes from that.
  */
 public class Base {
-  public static final int REVISION = 18;
+  public static final int REVISION = 19;
   /** This might be replaced by main() if there's a lib/version.txt file. */
-  static String VERSION_NAME = "0018";
+  static String VERSION_NAME = "0019";
   /** Set true if this a proper release rather than a numbered revision. */
   static public boolean RELEASE = false;
 
