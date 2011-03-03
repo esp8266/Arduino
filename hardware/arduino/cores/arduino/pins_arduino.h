5234a96b34c070528f6b7d98d5381076468e6391
David A. Mellis <d.mellis@arduino.cc>
2011-03-03 18:57:05 -0500

    Rearranging internal #defines in headers.

--- hardware/arduino/cores/arduino/pins_arduino.h
+++ hardware/arduino/cores/arduino/pins_arduino.h
@@ -27,32 +27,6 @@
 
 #include <avr/pgmspace.h>
 
-#define NOT_A_PIN 0
-#define NOT_A_PORT 0
-
-#define NOT_ON_TIMER 0
-#define TIMER0A 1
-#define TIMER0B 2
-#define TIMER1A 3
-#define TIMER1B 4
-#define TIMER2  5
-#define TIMER2A 6
-#define TIMER2B 7
-
-#define TIMER3A 8
-#define TIMER3B 9
-#define TIMER3C 10
-#define TIMER4A 11
-#define TIMER4B 12
-#define TIMER4C 13
-#define TIMER5A 14
-#define TIMER5B 15
-#define TIMER5C 16
-
-#ifndef INLINED
-#define INLINED static __attribute__((always_inline)) inline
-#endif
-
 #if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
 const static uint8_t SS   = 53;
 const static uint8_t MOSI = 51;
