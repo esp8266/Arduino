facbd279b67923bbabbc4540721280f652ae5726
amcewen <amcewen@bcs.org.uk>
2011-04-02 11:33:27 +0100

    Added a brief explanation of how you'd use Printable

--- hardware/arduino/cores/arduino/Printable.h
+++ hardware/arduino/cores/arduino/Printable.h
@@ -22,6 +22,12 @@
 
 class Print;
 
+/** The Printable class provides a way for new classes to allow themselves to be printed.
+    By deriving from Printable and implementing the printTo method, it will then be possible
+    for users to print out instances of this class by passing them into the usual
+    Print::print and Print::println methods.
+*/
+
 class Printable
 {
   public:
