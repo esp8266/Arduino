6554ae653c8aa8a0722877befff7e0acdf4f306b
David A. Mellis <d.mellis@arduino.cc>
2011-09-30 11:24:44 -0400

    Using alternate timer 0 overflow interrupt signal name for ATtiny24/44/84.

--- hardware/arduino/cores/arduino/wiring.c
+++ hardware/arduino/cores/arduino/wiring.c
@@ -41,7 +41,11 @@ volatile unsigned long timer0_overflow_count = 0;
 volatile unsigned long timer0_millis = 0;
 static unsigned char timer0_fract = 0;
 
+#if defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+SIGNAL(TIM0_OVF_vect)
+#else
 SIGNAL(TIMER0_OVF_vect)
+#endif
 {
 	// copy these to local variables so they can be stored in registers
 	// (volatile variables must be read from memory on every access)
