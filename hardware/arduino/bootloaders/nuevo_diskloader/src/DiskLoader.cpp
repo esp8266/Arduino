2f46c2a6e6bcaf6964a9c7b8bca8ba233ecaab72
Zach Eveland <zeveland@blacklabel-development.com>
2011-10-21 16:51:25 -0400

    can now enable interrupts but EORSTE or SOFE in USB_::attach() still cause bootloader to hang

--- hardware/arduino/bootloaders/nuevo_diskloader/src/DiskLoader.cpp
+++ hardware/arduino/bootloaders/nuevo_diskloader/src/DiskLoader.cpp
@@ -3,36 +3,34 @@
  */
 
 #include "Platform.h"
-//#include "CDC.cpp"
-#include "USBAPI.h"
 
 //extern "C"
-//void entrypoint(void) __attribute__ ((naked)) __attribute__ ((section (".vectors")));
-//void entrypoint(void)
-//{
-//	asm volatile (
-////				  "eor	r1,		r1\n"	// Zero register
-////				  "out	0x3F,	r1\n"	// SREG
-////				  "ldi	r28,	0xFF\n"
-////				  "ldi	r29,	0x0A\n"
-////				  "out	0x3E,	r29\n"	// SPH
-////				  "out	0x3D,	r28\n"	// SPL
-//				  "rjmp	main"			// Stack is all set up, start the main code
-//				  ::);
-//}
+void entrypoint(void) __attribute__ ((naked)) __attribute__ ((section (".vectors")));
+void entrypoint(void)
+{
+	asm volatile (
+//				  "eor	r1,		r1\n"	// Zero register
+//				  "out	0x3F,	r1\n"	// SREG
+//				  "ldi	r28,	0xFF\n"
+//				  "ldi	r29,	0x0A\n"
+//				  "out	0x3E,	r29\n"	// SPH
+//				  "out	0x3D,	r28\n"	// SPL
+				  "rjmp	main"			// Stack is all set up, start the main code
+				  ::);
+}
 
-//ISR(BADISR_vect)
-//{
-//	L_LED_ON();
-//}
+ISR(BADISR_vect)
+{
+	L_LED_ON();
+}
 
 int main(void) __attribute__ ((naked));
 int main() 
 {
-//	/* Enable change of interrupt vectors */ 
-//	MCUCR = (1<<IVCE); 
-//	/* Move interrupts to boot flash section */ 
-//	MCUCR = (1<<IVSEL); 
+	/* Enable change of interrupt vectors */ 
+	MCUCR = (1<<IVCE); 
+	/* Move interrupts to boot flash section */ 
+	MCUCR = (1<<IVSEL); 
 	
 	wdt_disable();
 	BOARD_INIT();
@@ -40,7 +38,7 @@ int main()
 	RX_LED_OFF();
 	L_LED_OFF();	
 	USB.attach();
-//	sei();
+	sei();
 	
 	for (;;) 
 	{
