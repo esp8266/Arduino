f2d5ea87ad6d1ab7deb04c6405c4858f2af9b03f
Zach Eveland <zeveland@blacklabel-development.com>
2012-02-06 15:23:41 -0500

    increased Leonardo bootloader timeout to 16 seconds

--- hardware/arduino/bootloaders/caterina_LUFA/Caterina.c
+++ hardware/arduino/bootloaders/caterina_LUFA/Caterina.c
@@ -121,7 +121,7 @@ int main(void)
 		CDC_Task();
 		USB_USBTask();
 		/* Time out and start the sketch if one is present */
-		if (Timeout > 8000 && pgm_read_word(0) != 0xFFFF) 
+		if (Timeout > 16000 && pgm_read_word(0) != 0xFFFF) 
 			RunBootloader = false;
 
 		LEDPulse();
