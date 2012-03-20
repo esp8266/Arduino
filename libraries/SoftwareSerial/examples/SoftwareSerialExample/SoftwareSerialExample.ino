87eaf2d0c50840862244f27678a265ae21a37e71
Tom Igoe <tigoe@tigoemac.itp.tsoa.nyu.edu>
2012-03-20 14:48:24 -0400

    Updated comments in softwareSerial example

--- libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
+++ libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
@@ -1,5 +1,22 @@
+/*
+  Software serial multple serial test
+ 
+ Receives from the hardware serial, sends to software serial.
+ Receives from software serial, sends to hardware serial.
+ 
+ The circuit: 
+ * Software serial  TX attached to digital pin 2, RX to pin 3
+ 
+ created back in the mists of time
+ by Tom Igoe
+ based on Mikal Hart's example
+ 
+ This example code is in the public domain.
+ 
+ */
 #include <SoftwareSerial.h>
 
+// software serial port: TX = digital pin 2, RX = digital pin 3
 SoftwareSerial mySerial(2, 3);
 
 void setup()  
