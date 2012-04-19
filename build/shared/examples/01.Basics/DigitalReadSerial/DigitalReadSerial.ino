59f7f988d7ebd20beda0c29c3cf4273d75651981
Zach Eveland <zeveland@blacklabel-development.com>
2012-04-19 10:39:34 -0400

    added a slight delay to loop in DigitalReadSerial example so we don't overload Serial Monitor

--- build/shared/examples/01.Basics/DigitalReadSerial/DigitalReadSerial.ino
+++ build/shared/examples/01.Basics/DigitalReadSerial/DigitalReadSerial.ino
@@ -22,6 +22,7 @@ void loop() {
   int buttonState = digitalRead(pushButton);
   // print out the state of the button:
   Serial.println(buttonState);
+  delay(1);        // delay in between reads for stability
 }
 
 
