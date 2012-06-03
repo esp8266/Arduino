3062c550b02d49076f36193d6f26ab14be2c4c3c
Cristian Maglie <c.maglie@bug.st>
2012-06-03 22:07:27 +0200

    Updated Wire library. Latest API not yet implemented.

--- hardware/arduino/sam/libraries/Wire/Wire.h
+++ hardware/arduino/sam/libraries/Wire/Wire.h
@@ -29,7 +29,7 @@
 
 #define BUFFER_LENGTH 32
 
-class TwoWire: public Stream {
+class TwoWire : public Stream {
 public:
 	TwoWire(Twi *twi, void(*begin_cb)(void));
 	void begin();
@@ -38,17 +38,25 @@ public:
 	void beginTransmission(uint8_t);
 	void beginTransmission(int);
 	uint8_t endTransmission(void);
+    uint8_t endTransmission(uint8_t);
 	uint8_t requestFrom(uint8_t, uint8_t);
+    uint8_t requestFrom(uint8_t, uint8_t, uint8_t);
 	uint8_t requestFrom(int, int);
-	virtual void write(uint8_t);
-	virtual void write(const char *);
-	virtual void write(const uint8_t *, size_t);
+    uint8_t requestFrom(int, int, int);
+	virtual size_t write(uint8_t);
+	virtual size_t write(const uint8_t *, size_t);
 	virtual int available(void);
 	virtual int read(void);
 	virtual int peek(void);
 	virtual void flush(void);
 	void onReceive(void(*)(int));
 	void onRequest(void(*)(void));
+
+    inline size_t write(unsigned long n) { return write((uint8_t)n); }
+    inline size_t write(long n) { return write((uint8_t)n); }
+    inline size_t write(unsigned int n) { return write((uint8_t)n); }
+    inline size_t write(int n) { return write((uint8_t)n); }
+    using Print::write;
 
 	void onService(void);
 
