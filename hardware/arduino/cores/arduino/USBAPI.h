f833374312bcab83d4531769a253122a9c4d7008
David A. Mellis <d.mellis@arduino.cc>
2012-06-14 15:53:27 +0100

    Adding write(str) and write(buf, size) for USB CDC.
    So that they work on the Leonardo.
    
    http://code.google.com/p/arduino/issues/detail?id=958

--- hardware/arduino/cores/arduino/USBAPI.h
+++ hardware/arduino/cores/arduino/USBAPI.h
@@ -39,6 +39,7 @@ public:
 	virtual int read(void);
 	virtual void flush(void);
 	virtual size_t write(uint8_t);
+	using Print::write; // pull in write(str) and write(buf, size) from Print
 	operator bool();
 };
 extern Serial_ Serial;
