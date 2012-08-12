0b44fb7f734de3b9da9cf27af1d8964904c98d6e
Alarus <alarus@qip.ru>
2012-08-12 20:23:00 +0600

    Update hardware/arduino/cores/arduino/HardwareSerial.h
    Adding advanced begin (); with the ability to specify the length of bits, parity, stop bits.

--- hardware/arduino/cores/arduino/HardwareSerial.h
+++ hardware/arduino/cores/arduino/HardwareSerial.h
@@ -16,6 +16,7 @@
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+  Modified 28 September 2010 by Mark Sproul
   Modified 12 August 2012 by Alarus
 */
 
