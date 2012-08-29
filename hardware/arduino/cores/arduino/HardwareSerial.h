222d51e3831d77f60ae6ce1f3d037b0febb0c717
David A. Mellis <d.mellis@arduino.cc>
2012-08-29 16:32:05 -0400

    Adding overloads so Serial.write(0) works.
    http://code.google.com/p/arduino/issues/detail?id=1006

--- hardware/arduino/cores/arduino/HardwareSerial.h
+++ hardware/arduino/cores/arduino/HardwareSerial.h
@@ -57,6 +57,10 @@ class HardwareSerial : public Stream
     virtual int read(void);
     virtual void flush(void);
     virtual size_t write(uint8_t);
+    inline size_t write(unsigned long n) { return write((uint8_t)n); }
+    inline size_t write(long n) { return write((uint8_t)n); }
+    inline size_t write(unsigned int n) { return write((uint8_t)n); }
+    inline size_t write(int n) { return write((uint8_t)n); }
     using Print::write; // pull in write(str) and write(buf, size) from Print
     operator bool();
 };
