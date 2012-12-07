b32ed2d45045c0e7c3a492f0a18ca06f26d2e8c8
Cristian Maglie <c.maglie@bug.st>
2012-12-07 18:31:48 +0100

    Upped version number to 1.0.3

--- app/src/processing/app/Base.java
+++ app/src/processing/app/Base.java
@@ -42,9 +42,9 @@ import static processing.app.I18n._;
  * files and images, etc) that comes from that.
  */
 public class Base {
-  public static final int REVISION = 102;
+  public static final int REVISION = 103;
   /** This might be replaced by main() if there's a lib/version.txt file. */
-  static String VERSION_NAME = "0102";
+  static String VERSION_NAME = "0103";
   /** Set true if this a proper release rather than a numbered revision. */
   static public boolean RELEASE = false;
 
