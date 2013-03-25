03a7529cc00b7ede1adb1e1dcbb29a555ea31c8a
Cristian Maglie <c.maglie@bug.st>
2013-03-25 01:29:42 +0100

    [sam] Added serialEvent*() support

--- hardware/arduino/sam/variants/arduino_due_x/variant.cpp
+++ hardware/arduino/sam/variants/arduino_due_x/variant.cpp
@@ -301,6 +301,8 @@ extern const PinDescription g_APinDescription[]=
 RingBuffer rx_buffer1;
 
 UARTClass Serial(UART, UART_IRQn, ID_UART, &rx_buffer1);
+void serialEvent() __attribute__((weak));
+void serialEvent() { }
 
 // IT handlers
 void UART_Handler(void)
@@ -317,8 +319,14 @@ RingBuffer rx_buffer3;
 RingBuffer rx_buffer4;
 
 USARTClass Serial1(USART0, USART0_IRQn, ID_USART0, &rx_buffer2);
+void serialEvent1() __attribute__((weak));
+void serialEvent1() { }
 USARTClass Serial2(USART1, USART1_IRQn, ID_USART1, &rx_buffer3);
+void serialEvent2() __attribute__((weak));
+void serialEvent2() { }
 USARTClass Serial3(USART3, USART3_IRQn, ID_USART3, &rx_buffer4);
+void serialEvent3() __attribute__((weak));
+void serialEvent3() { }
 
 // IT handlers
 void USART0_Handler(void)
@@ -334,6 +342,16 @@ void USART1_Handler(void)
 void USART3_Handler(void)
 {
   Serial3.IrqHandler();
+}
+
+// ----------------------------------------------------------------------------
+
+void serialEventRun(void)
+{
+  if (Serial.available()) serialEvent();
+  if (Serial1.available()) serialEvent1();
+  if (Serial2.available()) serialEvent2();
+  if (Serial3.available()) serialEvent3();
 }
 
 // ----------------------------------------------------------------------------
