be9462e41eeb1aa6c45c412141e6939faea88f55
Kristian Sloth Lauszus <lauszus@gmail.com>
2013-04-04 20:55:15 +0200

    Added support for all variants of Sanguino

--- hardware/arduino/cores/arduino/wiring_analog.c
+++ hardware/arduino/cores/arduino/wiring_analog.c
@@ -45,7 +45,7 @@ int analogRead(uint8_t pin)
 	if (pin >= 54) pin -= 54; // allow for channel or pin numbers
 #elif defined(__AVR_ATmega32U4__)
 	if (pin >= 18) pin -= 18; // allow for channel or pin numbers
-#elif defined(__AVR_ATmega1284P__) || defined(__AVR_ATmega644P__)
+#elif defined(__AVR_ATmega1284__) || defined(__AVR_ATmega1284P__) || defined(__AVR_ATmega644__) || defined(__AVR_ATmega644A__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644PA__)
 	if (pin >= 24) pin -= 24; // allow for channel or pin numbers
 #elif defined(analogPinToChannel) && (defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__))
 	pin = analogPinToChannel(pin);
