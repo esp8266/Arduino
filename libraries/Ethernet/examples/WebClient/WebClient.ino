2e6023bc2aadfe418dbdedf9a085e740a83f570f
Fede85 <f.vanzati@gmail.com>
2013-04-11 17:36:15 +0200

    edited some ethernet ad wifi examples

--- libraries/Ethernet/examples/WebClient/WebClient.ino
+++ libraries/Ethernet/examples/WebClient/WebClient.ino
@@ -8,8 +8,9 @@
  * Ethernet shield attached to pins 10, 11, 12, 13
  
  created 18 Dec 2009
- modified 9 Apr 2012
  by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe, based on work by Adrian McEwen
  
  */
 
@@ -18,8 +19,14 @@
 
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress server(173,194,33,104); // Google
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+char server[] = "www.google.com";    // name address for Google (using DNS)
+
+// Set the static IP address to use if the DHCP fails to assign
+IPAddress ip(192,168,0,177);
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server 
@@ -37,8 +44,8 @@ void setup() {
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
     // no point in carrying on, so do nothing forevermore:
-    for(;;)
-      ;
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip);
   }
   // give the Ethernet shield a second to initialize:
   delay(1000);
@@ -48,7 +55,9 @@ void setup() {
   if (client.connect(server, 80)) {
     Serial.println("connected");
     // Make a HTTP request:
-    client.println("GET /search?q=arduino HTTP/1.0");
+    client.println("GET /search?q=arduino HTTP/1.1");
+    client.println("Host: www.google.com");
+    client.println("Connection: close");
     client.println();
   } 
   else {
@@ -73,8 +82,7 @@ void loop()
     client.stop();
 
     // do nothing forevermore:
-    for(;;)
-      ;
+    while(true);
   }
 }
 
