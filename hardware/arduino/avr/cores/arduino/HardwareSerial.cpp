f35ec75dce0d9a2791545dd1d27b2149ea3849e4
Matthijs Kooijman <matthijs@stdin.nl>
2013-04-19 15:29:43 +0200

    Remove duplicate code from HardwareSerial::begin() methods.
    There are two begin methods, one which accepts just a baud rate and
    uses the default bit settings and one which accepts both a baudrate and
    a bit config. Previously, both of these contained a complete
    implementation, but now the former just calls the latter, explicitely
    passing the default 8N1 configuration.
    
    Technically, this causes a small change: Before the UCSRC register was
    untouched when calling begin(baud), now it is explicitely initialized
    with 8N1. However, since this is the default configuration for at least
    the Uno and the Mega (didn't check any others), probably for all avrs,
    this shouldn't effectively change anything. Given that the Arduino
    documentation also documents this as the default when none is passed,
    explicitly setting it is probably a good idea in any case.

--- hardware/arduino/avr/cores/arduino/HardwareSerial.cpp
+++ hardware/arduino/avr/cores/arduino/HardwareSerial.cpp
@@ -283,44 +283,7 @@ HardwareSerial::HardwareSerial(
 
 void HardwareSerial::begin(unsigned long baud)
 {
-  uint16_t baud_setting;
-  bool use_u2x = true;
-
-#if F_CPU == 16000000UL
-  // hardcoded exception for compatibility with the bootloader shipped
-  // with the Duemilanove and previous boards and the firmware on the 8U2
-  // on the Uno and Mega 2560.
-  if (baud == 57600) {
-    use_u2x = false;
-  }
-#endif
-
-try_again:
-  
-  if (use_u2x) {
-    *_ucsra = 1 << _u2x;
-    baud_setting = (F_CPU / 4 / baud - 1) / 2;
-  } else {
-    *_ucsra = 0;
-    baud_setting = (F_CPU / 8 / baud - 1) / 2;
-  }
-  
-  if ((baud_setting > 4095) && use_u2x)
-  {
-    use_u2x = false;
-    goto try_again;
-  }
-
-  // assign the baud_setting, a.k.a. ubbr (USART Baud Rate Register)
-  *_ubrrh = baud_setting >> 8;
-  *_ubrrl = baud_setting;
-
-  transmitting = false;
-
-  sbi(*_ucsrb, _rxen);
-  sbi(*_ucsrb, _txen);
-  sbi(*_ucsrb, _rxcie);
-  cbi(*_ucsrb, _udrie);
+  begin(baud, SERIAL_8N1);
 }
 
 void HardwareSerial::begin(unsigned long baud, byte config)
