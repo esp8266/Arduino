8ba3533a7ad347f7df651bec43ff9008266198f7
Federico Fissore <f.fissore@arduino.cc>
2013-04-22 10:15:39 +0200

    more macosx boards autodetect tests

--- app/test/processing/app/macosx/SystemProfilerParserTest.java
+++ app/test/processing/app/macosx/SystemProfilerParserTest.java
@@ -13,5 +13,11 @@ public class SystemProfilerParserTest {
 
     assertEquals("2341_0044", new SystemProfilerParser().extractVIDAndPID(output, "/dev/cu.usbmodemfa121"));
     assertEquals("2341_0044", new SystemProfilerParser().extractVIDAndPID(output, "/dev/tty.usbmodemfa121"));
+
+       output = TestHelper.inputStreamToString(SystemProfilerParserTest.class.getResourceAsStream("system_profiler_output2.txt"));
+
+      assertEquals("2341_8036", new SystemProfilerParser().extractVIDAndPID(output, "/dev/cu.usbmodemfd131"));
+      assertEquals("2341_8036", new SystemProfilerParser().extractVIDAndPID(output, "/dev/tty.usbmodemfd131"));
+
   }
 }
