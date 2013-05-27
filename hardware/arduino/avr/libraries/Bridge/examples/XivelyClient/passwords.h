5cb25503d767649b84aff4d95451f97f9f491360
Tom Igoe <tom.igoe@gmail.com>
2013-05-27 12:55:58 -0400

    Added passwords.h for Xively client

---
+++ hardware/arduino/avr/libraries/Bridge/examples/XivelyClient/passwords.h
@@ -0,0 +1,5 @@
+  
+#define APIKEY        "XXXXXXXX"               // replace your pachube api key here
+#define FEEDID        00000                    // replace your feed ID
+#define USERAGENT     "home-sensors"           // user agent is the project name
+
