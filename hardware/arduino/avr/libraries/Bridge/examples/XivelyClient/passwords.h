a49a2e3c2ed2f25d82bb997ef7b4e12790d1e466
tigoe <tom.igoe@gmail.com>
2013-05-30 23:10:48 -0400

    Ignore passwords.h for xively client

--- hardware/arduino/avr/libraries/Bridge/examples/XivelyClient/passwords.h
+++
@@ -1,5 +0,0 @@
-  
-#define APIKEY        "XXXXXXXX"               // replace your pachube api key here
-#define FEEDID        00000                    // replace your feed ID
-#define USERAGENT     "home-sensors"           // user agent is the project name
-
