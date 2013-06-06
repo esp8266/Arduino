a2e7413d229812ff123cb8864747558b270498f1
Cristian Maglie <c.maglie@bug.st>
2013-06-06 23:11:43 +0200

    More efficient dtostrf() emulation on ARM

--- hardware/arduino/sam/cores/arduino/avr/dtostrf.c
+++ hardware/arduino/sam/cores/arduino/avr/dtostrf.c
@@ -20,11 +20,8 @@
 
 char *dtostrf (double val, signed char width, unsigned char prec, char *sout) {
   char fmt[20];
-  if (width<0) {
-    sprintf(fmt, "%%-%d.%df", width, prec);
-  } else {
-    sprintf(fmt, "%%%d.%df", width, prec);
-  }
+  sprintf(fmt, "%%%d.%df", width, prec);
   sprintf(sout, fmt, val);
   return sout;
 }
+
