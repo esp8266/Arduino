d612a6f2ee7a687912a7e9d5aba3a58ded6b7b21
Tom Igoe <tom.igoe@gmail.com>
2013-06-22 02:03:35 -0400

    Changed ShellCommands from Console to Serial

--- hardware/arduino/avr/libraries/Bridge/examples/ShellCommands/ShellCommands.ino
+++ hardware/arduino/avr/libraries/Bridge/examples/ShellCommands/ShellCommands.ino
@@ -1,26 +1,41 @@
 
-/* Demonstrate shell commands */
+/*
+ Running shell coommands using Process class. 
+ 
+ This sketch demonstrate how to run linux shell commands
+ using an Arduino Yún. 
+
+ The circuit:
+ * Arduino Yun
+ 
+ created 12 Jun 2013
+ by Cristian Maglie
+ modified 21 June 2013
+ by Tom Igoe
+ 
+ This example code is in the public domain.
+
+ */
 
 #include <Process.h>
 
 void setup() {
+  // initialize the Bridge and Serial connections:
   Bridge.begin();
-  Console.begin();
-  Console.buffer(64);
+  Serial.begin(9600);
 }
 
 void loop() {
   Process p;
-  // This command line prints the name of the wireless that the board is connected to or that the board has created
+  // This command line prints the name of the wireless network
+  // that the board is connected to, or the network which the board has created:
   p.runShellCommand(F("lua /usr/lib/lua/pretty_wifi_info.lua | grep SSID"));
 
   // Read command output
   while (p.available()) {
     char c = p.read();
-    Console.print(c);
-  }
-  Console.flush();
-  
-  delay(5000);
+    Serial.print(c);
+  } 
+  while (true);    // do nothing more
 }
 
