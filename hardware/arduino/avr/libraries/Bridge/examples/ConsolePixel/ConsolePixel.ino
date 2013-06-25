86438fc84b663ebee6dbe357178d92b3562a3606
Tom Igoe <tom.igoe@gmail.com>
2013-06-25 13:57:19 -0400

    Added ConsolePixel example

---
+++ hardware/arduino/avr/libraries/Bridge/examples/ConsolePixel/ConsolePixel.ino
@@ -0,0 +1,58 @@
+/*
+  Console Pixel
+ 
+ An example of using the Arduino board to receive data from the 
+ Console on the Arduino Yun.  In this case, the Arduino boards turns on an LED when
+ it receives the character 'H', and turns off the LED when it
+ receives the character 'L'.
+ 
+ To see the Console, pick your Yun's name and IP address in the Port menu
+ then open the Port Monitor. You can also see it by opening a terminal window
+ and typing 
+ ssh root@ yourYunsName.local 'telnet localhost 6571'
+ then pressing enter. When prompted for the password, enter it.
+ 
+ 
+ The circuit:
+ * LED connected from digital pin 13 to ground
+ 
+ created 2006
+ by David A. Mellis
+ modified 25 Jun 2013
+ by Tom Igoe 
+ 
+ This example code is in the public domain.
+ 
+ */
+#include <Console.h>
+
+const int ledPin = 13; // the pin that the LED is attached to
+char incomingByte;      // a variable to read incoming Console data into
+
+void setup() {
+  // initialize Console communication:
+  Bridge.begin();
+  Console.begin();
+  while(!Console);  // wait for the Console to open from the remote side
+  Console.println("type H or L to turn pin 13 on or off");
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // see if there's incoming Console data:
+  if (Console.available() > 0) {
+    // read the oldest byte in the Console buffer:
+    incomingByte = Console.read();
+    Console.println(incomingByte);
+    // if it's a capital H (ASCII 72), turn on the LED:
+    if (incomingByte == 'H') {
+      digitalWrite(ledPin, HIGH);
+    } 
+    // if it's an L (ASCII 76) turn off the LED:
+    if (incomingByte == 'L') {
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
+
