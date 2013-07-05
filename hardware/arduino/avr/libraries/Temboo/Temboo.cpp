6eec8e09220082b320b5dd0ae61503c98b474f64
Federico Fissore <f.fissore@arduino.cc>
2013-07-05 11:54:56 +0200

    introducing Temboo library

---
+++ hardware/arduino/avr/libraries/Temboo/Temboo.cpp
@@ -0,0 +1,64 @@
+/*
+###############################################################################
+#
+# Temboo Arduino Yun library
+#
+# Copyright 2013, Temboo Inc.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+#
+###############################################################################
+*/
+
+#include <Temboo.h>
+
+void TembooChoreo::begin() {
+    Process::begin("temboo");
+}
+
+void TembooChoreo::setAccountName(const String& accountName) {
+    addParameter("-a" + accountName);
+}
+
+void TembooChoreo::setAppKeyName(const String& appKeyName) {
+    addParameter("-u" + appKeyName);
+}
+
+void TembooChoreo::setAppKey(const String& appKey) {
+    addParameter("-p" + appKey);
+}
+
+void TembooChoreo::setChoreo(const String& choreo) {
+    addParameter("-c" + choreo);
+}
+
+void TembooChoreo::setCredential(const String& credentialName) {
+    addParameter("-e" + credentialName);
+}
+
+void TembooChoreo::addInput(const String& inputName, const String& inputValue) {
+    addParameter("-i" + inputName + ":" + inputValue);
+}
+
+void TembooChoreo::addOutputFilter(const String& outputName, const String& filterPath, const String& variableName) {
+    addParameter("-o" + outputName + ":" + filterPath + ":" + variableName);
+}
+
+void TembooChoreo::setSettingsFileToWrite(const String& filePath) {
+    addParameter("-w" + filePath);
+}
+
+void TembooChoreo::setSettingsFileToRead(const String& filePath) {
+    addParameter("-r" + filePath);
+}
+
---
+++ hardware/arduino/avr/libraries/Temboo/Temboo.h
@@ -0,0 +1,44 @@
+/*
+###############################################################################
+#
+# Temboo Arduino Yun library
+#
+# Copyright 2013, Temboo Inc.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+#
+###############################################################################
+*/
+
+#ifndef _TEMBOO_H
+#define _TEMBOO_H
+#include <Arduino.h>
+#include <Process.h>
+
+class TembooChoreo : public Process {
+
+    public:
+        void begin();
+        void setAccountName(const String& accountName);
+        void setAppKeyName(const String& appKeyName);
+        void setAppKey(const String& appKey);
+        void setChoreo(const String& choreo);
+        void setCredential(const String& credentialName);
+        void addInput(const String& inputName, const String& inputValue);
+        void addOutputFilter(const String& filterName, const String& filterPath, const String& variableName);
+        void setSettingsFileToWrite(const String& filePath);
+        void setSettingsFileToRead(const String& filePath);
+
+};
+
+#endif 
