6ac8185c0885dc09b39cb8dc75b5d35eecdf7f8f
Matthijs Kooijman <matthijs@stdin.nl>
2013-12-01 19:18:31 +0100

    Define a _NOP() macro
    Recent avr-libc releases define one, but this allows using it also on
    older avr-libc releases.

--- hardware/arduino/avr/cores/arduino/Arduino.h
+++ hardware/arduino/avr/cores/arduino/Arduino.h
@@ -88,6 +88,10 @@ void yield(void);
 #define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
 #define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
 
+// avr-libc defines _NOP() since 1.6.2
+#ifndef _NOP
+#define _NOP() do { __asm__ volatile ("nop"); } while (0)
+#endif
 
 typedef unsigned int word;
 
