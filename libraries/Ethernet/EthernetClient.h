fbdf3a18ee84ae93f2f3227a557345ad5aa5acbf
ntruchsess <norbert.truchsess@t-online.de>
2013-12-06 19:05:31 +0100

    remove all Changes besides operator==

--- libraries/Ethernet/EthernetClient.h
+++ libraries/Ethernet/EthernetClient.h
@@ -25,9 +25,6 @@ public:
   virtual uint8_t connected();
   virtual operator bool();
   virtual bool operator==(const EthernetClient&);
-  virtual uint16_t localPort();
-  virtual IPAddress remoteIP();
-  virtual uint16_t remotePort();
 
   friend class EthernetServer;
   
