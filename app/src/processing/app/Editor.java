4f33d0851c0de227e640e10e6b214f17c2b5b316
Matthijs Kooijman <matthijs@stdin.nl>
2014-04-08 12:54:17 +0200

    Fix opening a non-primary .ino file
    When a sketch looks like this:
    
        Blink/
            Blink.ino
            Foo.ino
    
    The idea is that opening Foo.ino should open up the sketch. However,
    before this would show an error stating "The file Foo.ino needs to be
    inside a sketch folder named Foo" instead.
    
    This turned out to be due to a typo, which seems to have been present
    for a long time. Note that when the main sketch file was a .pde file,
    everything already worked as expected.

--- app/src/processing/app/Editor.java
+++ app/src/processing/app/Editor.java
@@ -2120,7 +2120,7 @@ public class Editor extends JFrame implements RunnerListener {
     String pdeName = parentName + ".pde";
     File altPdeFile = new File(parent, pdeName);
     String inoName = parentName + ".ino";
-    File altInoFile = new File(parent, pdeName);
+    File altInoFile = new File(parent, inoName);
 
     if (pdeName.equals(fileName) || inoName.equals(fileName)) {
       // no beef with this guy
