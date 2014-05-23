17e1eb5ef240e33511d86d191b5da31ce8cdec2b
Jens-Christian Skibakk <jens.christian@skibakk.no>
2014-05-23 11:29:30 +0200

    Fix idle level when initializing a inverted SoftwareSerial
    Previously, when SoftwareSerial was initialized, it would always be set
    to an idle level of HIGH, even when inverted logic was enabled. Once a
    byte is transmitted, the idle level gets correctly set to LOW instead.
    This commit makes sure that the idle level is correct directly after
    initialization already.
    
    This fixes #1361.

--- libraries/SoftwareSerial/SoftwareSerial.cpp
+++ libraries/SoftwareSerial/SoftwareSerial.cpp
@@ -355,7 +355,7 @@ SoftwareSerial::~SoftwareSerial()
 void SoftwareSerial::setTX(uint8_t tx)
 {
   pinMode(tx, OUTPUT);
-  digitalWrite(tx, HIGH);
+  digitalWrite(tx, _inverse_logic ? LOW : HIGH);
   _transmitBitMask = digitalPinToBitMask(tx);
   uint8_t port = digitalPinToPort(tx);
   _transmitPortRegister = portOutputRegister(port);
