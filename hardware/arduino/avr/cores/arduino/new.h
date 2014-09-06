9a8b9c5c5856a8531b62cd1f2a2db3188afc067a
Cristian Maglie <c.maglie@bug.st>
2014-09-06 12:44:53 +0200

    Fixed license for new.cpp and small cosmetic changes

--- hardware/arduino/avr/cores/arduino/new.h
+++ hardware/arduino/avr/cores/arduino/new.h
@@ -1,6 +1,20 @@
-/* Header to define new/delete operators as they aren't provided by avr-gcc by default
-   Taken from http://www.avrfreaks.net/index.php?name=PNphpBB2&file=viewtopic&t=59453 
- */
+/*
+  Copyright (c) 2014 Arduino.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 #ifndef NEW_H
 #define NEW_H
