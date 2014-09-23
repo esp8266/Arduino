ba8eadeeb54b8ff3dce11ca7030e114f31b7d64a
Cristian Maglie <c.maglie@bug.st>
2014-09-23 17:31:46 +0200

    Split IDE into 2 projects.
    BEWARE: HIGHLY EXPERIMENTAL BRANCH

---
+++ arduino-core/src/processing/app/windows/WINREG.java
@@ -0,0 +1,21 @@
+/*
+ * WINREG.java
+ *
+ * Created on 17. August 2007, 14:32
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package processing.app.windows;
+
+/**
+ *
+ * @author TB
+ */
+public interface WINREG {
+  public final static int HKEY_CLASSES_ROOT = 0x80000000;
+  public final static int HKEY_CURRENT_USER = 0x80000001;
+  public final static int HKEY_LOCAL_MACHINE = 0x80000002;
+  public final static int HKEY_USERS = 0x80000003;
+}
