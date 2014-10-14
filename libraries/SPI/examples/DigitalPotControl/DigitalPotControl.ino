042fa0c3f20cd654115de955f75a35de5d86aabd
Cristian Maglie <c.maglie@bug.st>
2014-10-14 17:41:59 +0200

    Backported SPI Transcations from 1.5.x

--- libraries/SPI/examples/DigitalPotControl/DigitalPotControl.ino
+++ libraries/SPI/examples/DigitalPotControl/DigitalPotControl.ino
@@ -1,16 +1,16 @@
 /*
   Digital Pot Control
-  
+
   This example controls an Analog Devices AD5206 digital potentiometer.
   The AD5206 has 6 potentiometer channels. Each channel's pins are labeled
   A - connect this to voltage
   W - this is the pot's wiper, which changes when you set it
   B - connect this to ground.
- 
- The AD5206 is SPI-compatible,and to command it, you send two bytes, 
+
+ The AD5206 is SPI-compatible,and to command it, you send two bytes,
  one with the channel number (0 - 5) and one with the resistance value for the
- channel (0 - 255).  
- 
+ channel (0 - 255).
+
  The circuit:
   * All A pins  of AD5206 connected to +5V
   * All B pins of AD5206 connected to ground
@@ -18,12 +18,12 @@
   * CS - to digital pin 10  (SS pin)
   * SDI - to digital pin 11 (MOSI pin)
   * CLK - to digital pin 13 (SCK pin)
- 
- created 10 Aug 2010 
+
+ created 10 Aug 2010
  by Tom Igoe
- 
+
  Thanks to Heather Dewey-Hagborg for the original tutorial, 2005
- 
+
 */
 
 
@@ -38,12 +38,12 @@ void setup() {
   // set the slaveSelectPin as an output:
   pinMode (slaveSelectPin, OUTPUT);
   // initialize SPI:
-  SPI.begin(); 
+  SPI.begin();
 }
 
 void loop() {
   // go through the six channels of the digital pot:
-  for (int channel = 0; channel < 6; channel++) { 
+  for (int channel = 0; channel < 6; channel++) {
     // change the resistance on this channel from min to max:
     for (int level = 0; level < 255; level++) {
       digitalPotWrite(channel, level);
@@ -62,10 +62,10 @@ void loop() {
 
 void digitalPotWrite(int address, int value) {
   // take the SS pin low to select the chip:
-  digitalWrite(slaveSelectPin,LOW);
+  digitalWrite(slaveSelectPin, LOW);
   //  send in the address and value via SPI:
   SPI.transfer(address);
   SPI.transfer(value);
   // take the SS pin high to de-select the chip:
-  digitalWrite(slaveSelectPin,HIGH); 
+  digitalWrite(slaveSelectPin, HIGH);
 }
