e52b4edd6f80054e67f0e5b3dc32e37243072fd4
Federico Fissore <f.fissore@arduino.cc>
2014-11-12 17:55:54 +0100

    Temboo: removing ControlBySMS and SendDataToMySQL examples, as per Temboo request

--- libraries/Bridge/examples/Temboo/SendDataToMySQL/TembooAccount.h
+++
@@ -1,4 +0,0 @@
-#define TEMBOO_ACCOUNT "myTembooAccountName"  // your Temboo account name 
-#define TEMBOO_APP_KEY_NAME "myFirstApp"  // your Temboo app key name
-#define TEMBOO_APP_KEY  "xxx-xxx-xxx-xx-xxx"  // your Temboo app key
-
