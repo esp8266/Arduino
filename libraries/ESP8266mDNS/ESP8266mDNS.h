31c7b384b205e6fea818cab3962170141d5c2d59
Ivan Grokhotkov <igrokhotkov@gmail.com>
2015-02-01 01:55:37 +0300

    Add mDNS library for esp8266

---
+++ libraries/ESP8266mDNS/ESP8266mDNS.h
@@ -0,0 +1,69 @@
+/*
+ESP8266 Multicast DNS (port of CC3000 Multicast DNS library)
+Version 1.1
+Copyright (c) 2013 Tony DiCola (tony@tonydicola.com)
+ESP8266 port (c) 2015 Ivan Grokhotkov (igrokhotkov@gmail.com)
+
+This is a simple implementation of multicast DNS query support for an Arduino
+running on ESP8266 chip. Only support for resolving address queries is currently
+implemented.
+
+Requirements:
+- ESP8266WiFi library
+
+Usage:
+- Include the ESP8266 Multicast DNS library in the sketch.
+- Create an instance of the MDNSResponder class.
+- Call the begin method in the sketch's setup and provide a domain name (without
+  the '.local' suffix, i.e. just provide 'foo' to resolve 'foo.local'), and the 
+  Adafruit CC3000 class instance.  Optionally provide a time to live (in seconds) 
+  for the DNS record--the default is 1 hour.
+- Call the update method in each iteration of the sketch's loop function.
+
+License (MIT license):
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+
+*/
+#ifndef ESP8266MDNS_H
+#define ESP8266MDNS_H
+
+#include "ESP8266WiFi.h"
+#include "WiFiUdp.h"
+
+class MDNSResponder {
+public:
+  MDNSResponder();
+  ~MDNSResponder();
+  bool begin(const char* domain, IPAddress addr, uint32_t ttlSeconds = 3600);
+  void update();
+
+private:
+  // Expected query values
+  uint8_t* _expected;
+  int _expectedLen;
+  // Current parsing state
+  int _index;
+  // Response data
+  uint8_t* _response;
+  int _responseLen;
+  // Socket for MDNS communication
+  WiFiUDP _mdnsConn;
+};
+
+#endif //ESP8266MDNS_H
---
+++ libraries/ESP8266mDNS/examples/mDNS_Web_Server/mDNS_Web_Server.ino
@@ -0,0 +1,124 @@
+/*
+  ESP8266 mDNS responder sample
+
+  This is an example of an HTTP server that is accessible
+  via http://esp8266.local URL thanks to mDNS responder.
+
+  Instructions:
+  - Update WiFi SSID and password as necessary.
+  - Flash the sketch to the ESP8266 board
+  - Install host software:
+    - For Linux, install Avahi (http://avahi.org/).
+    - For Windows, install Bonjour (http://www.apple.com/support/bonjour/).
+    - For Mac OSX and iOS support is built in through Bonjour already.
+  - Point your browser to http://esp8266.local, you should see a response.
+
+ */
+
+
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiClient.h>
+
+const char* ssid = "............";
+const char* password = "..............";
+
+// multicast DNS responder
+MDNSResponder mdns;
+
+// TCP server at port 80 will respond to HTTP requests
+WiFiServer server(80);
+
+void setup(void)
+{  
+  Serial.begin(115200);
+  
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);
+  Serial.println("");  
+  
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Set up mDNS responder:
+  // - first argument is the domain name, in this example
+  //   the fully-qualified domain name is "esp8266.local"
+  // - second argument is the IP address to advertise
+  //   we send our IP address on the WiFi network
+  //   Note: for AP mode we would use WiFi.softAPIP()!
+  if (!mdns.begin("esp8266", WiFi.localIP())) {
+    Serial.println("Error setting up MDNS responder!");
+    while(1) { 
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+  
+  // Start TCP (HTTP) server
+  server.begin();
+  Serial.println("TCP server started");
+}
+
+void loop(void)
+{
+  // Check for any mDNS queries and send responses
+  mdns.update();
+  
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  Serial.println("");
+  Serial.println("New client");
+
+  // Wait for data from client to become available
+  while(client.connected() && !client.available()){
+    delay(1);
+  }
+  
+  // Read the first line of HTTP request
+  String req = client.readStringUntil('\r');
+  
+  // First line of HTTP request looks like "GET /path HTTP/1.1"
+  // Retrieve the "/path" part by finding the spaces
+  int addr_start = req.indexOf(' ');
+  int addr_end = req.indexOf(' ', addr_start + 1);
+  if (addr_start == -1 || addr_end == -1) {
+    Serial.print("Invalid request: ");
+    Serial.println(req);
+    return;
+  }
+  req = req.substring(addr_start + 1, addr_end);
+  Serial.print("Request: ");
+  Serial.println(req);
+  client.flush();
+  
+  String s;
+  if (req == "/")
+  {
+    IPAddress ip = WiFi.localIP();
+    String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+    s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>Hello from ESP8266 at ";
+    s += ipStr;
+    s += "</html>\r\n\r\n";
+    Serial.println("Sending 200");
+  }
+  else
+  {
+    s = "HTTP/1.1 404 Not Found\r\n\r\n";
+    Serial.println("Sending 404");
+  }
+  client.print(s);
+  
+  Serial.println("Done with client");
+}
+
