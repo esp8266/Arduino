4c574ff4f5015cdb888b00935f6559537fecae1b
Ivan Grokhotkov <igrokhotkov@gmail.com>
2015-02-01 01:51:46 +0300

    Disable TCP/UDP debug by default

--- hardware/arduino/esp8266/cores/esp8266/debug.h
+++ hardware/arduino/esp8266/cores/esp8266/debug.h
@@ -2,7 +2,7 @@
 #define ARD_DEBUG_H
 
 #include <stddef.h>
-#define DEBUGV(...) ets_printf(__VA_ARGS__)
-
+// #define DEBUGV(...) ets_printf(__VA_ARGS__)
+#define DEBUGV(...) 
 
 #endif//ARD_DEBUG_H
