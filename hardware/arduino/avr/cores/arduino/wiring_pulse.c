d4a80be045f488b35093f329ed853ae8af0ff637
Martino Facchin <m.facchin@arduino.cc>
2015-03-02 13:35:30 +0100

    pulseIn: add alternative implementation based on micros()
    pulseInLong is suitable for long pulses in interrupt context

--- hardware/arduino/avr/cores/arduino/wiring_pulse.c
+++ hardware/arduino/avr/cores/arduino/wiring_pulse.c
@@ -50,3 +50,45 @@ unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout)
 	width = countPulseASM(portInputRegister(port), bit, stateMask, maxloops);
 	return clockCyclesToMicroseconds(width * 16 + 16);
 }
+
+/* Measures the length (in microseconds) of a pulse on the pin; state is HIGH
+ * or LOW, the type of pulse to measure.  Works on pulses from 2-3 microseconds
+ * to 3 minutes in length, but must be called at least a few dozen microseconds
+ * before the start of the pulse.
+ *
+ * ATTENTION:
+ * this function relies on micros() so cannot be used in noInterrupt() context
+ */
+unsigned long pulseInLong(uint8_t pin, uint8_t state, unsigned long timeout)
+{
+	// cache the port and bit of the pin in order to speed up the
+	// pulse width measuring loop and achieve finer resolution.  calling
+	// digitalRead() instead yields much coarser resolution.
+	uint8_t bit = digitalPinToBitMask(pin);
+	uint8_t port = digitalPinToPort(pin);
+	uint8_t stateMask = (state ? bit : 0);
+	unsigned long width = 0; // keep initialization out of time critical area
+
+	// convert the timeout from microseconds to a number of times through
+	// the initial loop; it takes 16 clock cycles per iteration.
+	unsigned long numloops = 0;
+	unsigned long maxloops = microsecondsToClockCycles(timeout);
+
+	// wait for any previous pulse to end
+	while ((*portInputRegister(port) & bit) == stateMask)
+		if (numloops++ == maxloops)
+			return 0;
+
+	// wait for the pulse to start
+	while ((*portInputRegister(port) & bit) != stateMask)
+		if (numloops++ == maxloops)
+			return 0;
+
+    unsigned long start = micros();
+    // wait for the pulse to stop
+    while ((*portInputRegister(port) & bit) == stateMask) {
+        if (numloops++ == maxloops)
+            return 0;
+    }
+    return micros() - start;
+}
