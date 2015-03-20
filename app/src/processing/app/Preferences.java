a0530886910e44026258423f6f021516d69a5059
Federico Fissore <f.fissore@arduino.cc>
2015-03-20 10:12:08 +0100

    Fixed bug with HTTPS proxy settings

--- app/src/processing/app/Preferences.java
+++ app/src/processing/app/Preferences.java
@@ -737,9 +737,9 @@ public class Preferences {
     } catch (NumberFormatException e) {
       Preferences.remove("proxy.http.port");
     }
-    Preferences.set("proxy.https.server", proxyHTTPServer.getText());
+    Preferences.set("proxy.https.server", proxyHTTPSServer.getText());
     try {
-      Preferences.set("proxy.https.port", Integer.valueOf(proxyHTTPPort.getText()).toString());
+      Preferences.set("proxy.https.port", Integer.valueOf(proxyHTTPSPort.getText()).toString());
     } catch (NumberFormatException e) {
       Preferences.remove("proxy.https.port");
     }
@@ -783,11 +783,11 @@ public class Preferences {
     } catch (NumberFormatException e) {
       proxyHTTPPort.setText("");
     }
-    proxyHTTPServer.setText(Preferences.get("proxy.https.server"));
+    proxyHTTPSServer.setText(Preferences.get("proxy.https.server"));
     try {
-      proxyHTTPPort.setText(Integer.toString(Preferences.getInteger("proxy.https.port", 8443)));
+      proxyHTTPSPort.setText(Integer.toString(Preferences.getInteger("proxy.https.port", 8443)));
     } catch (NumberFormatException e) {
-      proxyHTTPPort.setText("");
+      proxyHTTPSPort.setText("");
     }
     proxyUser.setText(Preferences.get("proxy.user"));
     proxyPassword.setText(Preferences.get("proxy.password"));
