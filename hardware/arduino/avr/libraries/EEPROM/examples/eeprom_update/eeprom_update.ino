ab1447524861fb02f68a2fb423c9c8504cfd87c4
Chris--A <chris@genx.biz>
2015-03-20 12:06:20 +1000

    Updated EEPROM examples.
    Removed hard coded lengths, which were incorrect for standard Arduino's
    now.

--- hardware/arduino/avr/libraries/EEPROM/examples/eeprom_update/eeprom_update.ino
+++ hardware/arduino/avr/libraries/EEPROM/examples/eeprom_update/eeprom_update.ino
@@ -38,15 +38,32 @@ void loop()
     The function EEPROM.update(addr, val) is equivalent to the following:
   
     if( EEPROM.read(addr) != val ){
-	  EEPROM.write(addr, val);
+      EEPROM.write(addr, val);
     }
   ***/
   
 
-  /** advance to the next address.  there are 512 bytes in the EEPROM, so go back to 0 when we hit 512. **/
+  /***
+    Advance to the next address, when at the end restart at the beginning.    
+    
+    Larger AVR processors have larger EEPROM sizes, E.g:
+    - Arduno Duemilanove: 512b EEPROM storage.
+    - Arduino Uno:        1kb EEPROM storage.
+    - Arduino Mega:       4kb EEPROM storage.
+    
+    Rather than hard-coding the length, you should use the pre-provided length function.
+    This will make your code portable to all AVR processors.    
+  ***/
   addr = addr + 1;
-  if (addr == 512)
+  if(addr == EEPROM.length())
     addr = 0;
+    
+  /***
+    As the EEPROM sizes are powers of two, wrapping (preventing overflow) of an 
+    EEPROM address is also doable by a bitwise and of the length - 1.
+    
+    ++addr &= EEPROM.length() - 1;
+  ***/
 
   delay(100);
 }
--- hardware/arduino/avr/libraries/EEPROM/examples/eeprom_write/eeprom_write.ino
+++ hardware/arduino/avr/libraries/EEPROM/examples/eeprom_write/eeprom_write.ino
@@ -8,31 +8,51 @@
 
 #include <EEPROM.h>
 
-// the current address in the EEPROM (i.e. which byte
-// we're going to write to next)
-int addr = 0;
+/** the current address in the EEPROM (i.e. which byte we're going to write to next) **/
+int addr = 0; 
 
-void setup()
-{
-}
+void setup(){ /** Empty setup. **/}
 
 void loop()
 {
-  // need to divide by 4 because analog inputs range from
-  // 0 to 1023 and each byte of the EEPROM can only hold a
-  // value from 0 to 255.
+  /***
+    Need to divide by 4 because analog inputs range from
+    0 to 1023 and each byte of the EEPROM can only hold a
+    value from 0 to 255.
+  ***/
+  
   int val = analogRead(0) / 4;
 
-  // write the value to the appropriate byte of the EEPROM.
-  // these values will remain there when the board is
-  // turned off.
+  /***
+    Write the value to the appropriate byte of the EEPROM.
+    these values will remain there when the board is
+    turned off.
+  ***/
+  
   EEPROM.write(addr, val);
 
-  // advance to the next address.  there are 512 bytes in
-  // the EEPROM, so go back to 0 when we hit 512.
+  /***
+    Advance to the next address, when at the end restart at the beginning.    
+    
+    Larger AVR processors have larger EEPROM sizes, E.g:
+    - Arduno Duemilanove: 512b EEPROM storage.
+    - Arduino Uno:        1kb EEPROM storage.
+    - Arduino Mega:       4kb EEPROM storage.
+    
+    Rather than hard-coding the length, you should use the pre-provided length function.
+    This will make your code portable to all AVR processors.    
+  ***/
   addr = addr + 1;
-  if (addr == 512)
+  if(addr == EEPROM.length())
     addr = 0;
+    
+  /***
+    As the EEPROM sizes are powers of two, wrapping (preventing overflow) of an 
+    EEPROM address is also doable by a bitwise and of the length - 1.
+    
+    ++addr &= EEPROM.length() - 1;
+  ***/
+
 
   delay(100);
 }
