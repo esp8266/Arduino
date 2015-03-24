ab5aea40a6bd3ccef74a2cc394f5bb88d4e940d8
Martino Facchin <m.facchin@arduino.cc>
2015-03-24 17:13:14 +0100

    Stream: set findMulti function as protected
    let's not expose it until the API is not accepted
    widely by the dev mailing list

--- hardware/arduino/avr/cores/arduino/Stream.h
+++ hardware/arduino/avr/cores/arduino/Stream.h
@@ -98,7 +98,6 @@ class Stream : public Print
 
   float parseFloat(char skipChar);  // as above but the given skipChar is ignored
 
-  public:
   struct MultiTarget {
     const char *str;  // string you're searching for
     size_t len;       // length of string you're searching for
@@ -108,8 +107,6 @@ class Stream : public Print
   // This allows you to search for an arbitrary number of strings.
   // Returns index of the target that is found first or -1 if timeout occurs.
   int findMulti(struct MultiTarget *targets, int tCount);
-
-
 };
 
 
