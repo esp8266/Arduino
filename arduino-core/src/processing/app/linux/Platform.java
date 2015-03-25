f183966296458bc9e157b3d05cadb54aaac090aa
Federico Fissore <f.fissore@arduino.cc>
2015-03-25 10:16:07 +0100

    Added GTKLookAndFeelFixer, courtesy Klaus Reimer https://www.ailis.de/~k/archives/67-Workaround-for-borderless-Java-Swing-menus-on-Linux.html It makes menu separators visible on linux with the GTK look and feel

--- arduino-core/src/processing/app/linux/Platform.java
+++ arduino-core/src/processing/app/linux/Platform.java
@@ -29,7 +29,9 @@ import processing.app.debug.TargetPackage;
 import processing.app.legacy.PConstants;
 import processing.app.tools.CollectStdOutExecutor;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 
@@ -42,17 +44,7 @@ public class Platform extends processing.app.Platform {
   // TODO Need to be smarter here since KDE people ain't gonna like that GTK.
   //      It may even throw a weird exception at 'em for their trouble.
   public void setLookAndFeel() throws Exception {
-    // Linux is by default even uglier than metal (Motif?).
-    // Actually, i'm using native menus, so they're even uglier
-    // and Motif-looking (Lesstif?). Ick. Need to fix this.
-    //String lfname = UIManager.getCrossPlatformLookAndFeelClassName();
-    //UIManager.setLookAndFeel(lfname);
-
-    // For 0120, trying out the gtk+ look and feel as the default.
-    // This is available in Java 1.4.2 and later, and it can't possibly
-    // be any worse than Metal. (Ocean might also work, but that's for
-    // Java 1.5, and we aren't going there yet)
-    //UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+    GTKLookAndFeelFixer.installGtkPopupBugWorkaround();
   }
 
 
