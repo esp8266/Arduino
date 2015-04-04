dc5ee0003c705cc14390b217a1921387841b2418
Sermus <andrey.v.filimonov@gmail.com>
2015-04-04 10:37:51 +0300

    Port of extended Adafruit ILI9341 lib to ESP8266

---
+++ libraries/Adafruit_ILI9341/Load_fonts.h
@@ -0,0 +1,8 @@
+// Comment out the #defines below with // to stop that font being loaded
+// If all fonts are loaded the total space required is ablout 17890 bytes
+
+//#define LOAD_GLCD // Standard Adafruit font needs ~1792 bytes in FLASH
+//#define LOAD_FONT2 // Small font, needs ~3092 bytes in FLASH
+//#define LOAD_FONT4 // Medium font, needs ~8126 bytes in FLASH
+//#define LOAD_FONT6 // Large font, needs ~4404 bytes in FLASH
+//#define LOAD_FONT7 // 7 segment font, needs ~3652 bytes in FLASH
