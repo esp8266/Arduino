b93915dcad8aceaf48cf25702eb12c2017fc882b
Sermus <andrey.v.filimonov@gmail.com>
2015-04-04 10:37:51 +0300

    Port of extended Adafruit ILI9341 lib to ESP8266

---
+++ hardware/esp8266com/esp8266/libraries/Adafruit_ILI9341/Font32.h
@@ -0,0 +1,7 @@
+#define nr_chrs_f32 96
+#define chr_hgt_f32 26
+#define data_size_f32 8
+#define firstchr_f32 32
+
+extern const unsigned char widtbl_f32[96];
+extern const unsigned char* chrtbl_f32[96];
---
+++ hardware/esp8266com/esp8266/libraries/Adafruit_ILI9341/Font64.h
@@ -0,0 +1,7 @@
+#define nr_chrs_f64 96
+#define chr_hgt_f64 48
+#define data_size_f64 8
+#define firstchr_f64 32
+
+extern const unsigned char widtbl_f64[96];
+extern const unsigned char* chrtbl_f64[96];
