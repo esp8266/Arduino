7795695c2e1f7bbf9c20df437f52c6bc258e5e21
Bundit J. <boybundit@users.noreply.github.com>
2015-04-18 16:08:36 +0700

    Add get and put functions to EEPROM
    As available in http://www.arduino.cc/en/Reference/EEPROM

--- hardware/esp8266com/esp8266/libraries/EEPROM/EEPROM.h
+++ hardware/esp8266com/esp8266/libraries/EEPROM/EEPROM.h
@@ -37,7 +37,7 @@ class EEPROMClass
 
     template<typename T> T &get(int address, T &t)
     {
-        if (address < 0 || address >= _size)
+        if (address < 0 || address + sizeof(T) > _size)
             return t;
 
         uint8_t *ptr = (uint8_t*) &t;
@@ -47,7 +47,7 @@ class EEPROMClass
 
     template<typename T> const T &put(int address, const T &t)
     {
-        if (address < 0 || address >= _size)
+        if (address < 0 || address + sizeof(T) > _size)
             return t;
 
         const uint8_t *ptr = (const uint8_t*) &t;
