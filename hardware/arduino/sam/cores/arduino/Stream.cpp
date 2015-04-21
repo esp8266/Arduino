c944a4c84d822977826da724bbbc8b0609d6aeba
Cristian Maglie <c.maglie@bug.st>
2015-04-21 18:11:05 +0200

    Updated Stream.cpp in SAM core to the latest version

--- hardware/arduino/sam/cores/arduino/Stream.cpp
+++ hardware/arduino/sam/cores/arduino/Stream.cpp
@@ -254,56 +254,61 @@ String Stream::readStringUntil(char terminator)
 int Stream::findMulti( struct Stream::MultiTarget *targets, int tCount) {
   // any zero length target string automatically matches and would make
   // a mess of the rest of the algorithm.
-  for (struct MultiTarget *t = targets; t < targets+tCount; ++t)
+  for (struct MultiTarget *t = targets; t < targets+tCount; ++t) {
     if (t->len <= 0)
       return t - targets;
+  }
 
-  while(1) {
+  while (1) {
     int c = timedRead();
     if (c < 0)
       return -1;
 
     for (struct MultiTarget *t = targets; t < targets+tCount; ++t) {
       // the simple case is if we match, deal with that first.
-      if (c == t->str[t->index])
-	if (++t->index == t->len)
-	  return t - targets;
-	else
-	  continue;
+      if (c == t->str[t->index]) {
+        if (++t->index == t->len)
+          return t - targets;
+        else
+          continue;
+      }
 
       // if not we need to walk back and see if we could have matched further
       // down the stream (ie '1112' doesn't match the first position in '11112'
       // but it will match the second position so we can't just reset the current
       // index to 0 when we find a mismatch.
       if (t->index == 0)
-	continue;
+        continue;
 
       int origIndex = t->index;
       do {
-	--t->index;
-	// first check if current char works against the new current index
-	if (c != t->str[t->index])
-	  continue;
+        --t->index;
+        // first check if current char works against the new current index
+        if (c != t->str[t->index])
+          continue;
 
-	// if it's the only char then we're good, nothing more to check
-	if (t->index == 0) {
-	  t->index++;
-	  break;
-	}
+        // if it's the only char then we're good, nothing more to check
+        if (t->index == 0) {
+          t->index++;
+          break;
+        }
 
-	// otherwise we need to check the rest of the found string
-	int diff = origIndex - t->index;
-	int i;
-	for (i = 0; i < t->index; ++i)
-	  if (t->str[i] != t->str[i + diff])
-	    break;
-	// if we successfully got through the previous loop then our current
-	// index is good.
-	if (i == t->index) {
-	  t->index++;
-	  break;
-	}
-	// otherwise we just try the next index
+        // otherwise we need to check the rest of the found string
+        int diff = origIndex - t->index;
+        size_t i;
+        for (i = 0; i < t->index; ++i) {
+          if (t->str[i] != t->str[i + diff])
+            break;
+        }
+
+        // if we successfully got through the previous loop then our current
+        // index is good.
+        if (i == t->index) {
+          t->index++;
+          break;
+        }
+
+        // otherwise we just try the next index
       } while (t->index);
     }
   }
