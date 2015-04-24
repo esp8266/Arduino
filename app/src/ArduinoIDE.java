58cd233aee5b5b80e48fe24b8da790eac20d710d
Federico Fissore <f.fissore@arduino.cc>
2015-04-24 12:59:46 +0200

    New editor: addede missing license headers on new files

--- app/src/ArduinoIDE.java
+++ app/src/ArduinoIDE.java
@@ -1,36 +1,44 @@
-/* -*- mode: java; c-basic-offset: 2; indent-tabs-mode: nil -*- */
-
 /*
-  Part of the Processing project - http://processing.org
-
-  Copyright (c) 2004-10 Ben Fry and Casey Reas
-  Copyright (c) 2001-04 Massachusetts Institute of Technology
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License version 2
-  as published by the Free Software Foundation.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software Foundation,
-  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ * This file is part of Arduino.
+ *
+ * Copyright 2015 Ricardo JL Rufino (ricardo@criativasoft.com.br)
+ *
+ * Arduino is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, you may use this file as part of a free software
+ * library without restriction.  Specifically, if other files instantiate
+ * templates or use macros or inline functions from this file, or you compile
+ * this file and link it with other files to produce an executable, this
+ * file does not by itself cause the resulting executable to be covered by
+ * the GNU General Public License.  This exception does not however
+ * invalidate any other reasons why the executable file might be covered by
+ * the GNU General Public License.
+ */
 
 import processing.app.Base;
 
 /**
  * Arduino IDE Launcher class
- * @author Ricardo JL Rufino (ricardo@criativasoft.com.br) 
+ *
+ * @author Ricardo JL Rufino (ricardo@criativasoft.com.br)
  * @date 23/01/2015
  */
 public class ArduinoIDE {
 
-   public static void main(String[] args) throws Exception {
-      Base.main(args);
-   }
+  public static void main(String[] args) throws Exception {
+    Base.main(args);
+  }
 
 }
