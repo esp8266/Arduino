a1aa4c1493189cce5366e0cb7c22d611403ae3ee
ficeto <ficeto@Hristos-Mac-mini-2.local>
2015-05-06 11:45:40 +0300

    adding some missing libs and definitions
    included also a dump of the known registers range

--- hardware/tools/esp8266/sdk/include/c_types.h
+++ hardware/tools/esp8266/sdk/include/c_types.h
@@ -9,10 +9,17 @@
 #include <stdbool.h>
 #include <stdarg.h>
 
+typedef unsigned char       uint8_t;
 typedef signed char         sint8_t;
+typedef signed char         int8_t;
+typedef unsigned short      uint16_t;
 typedef signed short        sint16_t;
+typedef signed short        int16_t;
+typedef unsigned long       uint32_t;
 typedef signed long         sint32_t;
+typedef signed long         int32_t;
 typedef signed long long    sint64_t;
+typedef unsigned long long  uint64_t;
 typedef unsigned long long  u_int64_t;
 typedef float               real32_t;
 typedef double              real64_t;
@@ -39,6 +46,8 @@ typedef float               real32;
 typedef double              real64;
 
 #define __le16      u16
+
+typedef unsigned int        size_t;
 
 #define __packed        __attribute__((packed))
 
@@ -76,7 +85,10 @@ typedef enum {
 #endif /* ICACHE_FLASH */
 
 #ifndef __cplusplus
+typedef unsigned char   bool;
 #define BOOL            bool
+#define true            (1)
+#define false           (0)
 #define TRUE            true
 #define FALSE           false
 
