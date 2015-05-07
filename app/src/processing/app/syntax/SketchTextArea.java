5bc6cee1185cf780a6e0445e43accf5842f515e8
Federico Fissore <f.fissore@arduino.cc>
2015-05-07 17:30:05 +0200

    Added missing licenses/copyrights

--- app/src/processing/app/syntax/SketchTextArea.java
+++ app/src/processing/app/syntax/SketchTextArea.java
@@ -2,6 +2,7 @@
  * This file is part of Arduino.
  *
  * Copyright 2015 Ricardo JL Rufino (ricardo@criativasoft.com.br)
+ * Copyright 2015 Arduino LLC
  *
  * Arduino is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
