4643cd173ddd7d18826907e7616dbfff890c9daa
Makuna <Makuna@hotmail.com>
2015-05-07 12:29:37 -0700

    refactored xt_disable_intterupts to accept level
    due to the rsil requirement that the level be a constant, the method was
    moved into a macro

--- hardware/esp8266com/esp8266/cores/esp8266/core_esp8266_wiring_digital.c
+++ hardware/esp8266com/esp8266/cores/esp8266/core_esp8266_wiring_digital.c
@@ -139,20 +139,8 @@ extern void __detachInterrupt(uint8_t pin) {
   }
 }
 
-static uint32_t interruptState = 0;
-
-void xt_disable_interrupts()
-{
-    __asm__ __volatile__("rsil %0,15":"=a" (interruptState));
-    __asm__("esync");
-    __asm__("isync");
-    __asm__("dsync");
-}
-void xt_enable_interrupts()
-{
-    __asm__ __volatile__("wsr %0,ps"::"a" (interruptState) : "memory");
-    __asm__("esync");
-}
+// stored state for the noInterrupts/interrupts methods
+uint32_t interruptsState = 0;
 
 void initPins() {
   //Disable UART interrupts
