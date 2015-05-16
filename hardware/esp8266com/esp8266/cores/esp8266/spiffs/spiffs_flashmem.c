8d92e73e6310e2df85ea78a3b6cbd9ca57ac955b
ficeto <ficeto@Hristos-Mac-mini-2.local>
2015-05-17 00:04:39 +0300

    spiffs fixes

--- hardware/esp8266com/esp8266/cores/esp8266/spiffs/spiffs_flashmem.c
+++ hardware/esp8266com/esp8266/cores/esp8266/spiffs/spiffs_flashmem.c
@@ -96,15 +96,6 @@ uint32_t flashmem_read( void *to, uint32_t fromaddr, uint32_t size )
   return ssize;
 }
 
-bool flashmem_erase_sector( uint32_t sector_id )
-{
-  WDT_RESET();
-  noInterrupts();
-  bool erased = spi_flash_erase_sector( sector_id ) == SPI_FLASH_RESULT_OK;
-  interrupts();
-  return erased;
-}
-
 SPIFlashInfo flashmem_get_info()
 {
     volatile SPIFlashInfo spi_flash_info STORE_ATTR;
@@ -176,6 +167,15 @@ uint32_t flashmem_get_sector_of_address( uint32_t addr )
 }
 
 /////////////////////////////////////////////////////
+
+bool flashmem_erase_sector( uint32_t sector_id )
+{
+  WDT_RESET();
+  noInterrupts();
+  bool erased = spi_flash_erase_sector( sector_id ) == SPI_FLASH_RESULT_OK;
+  interrupts();
+  return erased;
+}
 
 uint32_t flashmem_write_internal( const void *from, uint32_t toaddr, uint32_t size )
 {
