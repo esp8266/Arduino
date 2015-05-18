25107079114de15717efd59883c96f4eb15d5fb2
Ivan Grokhotkov <igrokhotkov@gmail.com>
2015-05-18 14:54:06 +0300

    Merge branch 'master' into esp8266
    * master: (414 commits)
      Don't export sketch if the underlying core does not support it. Fixes #3171
      RSyntaxTextArea: using a modified version, tracked at https://github.com/arduino/RSyntaxTextArea. Fixes #3099
      Updated keywords.txt
      New editor on MacOSX: since CMD+J is known as "jump to selection" and the editor has no such feature, CMD+J is disabled on mac. See #3098
      Old Preferences class remains for backwards compatibility as a delegate for PreferencesData
      New Preferences window: renders fine on every OS and it's easier to adapt using NetBeans as visual editor. Fixes #3140
      Remove spawn from exec command
      Removed redundant call to File.deleteIfExists()
      Removed buggy redundant check in FileUtils.deleteIfExists()
      Restored current line/current selected lines display on lower left of the IDE. Fixes #3134
      Updated cursor.ino
      New editor on MacOSX: restored CMD+E for finding selected text
      New editor on MacOSX: CMD+UP/DOWN moves cursor to start or end of sketch. See #3098
      New editor on MacOSX: CMD+BACKSPACE deletes current line until cursor position, ALT+BACKSPACE deletes previous word. See #3098
      ArduinoIDE is in the default package. Removed
      Fixes  #2969:
      Fix Uncategorized warning message
      New editor: ALT+ BACKSPACE deletes next word (OSX only). See #3098
      New editor: ALT+ UP/DOWN move current line only if "editor.advanced" (hidden pref) is true. Fixes #3101
      New editor: mark occurrences enable when "editor.advanced" (hidden pref) is true. Fixes #3102
      ...
    
    Conflicts:
    	.gitignore
    	build/build.xml
    	hardware/esp8266com/esp8266/libraries/ESP8266WiFi/keywords.txt
    	hardware/esp8266com/esp8266/libraries/ESP8266WiFi/library.properties
    	hardware/esp8266com/esp8266/libraries/ESP8266WiFi/src/ESP8266WiFi.h
    	libraries/WiFi/README.adoc
    	libraries/WiFi/src/WiFi.cpp
    	libraries/WiFi/src/WiFiClient.cpp
    	libraries/WiFi/src/WiFiClient.h
    	libraries/WiFi/src/WiFiServer.cpp
    	libraries/WiFi/src/WiFiUdp.cpp

--- libraries/SD/examples/ReadWrite/ReadWrite.ino
+++ libraries/SD/examples/ReadWrite/ReadWrite.ino
@@ -33,11 +33,6 @@ void setup()
 
 
   Serial.print("Initializing SD card...");
-  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
-  // Note that even if it's not used as the CS pin, the hardware SS pin
-  // (10 on most Arduino boards, 53 on the Mega) must be left as an output
-  // or the SD library functions will not work.
-  pinMode(10, OUTPUT);
 
   if (!SD.begin(4)) {
     Serial.println("initialization failed!");
