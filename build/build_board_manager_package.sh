9764c9614b93e713ccb57de9e392594c173ff34f
Ivan Grokhotkov <igrokhotkov@gmail.com>
2015-05-19 09:25:40 +0300

    Add 32-bit linux tools

--- build/build_board_manager_package.sh
+++ build/build_board_manager_package.sh
@@ -64,25 +64,32 @@ cat << EOF > package_esp8266com_index.json
       "version":"0.4.4",
       "systems": [
         {
-           "host":"i686-mingw32",
-           "url":"https://github.com/igrr/esptool-ck/releases/download/0.4.4/esptool-0.4.4-win32.zip",
-           "archiveFileName":"esptool-0.4.4-win32.zip",
-           "checksum":"SHA-256:bc52165c847b194d8f079add982eae1c4b4466784bff8c8494241de602a003b3",
-           "size":"17262"
+            "host":"i686-mingw32",
+            "url":"https://github.com/igrr/esptool-ck/releases/download/0.4.4/esptool-0.4.4-win32.zip",
+            "archiveFileName":"esptool-0.4.4-win32.zip",
+            "checksum":"SHA-256:bc52165c847b194d8f079add982eae1c4b4466784bff8c8494241de602a003b3",
+            "size":"17262"
         },
         {
-           "host":"x86_64-apple-darwin",
-           "url":"https://github.com/igrr/esptool-ck/releases/download/0.4.4/esptool-0.4.4-osx.tar.gz",
-           "archiveFileName":"esptool-0.4.4-osx.tar.gz",
-           "checksum":"SHA-256:bb2a58c3583f9dcb0d3e7611531a0f3a29c21a4a1c442987bb29d07408824dfe",
-           "size":"12145"
+            "host":"x86_64-apple-darwin",
+            "url":"https://github.com/igrr/esptool-ck/releases/download/0.4.4/esptool-0.4.4-osx.tar.gz",
+            "archiveFileName":"esptool-0.4.4-osx.tar.gz",
+            "checksum":"SHA-256:bb2a58c3583f9dcb0d3e7611531a0f3a29c21a4a1c442987bb29d07408824dfe",
+            "size":"12145"
         },
         {
-           "host":"x86_64-pc-linux-gnu",
-           "url":"https://github.com/igrr/esptool-ck/releases/download/0.4.4/esptool-0.4.4-linux64.tar.gz",
-           "archiveFileName":"esptool-0.4.4-linux64.tar.gz",
-           "checksum":"SHA-256:beedf89db0bdce0bf6034232d86edebcfed0966ff1501545aca9cfbba1f92593",
-           "size":"12513"
+            "host":"x86_64-pc-linux-gnu",
+            "url":"https://github.com/igrr/esptool-ck/releases/download/0.4.4/esptool-0.4.4-linux64.tar.gz",
+            "archiveFileName":"esptool-0.4.4-linux64.tar.gz",
+            "checksum":"SHA-256:beedf89db0bdce0bf6034232d86edebcfed0966ff1501545aca9cfbba1f92593",
+            "size":"12513"
+        },
+        {
+        	"host":"i686-pc-linux-gnu",
+        	"url":"https://github.com/igrr/esptool-ck/releases/download/0.4.4/esptool-0.4.4-linux32.tar.gz",
+            "archiveFileName":"esptool-0.4.4-linux32.tar.gz",
+            "checksum":"SHA-256:4aa81b97a470641771cf371e5d470ac92d3b177adbe8263c4aae66e607b67755",
+            "size":"12044"	
         }
       ]
     },
@@ -110,6 +117,13 @@ cat << EOF > package_esp8266com_index.json
            "archiveFileName":"linux64-xtensa-lx106-elf-gb404fb9.tar.gz",
            "checksum":"SHA-256:46f057fbd8b320889a26167daf325038912096d09940b2a95489db92431473b7",
            "size":"30262903"
+        },
+        {
+           "host":"i686-pc-linux-gnu",
+           "url":"http://arduino.esp8266.com/linux32-xtensa-lx106-elf.tar.gz",
+           "archiveFileName":"linux32-xtensa-lx106-elf.tar.gz",
+           "checksum":"SHA-256:b24817819f0078fb05895a640e806e0aca9aa96b47b80d2390ac8e2d9ddc955a",
+           "size":"32734156"
         }
       ]
     } ]
