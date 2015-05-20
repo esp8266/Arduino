79b374e4dbeb7611edf332d04db0e26f44be7ad1
Markus Sattler <help.markus+git@gmail.com>
2015-05-20 18:57:42 +0200

    add Hash library currently supports SHA1

---
+++ hardware/esp8266com/esp8266/libraries/Hash/examples/sha1.ino
@@ -0,0 +1,26 @@
+/**
+ * simple demo to show sha1 calculation
+ */
+#include <Arduino.h>
+#include <hash.h>
+
+void setup() {
+    Serial.begin(921600);
+}
+
+void loop() {
+    uint8_t hash[20];
+    const uint8_t test[] = "test";
+
+    sha1((uint8_t *)&test[0], sizeof(test)-1, &hash[0]);
+
+    // SHA1: A94A8FE5CCB19BA61C4C0873D391E987982FBBD3
+    Serial.print("SHA1:");
+    for(uint16_t i = 0; i < 20; i++) {
+        Serial.printf("%02X", hash[i]);
+    }
+    Serial.println();
+
+    delay(1000);
+}
+
