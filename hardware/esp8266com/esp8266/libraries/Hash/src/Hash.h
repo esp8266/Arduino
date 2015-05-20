2d28e8ad44ce0fe742b04cf9d67f9de616fbf631
Markus Sattler <help.markus+git@gmail.com>
2015-05-20 19:48:24 +0200

    code style

--- hardware/esp8266com/esp8266/libraries/Hash/src/Hash.h
+++ hardware/esp8266com/esp8266/libraries/Hash/src/Hash.h
@@ -31,7 +31,7 @@ void sha1(uint8_t * data, uint32_t size, uint8_t hash[20]);
 void sha1(char * data, uint32_t size, uint8_t hash[20]);
 void sha1(const uint8_t * data, uint32_t size, uint8_t hash[20]);
 void sha1(const char * data, uint32_t size, uint8_t hash[20]);
-void sha1(String data , uint8_t hash[20]);
+void sha1(String data, uint8_t hash[20]);
 
 String sha1(uint8_t* data, uint32_t size);
 String sha1(char* data, uint32_t size);
