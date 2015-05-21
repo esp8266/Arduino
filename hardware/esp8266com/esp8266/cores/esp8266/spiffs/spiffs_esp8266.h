3178e06df6492b444fe97772d23db4f2a1a6e37f
ficeto <ficeto@asterisk-servers-3.networx-bg.com>
2015-05-22 02:45:29 +0300

    clean unused defines and typos

--- hardware/esp8266com/esp8266/cores/esp8266/spiffs/spiffs_esp8266.h
+++ hardware/esp8266com/esp8266/cores/esp8266/spiffs/spiffs_esp8266.h
@@ -24,7 +24,6 @@ The small 4KB sectors allow for greater flexibility in applications that require
 #define SPIFFS_API_DBG_V(fmt, ...) //os_printf(fmt, ##__VA_ARGS__)
 #define SPIFFS_API_DBG_E(fmt, ...) //os_printf("ERROR: " fmt , ##__VA_ARGS__)
 
-#define INTERNAL_FLASH_PAGE_SIZE        256
 #define INTERNAL_FLASH_SECTOR_SIZE      4096
 #define INTERNAL_FLASH_START_ADDRESS    0x40200000
 
@@ -34,7 +33,6 @@ The small 4KB sectors allow for greater flexibility in applications that require
 extern uint32_t flashmem_write( const void *from, uint32_t toaddr, uint32_t size );
 extern uint32_t flashmem_read( void *to, uint32_t fromaddr, uint32_t size );
 extern bool flashmem_erase_sector( uint32_t sector_id );
-uint32_t flashmem_find_sector( uint32_t address, uint32_t *pstart, uint32_t *pend );
 uint32_t flashmem_get_sector_of_address( uint32_t addr );
 
 #ifdef __cplusplus
