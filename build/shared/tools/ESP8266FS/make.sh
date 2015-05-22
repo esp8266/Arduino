35a26d18f46b4e86031ef7e7a9dfe4b937f1d76b
Ivan Grokhotkov <igrokhotkov@gmail.com>
2015-05-22 18:40:21 +0300

    Update make script for ESP8266FS tool

--- build/shared/tools/ESP8266FS/make.sh
+++ build/shared/tools/ESP8266FS/make.sh
@@ -1,10 +1,30 @@
 #!/bin/sh
-ALIBDIR="/Users/ficeto/Documents/Arduino"
-mkdir -p bin && \
-javac -target 1.8 -cp "../../arduino-core.jar:../../pde.jar" -d bin src/ESP8266FS.java && \
-cd bin && \
-mkdir -p $ALIBDIR/tools && \
-rm -rf $ALIBDIR/tools/ESP8266FS && \
-mkdir -p $ALIBDIR/tools/ESP8266FS/tool && \
-zip -r $ALIBDIR/tools/ESP8266FS/tool/esp8266fs.jar * && \
-cd ..
+
+if [[ -z "$INSTALLDIR" ]]; then
+    INSTALLDIR="$HOME/Documents/Arduino"
+fi
+echo "INSTALLDIR: $INSTALLDIR"
+
+pde_path=`find ../../.. -name pde.jar`
+core_path=`find ../../.. -name arduino-core.jar`
+if [[ -z "$core_path" || -z "$pde_path" ]]; then
+    echo "Some java libraries have not been built yet (did you run ant build?)"
+    return 1
+fi
+
+set -e
+
+mkdir -p bin
+javac -target 1.7 -cp "$pde_path:$core_path" \
+      -d bin src/ESP8266FS.java
+
+pushd bin
+mkdir -p $INSTALLDIR/tools
+rm -rf $INSTALLDIR/tools/ESP8266FS
+mkdir -p $INSTALLDIR/tools/ESP8266FS/tool
+zip -r $INSTALLDIR/tools/ESP8266FS/tool/esp8266fs.jar *
+popd
+
+#pushd $INSTALLDIR/tools
+#zip -r ESP8266FS-$(git describe --tags).zip ESP8266FS/
+#popd
