06caa7f1991cc369eec5afcdea0f6f52d1acb92f
Ivan Grokhotkov <igrokhotkov@gmail.com>
2015-05-23 12:48:56 +0300

    Update SDK to 1.1.0 (#306)

--- hardware/esp8266com/esp8266/tools/sdk/include/osapi.h
+++ hardware/esp8266com/esp8266/tools/sdk/include/osapi.h
@@ -54,5 +54,8 @@ extern int os_printf_plus(const char * format, ...) __attribute__ ((format (prin
 #define os_printf	os_printf_plus
 #endif
 
+unsigned long os_random(void);
+int os_get_random(unsigned char *buf, size_t len);
+
 #endif
 
