01d0f6142936a91bc8d8a8fd9a93590c175eb45e
Ivan Grokhotkov <igrokhotkov@gmail.com>
2015-05-23 12:48:56 +0300

    Update SDK to 1.1.0 (#306)

--- tools/sdk/include/smartconfig.h
+++ tools/sdk/include/smartconfig.h
@@ -6,13 +6,10 @@
 #ifndef __SMARTCONFIG_H__
 #define __SMARTCONFIG_H__
 
-typedef void (*sc_callback_t)(void *data);
-
 typedef enum {
     SC_STATUS_WAIT = 0,
     SC_STATUS_FIND_CHANNEL,
     SC_STATUS_GETTING_SSID_PSWD,
-    SC_STATUS_GOT_SSID_PSWD,
     SC_STATUS_LINK,
     SC_STATUS_LINK_OVER,
 } sc_status;
@@ -22,7 +19,8 @@ typedef enum {
     SC_TYPE_AIRKISS,
 } sc_type;
 
-sc_status smartconfig_get_status(void);
+typedef void (*sc_callback_t)(sc_status status, void *pdata);
+
 const char *smartconfig_get_version(void);
 bool smartconfig_start(sc_type type, sc_callback_t cb, ...);
 bool smartconfig_stop(void);
