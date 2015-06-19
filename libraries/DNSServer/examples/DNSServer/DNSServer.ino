bcdb580d7aa8e11a039a55c61d7df5ef36d19dd2
Kristijan Novoselic <kristijan@glooko.com>
2015-06-19 19:59:54 +0200

    Added simple HTTP server to DNSServer example

--- libraries/DNSServer/examples/DNSServer/DNSServer.ino
+++ libraries/DNSServer/examples/DNSServer/DNSServer.ino
@@ -1,9 +1,11 @@
 #include <ESP8266WiFi.h>
 #include <DNSServer.h>
+#include <ESP8266WebServer.h>
 
 const byte DNS_PORT = 53;
 IPAddress apIP(192, 168, 1, 1);
 DNSServer dnsServer;
+ESP8266WebServer webServer(80);
 
 void setup() {
   WiFi.mode(WIFI_AP);
@@ -21,8 +23,19 @@ void setup() {
 
   //start DNS server for a specific domain name
   dnsServer.start(DNS_PORT, "www.example.com", apIP);
+
+  //simple HTTP server to see that DNS server is working
+  webServer.onNotFound([]() {
+    String message = "Hello World!\n\n";
+    message += "URI: ";
+    message += webServer.uri();
+
+    webServer.send(200, "text/plain", message);
+  });
+  webServer.begin();
 }
 
 void loop() {
   dnsServer.processNextRequest();
+  webServer.handleClient();
 }
