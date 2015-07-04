4fdba1b635871611fcb13b6baf4908b80b7cb70c
John Doe <ficeto@Hristos-Mac-mini-2.local>
2015-07-04 22:55:48 +0300

    Add SSDP Library and let Print::printf to handle longer strings

---
+++ libraries/ESP8266SSDP/ESP8266SSDP.h
@@ -0,0 +1,100 @@
+/*
+ESP8266 Simple Service Discovery
+Copyright (c) 2015 Hristo Gochkov
+
+Original (Arduino) version by Filippo Sallemi, July 23, 2014.
+Can be found at: https://github.com/nomadnt/uSSDP
+
+License (MIT license):
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+
+*/
+
+#ifndef ESP8266SSDP_H
+#define ESP8266SSDP_H
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+#define DEBUG_SSDP  Serial
+
+#define SSDP_BASE_SIZE              24
+#define SSDP_FRIENDLY_NAME_SIZE     32
+#define SSDP_SERIAL_NUMBER_SIZE     32
+#define SSDP_PRESENTATION_URL_SIZE  32
+#define SSDP_MODEL_NAME_SIZE        32
+#define SSDP_MODEL_URL_SIZE         32
+#define SSDP_MODEL_VERSION_SIZE     2
+#define SSDP_MANUFACTURER_SIZE      32
+#define SSDP_MANUFACTURER_URL_SIZE  32
+
+typedef enum {
+  NONE,
+  SEARCH,
+  NOTIFY
+} ssdp_method_t;
+
+typedef struct {
+  uint8_t major;
+  uint8_t minor;
+} ssdp_version_t;
+
+
+class SSDPClass{
+  public:
+    SSDPClass();
+    ~SSDPClass();
+
+    void begin(char *base);
+    uint8_t update();
+    void send(ssdp_method_t method);
+    void schema(WiFiClient client);
+
+    void setName(char *name);
+    void setURL(char *url);
+    void setSerialNumber(char *serialNumber);
+    void setModelName(char *name);
+    void setModelNumber(uint8_t major, uint8_t minor);
+    void setModelURL(char *url);
+    void setManufacturer(char *name);
+    void setManufacturerURL(char *url);
+    
+  private:
+    WiFiUDP _server;
+    bool _pending;
+    unsigned short _delay;
+    unsigned long _process_time;
+    unsigned long _notify_time;
+    
+    uint8_t *_mac;
+    char *_base;
+    char *_friendlyName;
+    char *_serialNumber;
+    char *_presentationURL;
+    char *_manufacturer;
+    char *_manufacturerURL;
+    char *_modelName;
+    char *_modelURL;
+    ssdp_version_t *_modelNumber;
+};
+
+extern SSDPClass SSDP;
+
+#endif
---
+++ libraries/ESP8266SSDP/examples/SSDP.ino
@@ -0,0 +1,57 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUDP.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266SSDP.h>
+
+const char* ssid = "************";
+const char* password = "***********";
+
+ESP8266WebServer HTTP(80);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Booting Sketch...");
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if(WiFi.waitForConnectResult() == WL_CONNECTED){
+    HTTP.on("/", HTTP_GET, [](){
+      HTTP.sendHeader("Connection", "close");
+      HTTP.sendHeader("Access-Control-Allow-Origin", "*");
+      HTTP.send(200, "text/plain", "Hello World!");
+    });
+    HTTP.on("/ssdp/schema.xml", HTTP_GET, [](){
+      SSDP.schema(HTTP.client());
+    });
+    HTTP.begin();
+    
+    byte mac[6];
+    char base[24];
+    WiFi.macAddress(mac);
+    sprintf(base, "esp8266x-%02x%02x-%02x%02x-%02x%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  
+    Serial.printf("Starting SSDP: %s\n", base);
+    
+    SSDP.begin((char*)base);
+    SSDP.setName((char*)"ESP8266");
+    SSDP.setSerialNumber((char*)"A0123456789");
+    SSDP.setURL((char*)"/");
+    SSDP.setModelName((char*)"ESP-12e");
+    SSDP.setModelNumber(1, 0);
+    SSDP.setModelURL((char*)"http://12e.espressif.com");
+    SSDP.setManufacturer((char*)"Espressif");
+    SSDP.setManufacturerURL((char*)"http://espressif.com");
+    
+    Serial.printf("Ready!\n");
+  } else {
+    Serial.printf("WiFi Failed\n");
+    while(1) delay(100);
+  }
+}
+
+void loop() {
+  HTTP.handleClient();
+  SSDP.update();
+  delay(1);
+}
