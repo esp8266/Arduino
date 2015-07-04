4e9b9b3d1dd1518ace397e3a95502f929bb78b92
momo96 <mohamed.elhariry.mail@gmail.com>
2015-07-05 00:54:09 +0200

    Added a sketch that connects the ESP8266 to a WPA network and prints MAC address, IP and SSID

---
+++ hardware/esp8266com/esp8266/libraries/ESP8266WiFi/examples/ConnectWithWPA/ConnectWithWpa.ino
@@ -0,0 +1,76 @@
+/*
+ * This example connects to a WPA encrypted Wifi network.
+ * Then it prints the  MAC address of the ESP8266,
+ * the IP address obtained, and other network details.
+ * 
+ *  created 5 July 2015
+ *  by Mohamed Elhariry
+ */
+
+#include <ESP8266WiFi.h>
+
+char ssid[] = "Dlink";
+char password[] = "wireless110249";
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+  delay(10);
+  
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, password);
+
+    // wait 1 second for connection:
+    delay(10000);
+  }
+  
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWifiData();
+
+}
+
+void loop() {
+  // check the network connection once every 10 seconds:
+  delay(10000);
+  printCurrentNet();
+  printWifiData();
+}
+
+void printWifiData() {
+  // print your ESP8266 IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  Serial.print(mac[5], HEX);
+  Serial.print(":");
+  Serial.print(mac[4], HEX);
+  Serial.print(":");
+  Serial.print(mac[3], HEX);
+  Serial.print(":");
+  Serial.print(mac[2], HEX);
+  Serial.print(":");
+  Serial.print(mac[1], HEX);
+  Serial.print(":");
+  Serial.println(mac[0], HEX);
+
+}
+
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  
+}
+
