3c54cb0a26ca797db47a16c0517a4ae38f87642a
John Doe <ficeto@Hristos-Mac-mini-2.local>
2015-07-05 01:36:43 +0300

    generate UUID automatically based on chip ID and MAC address

--- libraries/ESP8266SSDP/examples/SSDP.ino
+++ libraries/ESP8266SSDP/examples/SSDP.ino
@@ -11,29 +11,23 @@ ESP8266WebServer HTTP(80);
 void setup() {
   Serial.begin(115200);
   Serial.println();
-  Serial.println("Booting Sketch...");
+  Serial.println("Starting WiFi...");
   
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   if(WiFi.waitForConnectResult() == WL_CONNECTED){
+  
+    Serial.printf("Starting HTTP...\n");
     HTTP.on("/", HTTP_GET, [](){
-      HTTP.sendHeader("Connection", "close");
-      HTTP.sendHeader("Access-Control-Allow-Origin", "*");
       HTTP.send(200, "text/plain", "Hello World!");
     });
     HTTP.on("/ssdp/schema.xml", HTTP_GET, [](){
       SSDP.schema(HTTP.client());
     });
     HTTP.begin();
-    
-    byte mac[6];
-    char base[24];
-    WiFi.macAddress(mac);
-    sprintf(base, "esp8266x-%02x%02x-%02x%02x-%02x%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
   
-    Serial.printf("Starting SSDP: %s\n", base);
-    
-    SSDP.begin((char*)base);
+    Serial.printf("Starting SSDP...\n");
+    SSDP.begin();
     SSDP.setName((char*)"ESP8266");
     SSDP.setSerialNumber((char*)"A0123456789");
     SSDP.setURL((char*)"/");
