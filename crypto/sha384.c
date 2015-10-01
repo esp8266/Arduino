d1bcdc5f97ae590d798030e90e4df167e41fa36f
olereinhardt <olereinhardt@9a5d90b5-6617-0410-8a86-bb477d3ed2e3>
2015-10-01 15:58:22 +0000

    Tag 64-bit constants with "LL" (make e.g. AVR32 gcc happy)
    git-svn-id: svn://svn.code.sf.net/p/axtls/code/trunk@251 9a5d90b5-6617-0410-8a86-bb477d3ed2e3

--- crypto/sha384.c
+++ crypto/sha384.c
@@ -38,14 +38,14 @@
  void SHA384_Init(SHA384_CTX *ctx)
  {
     //Set initial hash value
-    ctx->h_dig.h[0] = 0xCBBB9D5DC1059ED8;
-    ctx->h_dig.h[1] = 0x629A292A367CD507;
-    ctx->h_dig.h[2] = 0x9159015A3070DD17;
-    ctx->h_dig.h[3] = 0x152FECD8F70E5939;
-    ctx->h_dig.h[4] = 0x67332667FFC00B31;
-    ctx->h_dig.h[5] = 0x8EB44A8768581511;
-    ctx->h_dig.h[6] = 0xDB0C2E0D64F98FA7;
-    ctx->h_dig.h[7] = 0x47B5481DBEFA4FA4;
+    ctx->h_dig.h[0] = 0xCBBB9D5DC1059ED8LL;
+    ctx->h_dig.h[1] = 0x629A292A367CD507LL;
+    ctx->h_dig.h[2] = 0x9159015A3070DD17LL;
+    ctx->h_dig.h[3] = 0x152FECD8F70E5939LL;
+    ctx->h_dig.h[4] = 0x67332667FFC00B31LL;
+    ctx->h_dig.h[5] = 0x8EB44A8768581511LL;
+    ctx->h_dig.h[6] = 0xDB0C2E0D64F98FA7LL;
+    ctx->h_dig.h[7] = 0x47B5481DBEFA4FA4LL;
  
     // Number of bytes in the buffer
     ctx->size = 0;
