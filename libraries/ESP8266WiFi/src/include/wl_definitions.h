ee2d4495fda9c1602b545873bc6d0c14e6ea7ecf
Ivan Grokhotkov <igrokhotkov@gmail.com>
2015-12-22 14:08:17 +0300

    Additional check in wl_definitions.h to guard against duplicate definitions

--- libraries/ESP8266WiFi/src/include/wl_definitions.h
+++ libraries/ESP8266WiFi/src/include/wl_definitions.h
@@ -68,7 +68,7 @@ enum wl_enc_type {  /* Values map to 802.11 encryption suites... */
         ENC_TYPE_AUTO = 8
 };
 
-#if !defined(LWIP_INTERNAL)
+#if !defined(LWIP_INTERNAL) && !defined(__LWIP_TCP_H__)
 enum wl_tcp_state {
   CLOSED      = 0,
   LISTEN      = 1,
