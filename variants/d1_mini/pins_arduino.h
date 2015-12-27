16244c5dfd694e9868384041e43f2b70be7a2318
wemos <11569115@qq.com>
2015-12-27 14:36:22 +0800

    fix d1_mini board.

--- variants/d1_mini/pins_arduino.h
+++ variants/d1_mini/pins_arduino.h
@@ -55,10 +55,10 @@ static const uint8_t D5   = 14;
 static const uint8_t D6   = 12;
 static const uint8_t D7   = 13;
 static const uint8_t D8   = 15;
-static const uint8_t D9   = 3;
-static const uint8_t D10  = 1;
+static const uint8_t RX   = 3;
+static const uint8_t TX   = 1;
 
-static const uint8_t D13  = 2;
+
 
 // These serial port names are intended to allow libraries and architecture-neutral
 // sketches to automatically default to the correct port name for a particular type
