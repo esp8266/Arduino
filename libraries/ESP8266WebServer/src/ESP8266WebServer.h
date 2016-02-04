94b3f415f24e1de0f4196a9db0e23c84a66ca5d9
Ivan Grokhotkov <igrokhotkov@gmail.com>
2016-02-04 16:35:21 +0300

    ESP8266WebServer: set write timeout before sending content

--- libraries/ESP8266WebServer/src/ESP8266WebServer.h
+++ libraries/ESP8266WebServer/src/ESP8266WebServer.h
@@ -34,6 +34,7 @@ enum HTTPClientStatus { HC_NONE, HC_WAIT_READ, HC_WAIT_CLOSE };
 #define HTTP_DOWNLOAD_UNIT_SIZE 1460
 #define HTTP_UPLOAD_BUFLEN 2048
 #define HTTP_MAX_DATA_WAIT 1000 //ms to wait for the client to send the request
+#define HTTP_MAX_SEND_WAIT 5000 //ms to wait for data chunk to be ACKed
 #define HTTP_MAX_CLOSE_WAIT 2000 //ms to wait for the client to close the connection
 
 #define CONTENT_LENGTH_UNKNOWN ((size_t) -1)
