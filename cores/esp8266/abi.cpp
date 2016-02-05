6fc141772c85d33640c32cea83bf7236ca64df7a
Ivan Grokhotkov <igrokhotkov@gmail.com>
2016-02-06 02:01:51 +0300

    Implement static initialization guards (#500)

--- cores/esp8266/abi.cpp
+++ cores/esp8266/abi.cpp
@@ -25,7 +25,10 @@ extern "C" {
 #include "osapi.h"
 #include "mem.h"
 }
+#include <Arduino.h>
+#include <cxxabi.h>
 
+using __cxxabiv1::__guard;
 
 void *operator new(size_t size) {
     size = ((size + 3) & ~((size_t)0x3));
@@ -55,6 +58,34 @@ void __cxa_pure_virtual(void) {
 void __cxa_deleted_virtual(void) {
     panic();
 }
+
+typedef struct {
+    uint8_t guard;
+    uint8_t ps;
+} guard_t;
+
+extern "C" int __cxa_guard_acquire(__guard* pg)
+{
+    uint8_t ps = xt_rsil(15);
+    if (reinterpret_cast<guard_t*>(pg)->guard) {
+        xt_wsr_ps(ps);
+        return 0;
+    }
+    reinterpret_cast<guard_t*>(pg)->ps = ps;
+    return 1;
+}
+
+extern "C" void __cxa_guard_release(__guard* pg)
+{
+    reinterpret_cast<guard_t*>(pg)->guard = 1;
+    xt_wsr_ps(reinterpret_cast<guard_t*>(pg)->ps);
+}
+
+extern "C" void __cxa_guard_abort(__guard* pg)
+{
+    xt_wsr_ps(reinterpret_cast<guard_t*>(pg)->ps);
+}
+
 
 namespace std {
 void __throw_bad_function_call() {
