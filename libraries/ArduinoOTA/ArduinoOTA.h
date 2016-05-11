c1ed77d55e631d0438a8a784d2938643f17d2351
NullMedia <simon@nullmedia.co.uk>
2016-05-11 07:53:57 +0100

    Modified callbacks to enable lambda capture

--- libraries/ArduinoOTA/ArduinoOTA.h
+++ libraries/ArduinoOTA/ArduinoOTA.h
@@ -21,13 +21,13 @@ typedef enum {
   OTA_END_ERROR
 } ota_error_t;
 
-typedef std::function<void(void)> THandlerFunction;
-typedef std::function<void(ota_error_t)> THandlerFunction_Error;
-typedef std::function<void(unsigned int, unsigned int)> THandlerFunction_Progress;
-
 class ArduinoOTAClass
 {
   public:
+	typedef std::function<void(void)> THandlerFunction;
+	typedef std::function<void(ota_error_t)> THandlerFunction_Error;
+	typedef std::function<void(unsigned int, unsigned int)> THandlerFunction_Progress;
+
     ArduinoOTAClass();
     ~ArduinoOTAClass();
     void setPort(uint16_t port);
