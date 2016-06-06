564019c4f27a45f04db8c370082ce451bf1943fe
Me No Dev <me-no-dev@vdnsbg.com>
2016-06-06 14:35:02 +0300

    fix strings
    interesting, the meta did not refresh if the successResponse is put in
    "R"

--- libraries/ESP8266HTTPUpdateServer/src/ESP8266HTTPUpdateServer.cpp
+++ libraries/ESP8266HTTPUpdateServer/src/ESP8266HTTPUpdateServer.cpp
@@ -12,8 +12,8 @@ R"(<html><body><form method='POST' action='' enctype='multipart/form-data'>
                   <input type='submit' value='Update'>
                </form>
          </body></html>)";
-const char* ESP8266HTTPUpdateServer::_failedResponse = R("Update Failed!");
-const char* ESP8266HTTPUpdateServer::_successResponse = R("<META http-equiv=\"refresh\" content=\"15;URL=\">Update Success! Rebooting...");
+const char* ESP8266HTTPUpdateServer::_failedResponse = R"(Update Failed!)";
+const char* ESP8266HTTPUpdateServer::_successResponse = "<META http-equiv=\"refresh\" content=\"15;URL=\">Update Success! Rebooting...";
 
 ESP8266HTTPUpdateServer::ESP8266HTTPUpdateServer(bool serial_debug)
 {
