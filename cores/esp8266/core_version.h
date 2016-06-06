b40dac9dce182e585ce6ccc9a83a2dede8f5c9ed
Ivan Grokhotkov <igrr@users.noreply.github.com>
2016-06-06 14:37:39 +0800

    Store git version of the core in the compiled binary (#2099)
    * Store git version of the core in the compiled binary
    
    * Don't update version number when using boards manager package

---
+++ cores/esp8266/core_version.h
@@ -0,0 +1,6 @@
+#define ARDUINO_ESP8266_GIT_VER 0x00000000
+
+// ARDUINO_ESP8266_RELEASE is defined for released versions as a string containing the version name, i.e. "2_3_0_RC1"
+// ARDUINO_ESP8266_RELEASE is used in the core internally. Please use ESP.getCoreVersion() function instead.
+
+// ARDUINO_ESP8266_RELEASE_<version number> are defined for releases, for use in #ifdef... constructs
