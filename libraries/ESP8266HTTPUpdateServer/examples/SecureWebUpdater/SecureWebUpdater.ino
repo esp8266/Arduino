12200a868606a6a6c235f794652028e30bf9eb60
Me No Dev <me-no-dev@users.noreply.github.com>
2016-06-07 05:09:05 +0300

    make HTTP Update Server more secure (#2104)
    * make HTTP Update Server more secure
    
    * added option for authentication
    * added option to change the url for upload
    
    * move to overloaded setup
    
    * remove delay in both examples
    
    * Get better result responses
    
    * fix strings
    
    interesting, the meta did not refresh if the successResponse is put in
    "R"

--- libraries/ESP8266HTTPUpdateServer/examples/SecureWebUpdater/SecureWebUpdater.ino
+++ libraries/ESP8266HTTPUpdateServer/examples/SecureWebUpdater/SecureWebUpdater.ino
@@ -9,6 +9,9 @@
 #include <ESP8266HTTPUpdateServer.h>
 
 const char* host = "esp8266-webupdate";
+const char* update_path = "/firmware";
+const char* update_username = "admin";
+const char* update_password = "admin";
 const char* ssid = "........";
 const char* password = "........";
 
@@ -30,11 +33,11 @@ void setup(void){
 
   MDNS.begin(host);
 
-  httpUpdater.setup(&httpServer);
+  httpUpdater.setup(&httpServer, update_path, update_username, update_password);
   httpServer.begin();
 
   MDNS.addService("http", "tcp", 80);
-  Serial.printf("HTTPUpdateServer ready! Open http://%s.local/update in your browser\n", host);
+  Serial.printf("HTTPUpdateServer ready! Open http://%s.local%s in your browser and login with username '%s' and password '%s'\n", host, update_path, update_username, update_password);
 }
 
 void loop(void){
