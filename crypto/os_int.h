14d6809c0d59026c18e84a5769d3a13f226b589c
cameronrich <cameronrich@9a5d90b5-6617-0410-8a86-bb477d3ed2e3>
2016-07-05 19:57:29 +0000

    removed endian.h from os_int.h as it is no longer needed and was causing issues with the micropython build
    git-svn-id: svn://svn.code.sf.net/p/axtls/code/trunk@256 9a5d90b5-6617-0410-8a86-bb477d3ed2e3

--- crypto/os_int.h
+++ crypto/os_int.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, Cameron Rich
+ * Copyright (c) 2012-2016, Cameron Rich
  * 
  * All rights reserved.
  * 
@@ -56,7 +56,6 @@ typedef INT64 int64_t;
 #include <inttypes.h>
 #else
 #include <stdint.h>
-#include <endian.h>
 #endif /* Not Solaris */
 
 #endif /* Not Win32 */
