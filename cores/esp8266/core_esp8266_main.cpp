ec593722624ebce3c047ba8ce9133b5b5b68c723
Alexander Sologub <alexander.sologub@rightscale.com>
2016-07-23 13:54:50 +0300

    Update SDK to 2.0.0 (#2304)

--- cores/esp8266/core_esp8266_main.cpp
+++ cores/esp8266/core_esp8266_main.cpp
@@ -43,7 +43,7 @@ struct rst_info resetInfo;
 
 extern "C" {
 extern const uint32_t __attribute__((section(".ver_number"))) core_version = ARDUINO_ESP8266_GIT_VER;
-const char* core_release = 
+const char* core_release =
 #ifdef ARDUINO_ESP8266_RELEASE
     ARDUINO_ESP8266_RELEASE;
 #else
@@ -170,4 +170,35 @@ extern "C" void user_init(void) {
         LOOP_QUEUE_SIZE);
 
     system_init_done_cb(&init_done);
+}
+
+extern "C" uint32 ICACHE_FLASH_ATTR user_rf_cal_sector_set(void) {
+    enum flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+
+    switch (size_map) {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 5;
+            break;
+
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            break;
+
+        case FLASH_SIZE_16M_MAP_512_512:
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            break;
+
+        case FLASH_SIZE_32M_MAP_512_512:
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            break;
+
+        default:
+            rf_cal_sec = 0;
+            break;
+    }
+
+    return rf_cal_sec;
 }
--- tools/sdk/include/c_types.h
+++ tools/sdk/include/c_types.h
@@ -10,10 +10,17 @@
 #include <stdarg.h>
 #include <sys/cdefs.h>
 
+typedef unsigned char       uint8_t;
 typedef signed char         sint8_t;
+typedef signed char         int8_t;
+typedef unsigned short      uint16_t;
 typedef signed short        sint16_t;
+typedef signed short        int16_t;
+typedef unsigned int        uint32_t;
 typedef signed long         sint32_t;
+typedef signed int          int32_t;
 typedef signed long long    sint64_t;
+typedef unsigned long long  uint64_t;
 typedef unsigned long long  u_int64_t;
 typedef float               real32_t;
 typedef double              real64_t;
@@ -40,6 +47,8 @@ typedef float               real32;
 typedef double              real64;
 
 #define __le16      u16
+
+typedef unsigned int        size_t;
 
 #define LOCAL       static
 
