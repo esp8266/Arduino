b8b7eace976ebaeb35030f931371a42e0341f305
Karl Palsson <karlp@tweak.net.au>
2016-11-14 23:23:22 +0000

    lib/HTTPUpdateServer: callbacks for events
    Provide basic callbacks for update start, end, error and progress.
    Progress callback provides as much information as we can, but because
    the filehandler processs as it receives data, we don't always know the
    entire file size in the first call.
    
    Signed-off-by: Karl Palsson <karlp@tweak.net.au>

--- libraries/ESP8266HTTPUpdateServer/src/ESP8266HTTPUpdateServer.cpp
+++ libraries/ESP8266HTTPUpdateServer/src/ESP8266HTTPUpdateServer.cpp
@@ -60,10 +60,12 @@ void ESP8266HTTPUpdateServer::setup(ESP8266WebServer *server, const char * path,
             if (_serial_output) {
                 Serial.setDebugOutput(true);
             }
+            handle_start();
 
             _authenticated = (_username == NULL || _password == NULL || _server->authenticate(_username, _password));
             if (!_authenticated) {
                 debug("Unauthenticated Update\n");
+                handle_error(1);
                 return;
             }
 
@@ -73,21 +75,28 @@ void ESP8266HTTPUpdateServer::setup(ESP8266WebServer *server, const char * path,
             if (!Update.begin(maxSketchSpace)) {//start with max available size
                 if (_serial_output) {
                     Update.printError(Serial);
+                    handle_error(2);
                 }
             }
+            handle_progress(0, upload.totalSize);
         } else if (_authenticated && upload.status == UPLOAD_FILE_WRITE) {
             debug(".");
             if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
                 if (_serial_output) {
                     Update.printError(Serial);
+                    handle_error(3);
                 }
             }
+            handle_progress(upload.currentSize, upload.totalSize);
         } else if (_authenticated && upload.status == UPLOAD_FILE_END) {
             if (Update.end(true)) { //true to set the size to the current progress
                 debug("Update Success: %u\nRebooting...\n", upload.totalSize);
+                handle_progress(upload.totalSize, upload.totalSize);
+                handle_end();
             } else {
                 if (_serial_output) {
                     Update.printError(Serial);
+                    handle_error(4);
                 }
             }
             if (_serial_output) {
@@ -96,7 +105,56 @@ void ESP8266HTTPUpdateServer::setup(ESP8266WebServer *server, const char * path,
         } else if (_authenticated && upload.status == UPLOAD_FILE_ABORTED) {
             Update.end();
             debug("Update was aborted");
+            handle_error(5);
         }
         delay(0);
     });
+}
+
+void ESP8266HTTPUpdateServer::onStart(THandlerFunction fn)
+{
+    _cb_start = fn;
+}
+
+void ESP8266HTTPUpdateServer::onEnd(THandlerFunction fn)
+{
+    _cb_end = fn;
+}
+
+void ESP8266HTTPUpdateServer::onError(THandlerFunction_Error fn)
+{
+    _cb_error = fn;
+}
+
+void ESP8266HTTPUpdateServer::onProgress(THandlerFunction_Progress fn)
+{
+    _cb_progress = fn;
+}
+
+void ESP8266HTTPUpdateServer::handle_start()
+{
+    if (_cb_start) {
+        _cb_start();
+    }
+}
+
+void ESP8266HTTPUpdateServer::handle_end()
+{
+    if (_cb_end) {
+        _cb_end();
+    }
+}
+
+void ESP8266HTTPUpdateServer::handle_error(int err)
+{
+    if (_cb_error) {
+        _cb_error(err);
+    }
+}
+
+void ESP8266HTTPUpdateServer::handle_progress(unsigned int i, unsigned int j)
+{
+    if (_cb_progress) {
+        _cb_progress(i, j);
+    }
 }
--- libraries/ESP8266HTTPUpdateServer/src/ESP8266HTTPUpdateServer.h
+++ libraries/ESP8266HTTPUpdateServer/src/ESP8266HTTPUpdateServer.h
@@ -1,13 +1,18 @@
 #ifndef __HTTP_UPDATE_SERVER_H
 #define __HTTP_UPDATE_SERVER_H
 
+#include <functional>
+
 class ESP8266WebServer;
 
 class ESP8266HTTPUpdateServer
 {
 public:
-    ESP8266HTTPUpdateServer(bool serial_debug = false);
+    typedef std::function<void(void)> THandlerFunction;
+    typedef std::function<void(int)> THandlerFunction_Error; // fixme enum?
+    typedef std::function<void(unsigned int chunk, unsigned int total)> THandlerFunction_Progress;
 
+    ESP8266HTTPUpdateServer(bool serial_debug = false);
     void setup(ESP8266WebServer *server)
     {
         setup(server, NULL, NULL);
@@ -25,6 +30,19 @@ public:
 
     void setup(ESP8266WebServer *server, const char * path, const char * username, const char * password);
 
+    //This callback will be called when an update starts
+    void onStart(THandlerFunction fn);
+
+    //This callback will be called when an update has finished
+    void onEnd(THandlerFunction fn);
+
+    //This callback will be called when an update encounters any error
+    void onError(THandlerFunction_Error fn);
+
+    //This callback will be called when an update is under way
+    void onProgress(THandlerFunction_Progress fn);
+
+
 private:
     bool _serial_output;
     ESP8266WebServer *_server;
@@ -34,9 +52,19 @@ private:
     char * _username;
     char * _password;
     bool _authenticated;
+
+    THandlerFunction _cb_start;
+    THandlerFunction _cb_end;
+    THandlerFunction_Error _cb_error;
+    THandlerFunction_Progress _cb_progress;
 protected:
     template<class ...Ts>
     void debug(const char *fmt, Ts... args);
+    void handle_start();
+    void handle_end();
+    void handle_error(int);
+    void handle_progress(unsigned int, unsigned int);
+
 };
 
 
