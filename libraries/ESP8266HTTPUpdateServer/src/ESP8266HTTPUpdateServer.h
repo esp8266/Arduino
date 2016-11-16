c02e72d47f159228db479e866b055e8821273dae
Karl Palsson <karlp@tweak.net.au>
2016-11-16 21:48:08 +0000

    lib/HTTPUpdateServer: whitespace and braces
    No semantic change.
    Added {} for all single line ifs.  Avoids future mistakes.
    Consistently use 4 spaces for all indents, instead of a mix of 2 and 4.
    This makes it more inline with other libraries, and easier on the eyes
    for newcomers to the codebase.
    
    Signed-off-by: Karl Palsson <karlp@tweak.net.au>

--- libraries/ESP8266HTTPUpdateServer/src/ESP8266HTTPUpdateServer.h
+++ libraries/ESP8266HTTPUpdateServer/src/ESP8266HTTPUpdateServer.h
@@ -5,27 +5,27 @@ class ESP8266WebServer;
 
 class ESP8266HTTPUpdateServer
 {
-  public:
-    ESP8266HTTPUpdateServer(bool serial_debug=false);
+public:
+    ESP8266HTTPUpdateServer(bool serial_debug = false);
 
     void setup(ESP8266WebServer *server)
     {
-      setup(server, NULL, NULL);
+        setup(server, NULL, NULL);
     }
 
     void setup(ESP8266WebServer *server, const char * path)
     {
-      setup(server, path, NULL, NULL);
+        setup(server, path, NULL, NULL);
     }
 
     void setup(ESP8266WebServer *server, const char * username, const char * password)
     {
-      setup(server, "/update", username, password);
+        setup(server, "/update", username, password);
     }
 
     void setup(ESP8266WebServer *server, const char * path, const char * username, const char * password);
 
-  private:
+private:
     bool _serial_output;
     ESP8266WebServer *_server;
     static const char *_serverIndex;
