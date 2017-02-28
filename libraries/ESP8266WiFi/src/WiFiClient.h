bd1c7ce1dc4f56d707637f2829f6473e888e533d
Earle F. Philhower, III <earlephilhower@yahoo.com>
2017-02-27 20:17:04 -0800

    Add SSL enabled WiFiServer, Updater, WebServer
    Adds SSL server mode for WiFiServerSecure, for plain SSL connections,
    ESP8266WebServerSecure, for HTTPS web serving, and SecureHTTPSUpdater for
    encrypted OTA updates.
    
    Example code is provided for all new options, as well as a BASH script for
    generating their own, self-signed certificates.
    
    Both ESP8266WebServerSecure and SecureHTTPSUpdater are important for secure
    password-based authentication.  HTTP Basic Authentication, the only supported
    model presently, sends the username and password in *cleartext* and therefore
    should *never* be used in any un-SSL encrypted channel unless you don't mind
    sharing your login and password with anyone else on the internet.  Even if the
    ESP8266 is not safety critical, this cleartext broadcast could expose you should
    you reuse this password elsewhere on your network or the internet.

--- libraries/ESP8266WiFi/src/WiFiClient.h
+++ libraries/ESP8266WiFi/src/WiFiClient.h
@@ -53,7 +53,7 @@ public:
   virtual int connect(const String host, uint16_t port);
   virtual size_t write(uint8_t);
   virtual size_t write(const uint8_t *buf, size_t size);
-  size_t write_P(PGM_P buf, size_t size);
+  virtual size_t write_P(PGM_P buf, size_t size);
   size_t write(Stream& stream);
 
   // This one is deprecated, use write(Stream& instead)
