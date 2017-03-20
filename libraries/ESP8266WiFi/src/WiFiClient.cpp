158b04ccbf680fcdbb2f470982f97b3ed9765539
David Gauchard <gauchard@laas.fr>
2017-03-20 14:56:53 +0100

    minimum changes for libraries to compile with lwip2

--- libraries/ESP8266WiFi/src/WiFiClient.cpp
+++ libraries/ESP8266WiFi/src/WiFiClient.cpp
@@ -106,11 +106,13 @@ int WiFiClient::connect(IPAddress ip, uint16_t port)
     // if the default interface is down, tcp_connect exits early without
     // ever calling tcp_err
     // http://lists.gnu.org/archive/html/lwip-devel/2010-05/msg00001.html
+#if LWIP_VERSION_MAJOR == 1
     netif* interface = ip_route(&addr);
     if (!interface) {
         DEBUGV("no route to host\r\n");
         return 0;
     }
+#endif
 
     tcp_pcb* pcb = tcp_new();
     if (!pcb)
