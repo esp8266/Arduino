0dd575994bdd8bbc5051d7108e803b73d4c4c443
salqadri <salqadri@gmail.com>
2017-04-28 06:23:30 -0700

    Added instructions on how to generate certificate dump

--- libraries/ESP8266HTTPClient/examples/SSLCertificateVerification/SSLCertificateVerification.ino
+++ libraries/ESP8266HTTPClient/examples/SSLCertificateVerification/SSLCertificateVerification.ino
@@ -1,3 +1,27 @@
+/**
+ * SSLCertificateVerification.ino
+ *
+ * Created on: 04.28.2017
+ *
+ * Copyright (c) 2017 Syed Salman Qadri. All rights reserved.
+ * This file is part of the ESP8266HTTPClient for Arduino.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <time.h>
@@ -6,6 +30,17 @@
 const char wifissid[] = "WiFi";
 const char wifipsk[] = "Password";
 
+// This is a binary dump of the root CA.
+// You can also choose to load the cert via a file stream.
+// Here are the steps I did to dump the CA on my Mac:
+//   1) openssl s_client -connect google.com:443
+//        The output has a "Certificate chain" section. Your root CA is the bottom line.
+//        In the case of google.com, this line is
+//          "i:/C=US/O=Equifax/OU=Equifax Secure Certificate Authority"
+//        This means I need to validate with the Equifax Secure CA
+//   2) Open Keychain Access. In the Search bar type "Equifax" and press Enter.
+//   3) Right-click the cert and click Export and save it as a .cer.
+//   4) In the terminal, type xxd -i <path_to_cer_file>
 unsigned char Equifax_cer[] = {
   0x30, 0x82, 0x03, 0x20, 0x30, 0x82, 0x02, 0x89, 0xa0, 0x03, 0x02, 0x01,
   0x02, 0x02, 0x04, 0x35, 0xde, 0xf4, 0xcf, 0x30, 0x0d, 0x06, 0x09, 0x2a,
@@ -97,7 +132,6 @@ void runTest() {
   HTTPClient http;
   http.begin("https://www.google.com", 443);
   http.setRootCA(Equifax_cer, Equifax_cer_len);
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
   int result = http.GET();
   if (result < 0) {
     Serial.printf("Failed to connect. Error code %d\n", result);
