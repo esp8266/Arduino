2ea36fb765fa5eaa8a1bace601761522c7001f5e
Lan-Hekary <Lan.Hekary@Gmail.com>
2017-09-12 20:16:09 +0200

    update license to public domain

--- libraries/ESP8266WebServer/examples/HttpAdvancedAuth/HttpAdvancedAuth.ino
+++ libraries/ESP8266WebServer/examples/HttpAdvancedAuth/HttpAdvancedAuth.ino
@@ -1,25 +1,7 @@
 /*
-  Http Advanced Authentication example
-  Copyright (c) 2017 Ahmed El-Sharnoby . All rights reserved.
-  This file is part of the ESP8266WebServer library for Arduino environment.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-  Explaining the use of Advanced Authentication Methods to increase Security
-  Recommended to use the Digest Authentication Method as it's less vulnerable
-  than the Basic Method
+  HTTP Advanced Authentication example
+  Created Mar 16, 2017 by Ahmed El-Sharnoby.
+  This example code is in the public domain.
 */
 
 #include <ESP8266WiFi.h>
