d1e8fe99a23c0e4641199f7a5ee3b0b8ca334443
Luc <luc.lebosse@gmail.com>
2017-11-17 04:33:19 +0100

    Fix compilation with LWIP v2 (#3822)

--- libraries/ESP8266SSDP/ESP8266SSDP.cpp
+++ libraries/ESP8266SSDP/ESP8266SSDP.cpp
@@ -201,7 +201,7 @@ bool SSDPClass::begin(){
 
 void SSDPClass::_send(ssdp_method_t method){
   char buffer[1460];
-  uint32_t ip = WiFi.localIP();
+  IPAddress ip = WiFi.localIP();
 
   char valueBuffer[strlen_P(_ssdp_notify_template)+1];
   strcpy_P(valueBuffer, (method == NONE)?_ssdp_response_template:_ssdp_notify_template);
@@ -214,7 +214,7 @@ void SSDPClass::_send(ssdp_method_t method){
     _uuid,
     (method == NONE)?"ST":"NT",
     _deviceType,
-    IP2STR(&ip), _port, _schemaURL
+   ip[0], ip[1], ip[2], ip[3], _port, _schemaURL
   );
 
   _server->append(buffer, len);
@@ -244,11 +244,11 @@ void SSDPClass::_send(ssdp_method_t method){
 }
 
 void SSDPClass::schema(WiFiClient client){
-  uint32_t ip = WiFi.localIP();
+  IPAddress ip = WiFi.localIP();
   char buffer[strlen_P(_ssdp_schema_template)+1];
   strcpy_P(buffer, _ssdp_schema_template);
   client.printf(buffer,
-    IP2STR(&ip), _port,
+    ip[0], ip[1], ip[2], ip[3], _port,
     _deviceType,
     _friendlyName,
     _presentationURL,
