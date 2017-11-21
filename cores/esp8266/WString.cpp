c349b439d4cb81a33a566dc1d3dc11b576cbac0b
Develo <deveyes@gmail.com>
2017-11-21 00:08:22 -0300

    changed counter names, removed else

--- cores/esp8266/WString.cpp
+++ cores/esp8266/WString.cpp
@@ -482,24 +482,26 @@ unsigned char String::equalsConstantTime(const String &s2) const {
     // compares given strings in a constant time.
     if(len != s2.len)
         return 0;
+    //at this point lengths are the same
     if(len == 0)
         return 1;
+    //at this point lenghts are the same and non-zero
     const char *p1 = buffer;
     const char *p2 = s2.buffer;
-    unsigned int u1 = 0;
-    unsigned int u2 = 0;
+    unsigned int equalchars = 0;
+    unsigned int diffchars = 0;
     while(*p1) {
         if(*p1 == *p2)
-            ++u1;
+            ++equalchars;
         else
-            ++u2;
+            ++diffchars;
         ++p1;
         ++p2;
     }
-    if((u1 == len) && (u2 == 0))
-        return 1;
-    else
-        return 0;
+    //the following should force a constant time eval of the condition without a compiler "logical shortcut"
+    unsigned char equalcond = (equalchars == len);
+    unsigned char diffcond = (diffchars == 0);
+    return (equalcond & diffcond); //bitwise AND
 }
 
 unsigned char String::startsWith(const String &s2) const {
