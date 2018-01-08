4b69085aab3c2e89fea20fd1ea136c678fb58ca6
Parham Alvani <parham.alvani@gmail.com>
2018-01-08 10:24:21 +0330

    Reviews digest authentication example.

--- libraries/ESP8266HTTPClient/examples/DigestAuthorization/DigestAuthorization.ino
+++ libraries/ESP8266HTTPClient/examples/DigestAuthorization/DigestAuthorization.ino
@@ -1,117 +1,101 @@
+/*
+
+   This example is released into public domain,
+   or, at your option, CC0 licensed.
+ */
+
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
 
 #include <ESP8266HTTPClient.h>
 
-#define USE_SERIAL Serial
+const char* ssid = "........";
+const char* password = "........";
 
-ESP8266WiFiMulti WiFiMulti;
+const char *username = "admin";
+const char *password = "admin";
 
-String _exractParam(String& authReq, const String& param, const char delimit){
+const char *server = "http://192.168.100.1";
+const char *uri = "/api/state?power=1";
+
+String exractParam(String& authReq, const String& param, const char delimit){
 	int _begin = authReq.indexOf(param);
 	if (_begin==-1) return "";
 	return authReq.substring(_begin+param.length(),authReq.indexOf(delimit,_begin+param.length()));
 }
 
 void setup() {
-
-	USE_SERIAL.begin(9600);
-	// USE_SERIAL.setDebugOutput(true);
-
-	USE_SERIAL.println();
-	USE_SERIAL.println();
-	USE_SERIAL.println();
-
-	for(uint8_t t = 4; t > 0; t--) {
-		USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
-		USE_SERIAL.flush();
-		delay(1000);
-	}
+	Serial.begin(9600);
 
 	WiFi.mode(WIFI_STA);
-	WiFiMulti.addAP("Niligo-Prism-yyr9uma", "");
+	WiFi.begin(ssid, password);
 
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+
+	Serial.println("");
+	Serial.println("WiFi connected");
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
 }
 
 void loop() {
-	// wait for WiFi connection
-	if((WiFiMulti.run() == WL_CONNECTED)) {
+	HTTPClient http;
 
-		HTTPClient http;
+	Serial.print("[HTTP] begin...\n");
 
-		USE_SERIAL.print("[HTTP] begin...\n");
-		// configure traged server and url
-
-
-		// http.begin("http://admin:admin@192.168.100.1/api/state?power=1");
-		http.begin("http://192.168.100.1/api/state?power=0");
-		/*
-		// or
-		// or
-		http.begin("http://192.168.1.12/test.html");
-		http.setAuthorization("dXNlcjpwYXN3b3Jk");
-		 */
+	// configure traged server and url
+	http.begin(String(server) + String(uri));
 
 
-		const char *keys[] = {"WWW-Authenticate", "Server", "Connection"};
-		http.collectHeaders(keys, 3);
+	const char *keys[] = {"WWW-Authenticate"};
+	http.collectHeaders(keys, 1);
 
-		USE_SERIAL.print("[HTTP] GET...\n");
-		// start connection and send HTTP header
+	Serial.print("[HTTP] GET...\n");
+	// start connection and send HTTP header
+	int httpCode = http.GET();
+
+	if (httpCode > 0) {
+		String authReq = http.header("WWW-Authenticate");
+		Serial.println(authReq);
+
+		// extracting required parameters for RFC 2069 simpler Digest
+		String _realm    = _exractParam(authReq, "realm=\"", '"');
+		String _nonce    = _exractParam(authReq, "nonce=\"", '"');
+		String _opaque   = _exractParam(authReq, "opaque=\"", '"');
+
+		// parameters for the RFC 2617 newer Digest
+		MD5Builder md5;
+		md5.begin();
+		md5.add(String(username) + ":" + _realm + ":" + String(password));  // md5 of the user:realm:user
+		md5.calculate();
+		String _H1 = md5.toString();
+
+		md5.begin();
+		md5.add(String("GET:") + String(uri));
+		md5.calculate();
+		String _H2 = md5.toString();
+
+		md5.begin();
+		md5.add(_H1 + ":" + _nonce + ":" + "00000001" + ":" + "gDBuFY4s" + ":" + "auth" + ":" + _H2);
+		md5.calculate();
+		String _response = md5.toString();
+
+		http.begin(String(server) + String(uri));
+		String authorization = "Digest username=\"admin\", realm=\"" + _realm + "\", nonce=\"" + _nonce + "\", uri=\"" + uri + "\", algorithm=\"MD5\", qop=auth, nc=00000001, cnonce=\"gDBuFY4s\", response=\"" + _response + "\"";
+		Serial.println(authorization);
+		http.addHeader("Authorization", authorization);
+
 		int httpCode = http.GET();
-
 		if (httpCode > 0) {
-			String authReq = http.header("WWW-Authenticate");
-			USE_SERIAL.println(authReq);
-
-			// file found at server
-			String payload = http.getString();
-			USE_SERIAL.println(payload);
-
-			String username = "admin";
-			String password = "admin";
-			
-			// extracting required parameters for RFC 2069 simpler Digest
-			// String _realm    = _exractParam(authReq, "realm=\"", '"');
-			String _realm = "Protected";
-			String _nonce    = _exractParam(authReq, "nonce=\"", '"');
-			// String _nonce = "2cadff7e2f160969";
-			String _opaque   = _exractParam(authReq, "opaque=\"", '"');
-
-			// parameters for the RFC 2617 newer Digest
-			MD5Builder md5;
-			md5.begin();
-			md5.add(username + ":" + _realm + ":" + password);  // md5 of the user:realm:user
-			md5.calculate();
-			String _H1 = md5.toString();
-
-			md5.begin();
-			md5.add(String("GET:") + String("/api/state?power=0"));
-			md5.calculate();
-			String _H2 = md5.toString();
-
-			md5.begin();
-			md5.add(_H1 + ":" + _nonce + ":" + "00000001" + ":" + "gDBuFY4s" + ":" + "auth" + ":" + _H2);
-			md5.calculate();
-			String _response = md5.toString();
-
-			http.begin("http://192.168.100.1/api/state?power=0");
-			USE_SERIAL.println(_nonce);
-			String Author = "Digest username=\"admin\", realm=\"Protected\", nonce=\"" + _nonce + "\", uri=\"/api/state?power=0\", algorithm=\"MD5\", qop=auth, nc=00000001, cnonce=\"gDBuFY4s\", response=\"" + _response + "\"\r\n";
-			USE_SERIAL.println(Author);
-			http.addHeader("Authorization", Author);
-
-			http.GET();
-
 			payload = http.getString();
-			USE_SERIAL.println(payload);
-		} else {
-			USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+			Serial.println(payload);
 		}
-
-		http.end();
+	} else {
+		USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
 	}
 
+	http.end();
 	delay(10000);
 }
-
