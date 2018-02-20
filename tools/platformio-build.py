f2fc590d51091356320f35641b48eb4439ea7aae
Ivan Kravets <me@ikravets.com>
2018-02-20 13:40:54 +0200

    Drop PlatformIO lines from LD script; append object suffix to the end of target name // Resolve #4355

--- tools/platformio-build.py
+++ tools/platformio-build.py
@@ -26,7 +26,22 @@ http://arduino.cc/en/Reference/HomePage
 
 from os.path import isdir, join
 
+from SCons import Builder, Util
 from SCons.Script import DefaultEnvironment
+
+
+def scons_patched_match_splitext(path, suffixes=None):
+    """
+    Patch SCons Builder, append $OBJSUFFIX to the end of each target
+    """
+    tokens = Util.splitext(path)
+    if suffixes and tokens[1] and tokens[1] in suffixes:
+        return (path, tokens[1])
+    return tokens
+
+
+Builder.match_splitext = scons_patched_match_splitext
+
 
 env = DefaultEnvironment()
 platform = env.PioPlatform()
