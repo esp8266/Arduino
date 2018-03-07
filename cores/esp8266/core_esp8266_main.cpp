6dc6514bc140a3c35a8706c96caab1fc1b77d3c3
David Gauchard <gauchard@laas.fr>
2018-03-07 11:17:56 +0100

    do not automatically enable sdk messages along with core messages

--- cores/esp8266/core_esp8266_main.cpp
+++ cores/esp8266/core_esp8266_main.cpp
@@ -115,9 +115,6 @@ static void loop_wrapper() {
     preloop_update_frequency();
     if(!setup_done) {
         setup();
-#ifdef DEBUG_ESP_PORT
-        DEBUG_ESP_PORT.setDebugOutput(true);
-#endif
         setup_done = true;
     }
     loop();
