96e2fa282aef3bcad51bb408293ee61e51bdb9e0
David Gauchard <gauchard@laas.fr>
2018-03-20 09:48:12 +0100

    misleadedly assumed ::write() could return -1 like arduino's ::read does

--- cores/esp8266/Print.cpp
+++ cores/esp8266/Print.cpp
@@ -46,7 +46,7 @@ size_t Print::write(const uint8_t *buffer, size_t size) {
     size_t n = 0;
     while (size--) {
         size_t ret = write(*buffer++);
-        if (ret <= 0) {
+        if (ret == 0) {
             // Write of last byte didn't complete, abort additional processing
             break;
         }
