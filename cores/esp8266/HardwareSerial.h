54a984fd6efa6e29bd12d622437037a759828786
David Gauchard <gauchard@laas.fr>
2018-03-26 09:51:31 +0200

    restore HardwareSerial::write(str)

--- cores/esp8266/HardwareSerial.h
+++ cores/esp8266/HardwareSerial.h
@@ -157,6 +157,10 @@ public:
     {
         return uart_write(_uart, (const char*)buffer, size);
     }
+    size_t write(const char *buffer)
+    {
+        return buffer? uart_write(_uart, buffer, strlen(size)): 0;
+    }
     operator bool() const
     {
         return _uart != 0;
