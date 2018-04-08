a055e4ab9923e35d55d0683222a6b7f046b46c67
david gauchard <gauchard@laas.fr>
2018-04-08 23:49:47 +0200

    casting pgm_read_+* only with __PROG_TYPES_COMPAT__, also add <avr/pgmspace.h> compatibility

--- cores/esp8266/pgmspace.h
+++ cores/esp8266/pgmspace.h
@@ -112,8 +112,13 @@ static inline uint16_t pgm_read_word_inlined(const void* addr) {
 }
 
 // Make sure, that libraries checking existence of this macro are not failing
+#ifdef __PROG_TYPES_COMPAT__
 #define pgm_read_byte(addr) pgm_read_byte_inlined((const void*)(addr))
 #define pgm_read_word(addr) pgm_read_word_inlined((const void*)(addr))
+#else
+#define pgm_read_byte(addr) pgm_read_byte_inlined(addr)
+#define pgm_read_word(addr) pgm_read_word_inlined(addr)
+#endif
 
 #else //__ets__
 #define pgm_read_byte(addr)     (*reinterpret_cast<const uint8_t*>(addr))
