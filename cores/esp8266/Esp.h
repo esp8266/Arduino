d0c16b58f0f3a5744cc260a6339919659961d191
marcvtew <40693695+marcvtew@users.noreply.github.com>
2018-07-26 16:39:47 +0200

    Update Esp.h

--- cores/esp8266/Esp.h
+++ cores/esp8266/Esp.h
@@ -92,7 +92,7 @@ class EspClass {
         void wdtDisable();
         void wdtFeed();
 
-        bool deepSleep(uint64_t time_us, RFMode mode = RF_DEFAULT);
+        void deepSleep(uint64_t time_us, RFMode mode = RF_DEFAULT);
         uint64_t deepSleepMax();
 
         bool rtcUserMemoryRead(uint32_t offset, uint32_t *data, size_t size);
