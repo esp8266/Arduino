d2c4cc795b3d5ec899d21c35b644fab9acd56b51
ShinDaiIchi <cakra2600@gmail.com>
2018-07-30 13:56:56 +0700

    Add  td_split()   for spliting String to String Array by multi-delimiters
    split Arduino's String to Arduino's String Array

--- cores/esp8266/WString.h
+++ cores/esp8266/WString.h
@@ -237,7 +237,8 @@ class String {
         void toLowerCase(void);
         void toUpperCase(void);
         void trim(void);
-
+        
+        int td_split(String delimiter, String** str_array); // v2.3   split String to String Array by multi-delimiters
         // parsing/conversion
         long toInt(void) const;
         float toFloat(void) const;
