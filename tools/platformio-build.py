1a87cd64497b5a37696af72514a94a8d7c657e7c
David Gauchard <gauchard@laas.fr>
2018-09-28 13:47:01 +0200

    pio

--- tools/platformio-build.py
+++ tools/platformio-build.py
@@ -137,15 +137,15 @@ flatten_cppdefines = env.Flatten(env['CPPDEFINES'])
 #
 if "PIO_FRAMEWORK_ARDUINO_LWIP2_LOW_MEMORY" in flatten_cppdefines:
     env.Append(
-        CPPDEFINES=[("TCP_MSS", 536)],
+        CPPDEFINES=[("TCP_MSS", 536), ("LWIP_TCP_SACK_OUT", 1)],
         CPPPATH=[join(FRAMEWORK_DIR, "tools", "sdk", "lwip2", "include")],
-        LIBS=["lwip2"]
+        LIBS=["lwip2-536-sack"]
     )
 elif "PIO_FRAMEWORK_ARDUINO_LWIP2_HIGHER_BANDWIDTH" in flatten_cppdefines:
     env.Append(
-        CPPDEFINES=[("TCP_MSS", 1460)],
+        CPPDEFINES=[("TCP_MSS", 1460), ("LWIP_TCP_SACK_OUT", 1)],
         CPPPATH=[join(FRAMEWORK_DIR, "tools", "sdk", "lwip2", "include")],
-        LIBS=["lwip2_1460"]
+        LIBS=["lwip2-1460-sack"]
     )
 else:
     env.Append(
