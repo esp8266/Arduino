a552dbd616fec221e0d7523e4de3092e05c54619
Christopher Liebman <liebman@zod.com>
2018-09-29 17:18:40 -0700

    fix indentation for style check

--- libraries/ESP8266HTTPClient/examples/HTTPRedirect/HTTPRedirect.ino
+++ libraries/ESP8266HTTPClient/examples/HTTPRedirect/HTTPRedirect.ino
@@ -20,15 +20,15 @@ ESP8266WiFiMulti WiFiMulti;
 
 void setup() {
 
-    Serial.begin(115200);
+  Serial.begin(115200);
   // Serial.setDebugOutput(true);
-    Serial.println();
-    Serial.println();
-    Serial.println();
+  Serial.println();
+  Serial.println();
+  Serial.println();
 
   for (uint8_t t = 10; t > 0; t--) {
-      Serial.printf("[SETUP] WAIT %d...\n", t);
-      Serial.flush();
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
     delay(1000);
   }
 
@@ -60,20 +60,20 @@ void loop() {
 
     // As long as we get redirects, follow them
     while(httpCode == 301 || httpCode == 302 || httpCode == 307) {
-        String location = http.header("Location");
-        Serial.printf("[HTTP] redirect(%d): %s\n", httpCode, location.c_str());
-        http.end();
-        if (!http.setURL(location)) {
-            Serial.printf("[HTTP] Bad redirect location: '%s'", location.c_str());
-            return;
-        }
-        httpCode = http.GET();
+      String location = http.header("Location");
+      Serial.printf("[HTTP] redirect(%d): %s\n", httpCode, location.c_str());
+      http.end();
+      if (!http.setURL(location)) {
+        Serial.printf("[HTTP] Bad redirect location: '%s'", location.c_str());
+        return;
+      }
+      httpCode = http.GET();
     }
 
     // httpCode will be negative on error
     if (httpCode > 0) {
       // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTP] GET... httpCode: %d\n", httpCode);
+      Serial.printf("[HTTP] GET... httpCode: %d\n", httpCode);
 
       // file found at server
       if (httpCode == HTTP_CODE_OK) {
@@ -81,7 +81,7 @@ void loop() {
         Serial.println(payload);
       }
     } else {
-        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
 
     http.end();
