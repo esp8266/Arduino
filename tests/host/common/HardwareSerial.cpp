b4e9cfb142cfe68e1243afa5dcaf15a816beb14d
David Gauchard <david@unixrox.net>
2018-11-10 02:18:43 +0100

    WIP

--- tests/host/common/HardwareSerial.cpp
+++ tests/host/common/HardwareSerial.cpp
@@ -11,19 +11,19 @@ HardwareSerial::HardwareSerial (int uart_nr)
 		fprintf(stderr, MOCK "FIXME HardwareSerial::HardwareSerial(%d)\n", uart_nr);
 }
 
-void HardwareSerial::begin(unsigned long baud, SerialConfig config, SerialMode mode, uint8_t tx_pin)
+void HardwareSerial::begin (unsigned long baud, SerialConfig config, SerialMode mode, uint8_t tx_pin)
 {
 	if (config != SERIAL_8N1 || mode != SERIAL_FULL || tx_pin != 1)
 		fprintf(stderr, MOCK "FIXME HardwareSerial::begin(baud=%ld config=0x%x mode=0x%x)\n", baud, (int)config, (int)mode);
 }
 
-int HardwareSerial::available(void)
+int HardwareSerial::available (void)
 {
 	printf(MOCK "TODO HardwareSerial::available\n");
 	return 0;
 }
 
-void HardwareSerial::flush()
+void HardwareSerial::flush ()
 {
 	//XXXTODO
 	fflush(stdout);
@@ -34,29 +34,40 @@ void HardwareSerial::flush()
 extern "C"
 {
 
-size_t uart_write_char(uart_t* uart, char c)
+size_t uart_write_char (uart_t* uart, char c)
 {
 	//XXXTODO
 	(void)uart;
-	write(1, &c, 1);
-	return 1;
+	return write(1, &c, 1);
 }
 
-int uart_peek_char(uart_t* uart)
+int uart_peek_char (uart_t* uart)
 {
 	///XXXTODO
+	static bool notimpl = false;
+	if (!notimpl)
+	{
+		notimpl = true;
+		fprintf(stderr, MOCK "FIXME uart_peek_char\n");
+	}
 	(void)uart;
 	return -1;
 }
 
-int uart_read_char(uart_t* uart)
+int uart_read_char (uart_t* uart)
 {
 	///XXXTODO
+	static bool notimpl = false;
+	if (!notimpl)
+	{
+		notimpl = true;
+		fprintf(stderr, MOCK "FIXME uart_read_char\n");
+	}
 	(void)uart;
 	return -1;
 }
 
-size_t uart_write(uart_t* uart, const char* buf, size_t size)
+size_t uart_write (uart_t* uart, const char* buf, size_t size)
 {
 	///XXXTODO
 	(void)uart;
