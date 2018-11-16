77b61d1d1fc1571714473719ea3746f1ba1abd9a
David Gauchard <gauchard@laas.fr>
2018-11-16 11:50:21 +0100

    example astyle

--- libraries/ESP8266WiFi/examples/udp/udp.ino
+++ libraries/ESP8266WiFi/examples/udp/udp.ino
@@ -73,45 +73,8 @@ void loop() {
   delay(10);
 }
 
-
 /*
-  Processing sketch to run with this example
-  =====================================================
-
-  // Processing UDP example to send and receive string data from Arduino
-  // press any key to send the "Hello Arduino" message
-
-
-  import hypermedia.net.*;
-
-  UDP udp;  // define the UDP object
-
-
-  void setup() {
-  udp = new UDP( this, 6000 );  // create a new datagram connection on port 6000
-  //udp.log( true ); 		// <-- printout the connection activity
-  udp.listen( true );           // and wait for incoming message
-  }
-
-  void draw()
-  {
-  }
-
-  void keyPressed() {
-  String ip       = "192.168.1.177";	// the remote IP address
-  int port        = 8888;		// the destination port
-
-  udp.send("Hello World", ip, port );   // the message to send
-
-  }
-
-  void receive( byte[] data ) { 			// <-- default handler
-  //void receive( byte[] data, String ip, int port ) {	// <-- extended handler
-
-  for(int i=0; i < data.length; i++)
-  print(char(data[i]));
-  println();
-  }
+  test (shell/netcat):
+  ---------------
+	  nc -u 192.168.esp.address 8888
 */
-
-
