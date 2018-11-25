c48d5f414ac1b57c9539acdd9b1974b4d01af789
david gauchard <gauchard@laas.fr>
2018-11-25 21:52:21 +0100

    fix example

--- libraries/ESP8266WiFi/examples/WiFiManualWebServer/WiFiManualWebServer.ino
+++ libraries/ESP8266WiFi/examples/WiFiManualWebServer/WiFiManualWebServer.ino
@@ -31,7 +31,7 @@ void setup() {
   // Connect to WiFi network
   Serial.println();
   Serial.println();
-  Serial.print("Connecting to ");
+  Serial.print(F("Connecting to "));
   Serial.println(ssid);
 
   WiFi.mode(WIFI_STA);
@@ -39,14 +39,14 @@ void setup() {
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    Serial.print(F("."));
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println();
+  Serial.println(F("WiFi connected"));
 
   // Start the server
   server.begin();
-  Serial.println("Server started");
+  Serial.println(F("Server started"));
 
   // Print the IP address
   Serial.println(WiFi.localIP());
@@ -58,25 +58,24 @@ void loop() {
   if (!client) {
     return;
   }
-  Serial.println("new client");
+  Serial.println(F("new client"));
 
   client.setTimeout(5000); // default is 1000
 
   // Read the first line of the request
   String req = client.readStringUntil('\r');
-  Serial.println("request: ");
+  Serial.println(F("request: "));
   Serial.println(req);
 
   // Match the request
   int val;
-  if (req.indexOf("/gpio/0") != -1) {
+  if (req.indexOf(F("/gpio/0")) != -1) {
     val = 0;
-  } else if (req.indexOf("/gpio/1") != -1) {
+  } else if (req.indexOf(F("/gpio/1")) != -1) {
     val = 1;
   } else {
-    Serial.println("invalid request");
-    client.stop();
-    return;
+    Serial.println(F("invalid request"));
+    val = digitalRead(LED_BUILTIN);
   }
 
   // Set LED according to the request
@@ -85,21 +84,24 @@ void loop() {
   // read/ignore the rest of the request
   // do not client.flush(): it is for output only, see below
   while (client.available())
-    // byte by byte is not very efficient
   {
+    // byte by byte is not very efficient
     client.read();
   }
 
   // Send the response to the client
   // it is OK for multiple small client.print/write,
   // because nagle algorithm will group them into one single packet
-  // ::print*_P(F("string")) is always usable and saves RAM
-  client.print_P(F("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIO is now "));
-  client.print_P((val) ? F("high") : F("low"));
-  client.print_P(F("</html>\n"));
+  client.print(F("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIO is now "));
+  client.print((val) ? F("high") : F("low"));
+  client.print(F("<br><br>Click <a href='http://"));
+  client.print(WiFi.localIP());
+  client.print(F("/gpio/1'>here</a> to switch LED GPIO on, or <a href='http://"));
+  client.print(WiFi.localIP());
+  client.print(F("/gpio/0'>here</a> to switch LED GPIO off.</html>"));
 
   // The client will actually be *flushed* then disconnected
   // when the function returns and 'client' object is destroyed (out-of-scope)
   // flush = ensure written data are received by the other side
-  Serial.println("Disconnecting from client");
+  Serial.println(F("Disconnecting from client"));
 }
