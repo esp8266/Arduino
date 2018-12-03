6c09fbdb126150c2a80225ba6269a87a84a8ee78
David Gauchard <gauchard@laas.fr>
2018-12-03 14:09:10 +0100

    update early disable wifi example

--- libraries/ESP8266WiFi/examples/EarlyDisableWiFi/EarlyDisableWiFi.ino
+++ libraries/ESP8266WiFi/examples/EarlyDisableWiFi/EarlyDisableWiFi.ino
@@ -10,31 +10,9 @@
 // from nonos-sdk's user entry point user_init()
 
 void preinit() {
-  // Global constructors are not called yet
-  // (global class instances like WiFi, Serial... are not yet initialized).
-  // In this function, we must only use nonos-sdk API.
-
-  // example
-
-  // https://github.com/esp8266/Arduino/issues/2111#issuecomment-224251391
-  // WiFi.persistent(false);
-  // WiFi.mode(WIFI_OFF);
-  // WiFi.forceSleepBegin();
-
-  //WiFi.mode(WIFI_OFF) equivalent:
-  // datasheet:
-  // Set Wi-Fi working mode to Station mode, SoftAP
-  // or Station + SoftAP, and do not update flash
-  // (not persistent)
-  wifi_set_opmode_current(WIFI_OFF);
-
-  //WiFi.forceSleepBegin(/*default*/0) equivalent:
-  // sleep forever until wifi_fpm_do_wakeup() is called
-  wifi_fpm_set_sleep_type(MODEM_SLEEP_T);
-  wifi_fpm_open();
-  wifi_fpm_do_sleep(0xFFFFFFF);
-
-  // use WiFi.forceSleepWake() to wake WiFi up
+  // Global WiFi constructors are not called yet
+  // (global class instances like WiFi, Serial... are not yet initialized)..
+  ESP8266WiFiClass::preinit_wifi_off();
 }
 
 void setup() {
@@ -47,7 +25,7 @@ void setup() {
   // a DSO is needed to check #2111
   delay(5000);
 
-  Serial.println("waking  WiFi up, sleeping 5s");
+  Serial.println("waking WiFi up, sleeping 5s");
   WiFi.forceSleepWake();
 
   // amp meter raises to 75mA
@@ -59,11 +37,11 @@ void setup() {
 
   for (bool configured = false; !configured;) {
     for (auto addr : addrList)
-      if ((configured = !addr->isLocal() && addr->ifnumber() == STATION_IF)) {
+      if ((configured = !addr.isLocal() && addr.ifnumber() == STATION_IF)) {
         Serial.printf("STA: IF='%s' hostname='%s' addr= %s\n",
-                      addr->ifname().c_str(),
-                      addr->ifhostname(),
-                      addr->addr().toString().c_str());
+                      addr.ifname().c_str(),
+                      addr.ifhostname(),
+                      addr.toString().c_str());
         break;
       }
     Serial.print('.');
