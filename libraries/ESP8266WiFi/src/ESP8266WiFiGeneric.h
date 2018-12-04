216680bb576d845547349ab992043c6d6f83fe73
david gauchard <gauchard@laas.fr>
2018-12-04 05:54:27 +0100

    weak hook preinit() #2111 #2133 #2136 (#5395)
    * weak hook early_setup() #2111 #2133 #2136
    
    * rename to early_init (more "c" vs early_setup which is more "c++arduino")
    
    * example
    
    * improve earlyWiFi example, slightly change AddrList interface, move WiFi sketches into WiFi examples
    
    * fix CI
    
    * fix local CI runner
    
    * fix local CI runner
    
    * rename early_init() to preinit()
    
    * + static ESP8266WiFiClass::preinit_wifi_off()
    
    * update early disable wifi example
    
    * example update
    
    * IPv6 example update
    
    * Update ESP8266WiFiGeneric.h
    
    camelCase for static method name
    
    * Update ESP8266WiFiGeneric.cpp
    
    camelCase for static method name
    
    * Update EarlyDisableWiFi.ino
    
    Expand comment, fix static method name
    
    * Update core_esp8266_main.cpp
    
    Expanded comment.
    
    * Update core_esp8266_main.cpp
    
    Expanded comment
    
    * Update EarlyDisableWiFi.ino
    
    Expanded comment

--- libraries/ESP8266WiFi/src/ESP8266WiFiGeneric.h
+++ libraries/ESP8266WiFi/src/ESP8266WiFiGeneric.h
@@ -88,6 +88,8 @@ class ESP8266WiFiGenericClass {
         bool forceSleepBegin(uint32 sleepUs = 0);
         bool forceSleepWake();
 
+        static void preinitWiFiOff (); //meant to be called in user-defined preinit()
+
     protected:
         static bool _persistent;
         static WiFiMode_t _forceSleepLastMode;
