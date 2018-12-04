f50df4ee7bb03f108dfabd9c6866fe80e515f6a3
Alexandre Picquot <alexandre.picquot@gmail.com>
2018-12-03 19:19:10 -0500

    keep indent issue

--- tools/boards.txt.py
+++ tools/boards.txt.py
@@ -1412,10 +1412,10 @@ def package ():
         filestr = package_file.read()
 
     substitution = '"boards": [\n'
-    board_items = ['                  {\n                     "name": "%s"\n                  }' % boards[id]['name']
+    board_items = ['            {\n              "name": "%s"\n            }' % boards[id]['name']
                     for id in boards]
     substitution += ',\n'.join(board_items)        
-    substitution += '\n               ],'
+    substitution += '\n          ],'
 
     newfilestr = re.sub(r'"boards":[^\]]*\],', substitution, filestr, re.MULTILINE)
 
