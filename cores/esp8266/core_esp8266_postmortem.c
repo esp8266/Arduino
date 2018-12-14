36689f7f322f4442b9b2792110224f99c03fb6cd
Earle F. Philhower, III <Earle.Philhower.III@wdc.com>
2018-12-14 09:21:57 -0800

    Use function, not macro, to save code space
    Save ~2KB final bin size by using a ets_printf_P function and not
    an inline macro.  IRAM and HEAP unaffected.

--- cores/esp8266/core_esp8266_postmortem.c
+++ cores/esp8266/core_esp8266_postmortem.c
@@ -64,16 +64,18 @@ extern void custom_crash_callback( struct rst_info * rst_info, uint32_t stack, u
 
 
 // Prints need to use our library function to allow for file and function
-// to be safely accessed from flash. This macro encapsulates the snprintf()
+// to be safely accessed from flash. This function encapsulates snprintf()
 // [which by definition will 0-terminate] and dumping to the UART
-#define ets_printf_P(str, ...) \
-{ \
-    char destStr[160]; \
-    snprintf(destStr, sizeof(destStr), PSTR(str), ##__VA_ARGS__); \
-    char *c = destStr; \
-    while (*c) { \
-        ets_putc(*(c++)); \
-    } \
+static void ets_printf_P(const char *str, ...) {
+    char destStr[160];
+    char *c = destStr;
+    va_list argPtr;
+    va_start(argPtr, str);
+    vsnprintf(destStr, sizeof(destStr), str, argPtr);
+    va_end(argPtr);
+    while (*c) {
+        ets_putc(*(c++));
+    }
 }
 
 void __wrap_system_restart_local() {
@@ -100,24 +102,24 @@ void __wrap_system_restart_local() {
     ets_install_putc1(&uart_write_char_d);
 
     if (s_panic_line) {
-        ets_printf_P("\nPanic %S:%d %S", s_panic_file, s_panic_line, s_panic_func);
+        ets_printf_P(PSTR("\nPanic %S:%d %S"), s_panic_file, s_panic_line, s_panic_func);
         if (s_panic_what) {
-            ets_printf_P(": Assertion '%S' failed.", s_panic_what);
+            ets_printf_P(PSTR(": Assertion '%S' failed."), s_panic_what);
         }
         ets_putc('\n');
     }
     else if (s_unhandled_exception) {
-        ets_printf_P("\nUnhandled exception: %S\n", s_unhandled_exception);
+        ets_printf_P(PSTR("\nUnhandled exception: %S\n"), s_unhandled_exception);
     }
     else if (s_abort_called) {
-        ets_printf_P("\nAbort called\n");
+        ets_printf_P(PSTR("\nAbort called\n"));
     }
     else if (rst_info.reason == REASON_EXCEPTION_RST) {
-        ets_printf_P("\nException (%d):\nepc1=0x%08x epc2=0x%08x epc3=0x%08x excvaddr=0x%08x depc=0x%08x\n",
+        ets_printf_P(PSTR("\nException (%d):\nepc1=0x%08x epc2=0x%08x epc3=0x%08x excvaddr=0x%08x depc=0x%08x\n"),
             rst_info.exccause, rst_info.epc1, rst_info.epc2, rst_info.epc3, rst_info.excvaddr, rst_info.depc);
     }
     else if (rst_info.reason == REASON_SOFT_WDT_RST) {
-        ets_printf_P("\nSoft WDT reset\n");
+        ets_printf_P(PSTR("\nSoft WDT reset\n"));
     }
 
     uint32_t cont_stack_start = (uint32_t) &(g_pcont->stack);
@@ -138,37 +140,36 @@ void __wrap_system_restart_local() {
         offset = 0x10;
     }
 
-    ets_printf_P("\n>>>stack>>>\n");
+    ets_printf_P(PSTR("\n>>>stack>>>\n"));
 
     if (sp > stack_thunk_get_stack_bot() && sp <= stack_thunk_get_stack_top()) {
         // BearSSL we dump the BSSL second stack and then reset SP back to the main cont stack
-        ets_printf_P("\nctx: bearssl\n");
-        ets_printf_P("sp: %08x end: %08x offset: %04x\n", sp, stack_thunk_get_stack_top(), offset);
+        ets_printf_P(PSTR("\nctx: bearssl\nsp: %08x end: %08x offset: %04x\n"), sp, stack_thunk_get_stack_top(), offset);
         print_stack(sp + offset, stack_thunk_get_stack_top());
         offset = 0; // No offset needed anymore, the exception info was stored in the bssl stack
         sp = stack_thunk_get_cont_sp();
     }
 
     if (sp > cont_stack_start && sp < cont_stack_end) {
-        ets_printf_P("\nctx: cont\n");
+        ets_printf_P(PSTR("\nctx: cont\n"));
         stack_end = cont_stack_end;
     }
     else {
-        ets_printf_P("\nctx: sys\n");
+        ets_printf_P(PSTR("\nctx: sys\n"));
         stack_end = 0x3fffffb0;
         // it's actually 0x3ffffff0, but the stuff below ets_run
         // is likely not really relevant to the crash
     }
 
-    ets_printf_P("sp: %08x end: %08x offset: %04x\n", sp, stack_end, offset);
+    ets_printf_P(PSTR("sp: %08x end: %08x offset: %04x\n"), sp, stack_end, offset);
 
     print_stack(sp + offset, stack_end);
 
-    ets_printf_P("<<<stack<<<\n");
+    ets_printf_P(PSTR("<<<stack<<<\n"));
 
     // Use cap-X formatting to ensure the standard EspExceptionDecoder doesn't match the address
     if (umm_last_fail_alloc_addr) {
-      ets_printf_P("\nlast failed alloc call: %08X(%d)\n", (uint32_t)umm_last_fail_alloc_addr, umm_last_fail_alloc_size);
+      ets_printf_P(PSTR("\nlast failed alloc call: %08X(%d)\n"), (uint32_t)umm_last_fail_alloc_addr, umm_last_fail_alloc_size);
     }
 
     custom_crash_callback( &rst_info, sp + offset, stack_end );
@@ -185,7 +186,7 @@ static void print_stack(uint32_t start, uint32_t end) {
         // rough indicator: stack frames usually have SP saved as the second word
         bool looksLikeStackFrame = (values[2] == pos + 0x10);
 
-        ets_printf_P("%08x:  %08x %08x %08x %08x %c\n",
+        ets_printf_P(PSTR("%08x:  %08x %08x %08x %08x %c\n"),
             pos, values[0], values[1], values[2], values[3], (looksLikeStackFrame)?'<':' ');
     }
 }
