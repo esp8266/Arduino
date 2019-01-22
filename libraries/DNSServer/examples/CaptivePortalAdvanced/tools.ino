e79079daaf9d735263f1dcd17ceb8522f511f401
david gauchard <gauchard@laas.fr>
2019-01-23 00:21:17 +0100

    fix example

--- libraries/DNSServer/examples/CaptivePortalAdvanced/tools.ino
+++ libraries/DNSServer/examples/CaptivePortalAdvanced/tools.ino
@@ -1,21 +1,10 @@
 /** Is this an IP? */
-boolean isIp(String str) {
-  for (size_t i = 0; i < str.length(); i++) {
-    int c = str.charAt(i);
-    if (c != '.' && (c < '0' || c > '9')) {
-      return false;
-    }
-  }
-  return true;
+boolean isIp(const String& str) {
+  return IPAddress().fromString(str);
 }
 
 /** IP to String? */
-String toStringIp(IPAddress ip) {
-  String res = "";
-  for (int i = 0; i < 3; i++) {
-    res += String((ip >> (8 * i)) & 0xFF) + ".";
-  }
-  res += String(((ip >> 8 * 3)) & 0xFF);
-  return res;
+String toStringIp(const IPAddress& ip) {
+  return ip.toString();
 }
 
