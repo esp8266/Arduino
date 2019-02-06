55eb7c8190039704b39301ebca6ea3bb5a2c3980
Allman <gauchard+allman@laas.fr>
2019-02-06 15:59:12 +0100

    restyle core and libraries with Allman

---
+++ tests/restyle.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -x
+
+set -ev
+
+org=$(cd ${0%/*}; pwd)
+cd ${org}/..
+pwd
+test -d cores/esp8266
+test -d libraries
+
+for d in cores/esp8266 libraries; do
+	for e in c cpp h; do
+		find $d -name "*.$e" -exec \
+		    astyle \
+		        --suffix=none \
+	        	--options=${org}/astyle_core.conf {} \;
+	done
+done
+
+for d in libraries; do
+	find $d -name "*.ino" -exec \
+	    astyle \
+	        --suffix=none \
+        	--options=${org}/astyle_examples.conf {} \;
+done
