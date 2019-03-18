esp8266/Arduino

commit 7369133681922880aa42c73374d74aaa3fc16e25

Small String Optimization (#5690)
Reduce String memory overhead from 24 bytes to 16 bytes by limiting the
maximum string length to <64Kbytes (which is larger than heap so no
effective problem).

Add Small String Optimization, SSO, which instead of allocating pointers
to small strings on the heap will store the string in place of the
pointer in the class.  This should reduce memory fragmentation as

Save up to 12 chars (11 + \0) in String itself by using the terminating
\0 in the inline string as a flag to identify if this is a SSO or a heap
string.

Add a host test that verifies that no memory is allocated until a
full 11 characters are assigned to a string, as well as checking all
intermediate values.

No user code changes should be required to work with this optimization.

tests/host/core/test_string.cpp
@@ -268,3 +268,96 @@ TEST_CASE("String sizes near 8b", "[core][String]")
     REQUIRE(!strcmp(s16.c_str(),"123456789012345_"));
     REQUIRE(!strcmp(s17.c_str(),"1234567890123456_"));
 }
+
+TEST_CASE("String SSO works", "[core][String]")
+{
+  // This test assumes that SSO_SIZE==8, if that changes the test must as well
+  String s;
+  s += "0";
+  REQUIRE(s == "0");
+  REQUIRE(s.length() == 1);
+  const char *savesso = s.c_str();
+  s += 1;
+  REQUIRE(s.c_str() == savesso);
+  REQUIRE(s == "01");
+  REQUIRE(s.length() == 2);
+  s += "2";
+  REQUIRE(s.c_str() == savesso);
+  REQUIRE(s == "012");
+  REQUIRE(s.length() == 3);
+  s += 3;
+  REQUIRE(s.c_str() == savesso);
+  REQUIRE(s == "0123");
+  REQUIRE(s.length() == 4);
+  s += "4";
+  REQUIRE(s.c_str() == savesso);
+  REQUIRE(s == "01234");
+  REQUIRE(s.length() == 5);
+  s += "5";
+  REQUIRE(s.c_str() == savesso);
+  REQUIRE(s == "012345");
+  REQUIRE(s.length() == 6);
+  s += "6";
+  REQUIRE(s.c_str() == savesso);
+  REQUIRE(s == "0123456");
+  REQUIRE(s.length() == 7);
+  s += "7";
+  REQUIRE(s.c_str() == savesso);
+  REQUIRE(s == "01234567");
+  REQUIRE(s.length() == 8);
+  s += "8";
+  REQUIRE(s.c_str() == savesso);
+  REQUIRE(s == "012345678");
+  REQUIRE(s.length() == 9);
+  s += "9";
+  REQUIRE(s.c_str() == savesso);
+  REQUIRE(s == "0123456789");
+  REQUIRE(s.length() == 10);
+  s += "a";
+  REQUIRE(s.c_str() == savesso);
+  REQUIRE(s == "0123456789a");
+  REQUIRE(s.length() == 11);
+  if (sizeof(savesso) == 4) {
+    s += "b";
+    REQUIRE(s.c_str() != savesso);
+    REQUIRE(s == "0123456789ab");
+    REQUIRE(s.length() == 12);
+    s += "c";
+    REQUIRE(s.c_str() != savesso);
+    REQUIRE(s == "0123456789abc");
+    REQUIRE(s.length() == 13);
+  } else {
+    s += "bcde";
+    REQUIRE(s.c_str() == savesso);
+    REQUIRE(s == "0123456789abcde");
+    REQUIRE(s.length() == 15);
+    s += "fghi";
+    REQUIRE(s.c_str() == savesso);
+    REQUIRE(s == "0123456789abcdefghi");
+    REQUIRE(s.length() == 19);
+    s += "j";
+    REQUIRE(s.c_str() != savesso);
+    REQUIRE(s == "0123456789abcdefghij");
+    REQUIRE(s.length() == 20);
+    s += "k";
+    REQUIRE(s.c_str() != savesso);
+    REQUIRE(s == "0123456789abcdefghijk");
+    REQUIRE(s.length() == 21);
+    s += "l";
+    REQUIRE(s.c_str() != savesso);
+    REQUIRE(s == "0123456789abcdefghijkl");
+    REQUIRE(s.length() == 22);
+    s += "m";
+    REQUIRE(s.c_str() != savesso);
+    REQUIRE(s == "0123456789abcdefghijklm");
+    REQUIRE(s.length() == 23);
+    s += "nopq";
+    REQUIRE(s.c_str() != savesso);
+      REQUIRE(s == "0123456789abcdefghijklmnopq");
+    REQUIRE(s.length() == 27);
+    s += "rstu";
+    REQUIRE(s.c_str() != savesso);
+    REQUIRE(s == "0123456789abcdefghijklmnopqrstu");
+    REQUIRE(s.length() == 31);
+  }
+}

cores/esp8266/StreamString.cpp
@@ -26,9 +26,9 @@
 size_t StreamString::write(const uint8_t *data, size_t size) {
     if(size && data) {
         if(reserve(length() + size + 1)) {
-            memcpy((void *) (buffer + len), (const void *) data, size);
-            len += size;
-            *(buffer + len) = 0x00; // add null for string end
+            memcpy((void *) (wbuffer() + len()), (const void *) data, size);
+            setLen(len() + size);
+            *(wbuffer() + len()) = 0x00; // add null for string end
             return size;
         }
     }

cores/esp8266/WString.h
@@ -76,8 +76,8 @@ class String {
         // invalid string (i.e., "if (s)" will be true afterwards)
         unsigned char reserve(unsigned int size);
         inline unsigned int length(void) const {
-            if(buffer) {
-                return len;
+            if(buffer()) {
+                return len();
             } else {
                 return 0;
             }
@@ -172,7 +172,7 @@ class String {
 
         // comparison (only works w/ Strings and "strings")
         operator StringIfHelperType() const {
-            return buffer ? &String::StringIfHelper : 0;
+            return buffer() ? &String::StringIfHelper : 0;
         }
         int compareTo(const String &s) const;
         unsigned char equals(const String &s) const;
@@ -208,9 +208,9 @@ class String {
         void toCharArray(char *buf, unsigned int bufsize, unsigned int index = 0) const {
             getBytes((unsigned char *) buf, bufsize, index);
         }
-        const char* c_str() const { return buffer; }
-        char* begin() { return buffer; }
-        char* end() { return buffer + length(); }
+        const char* c_str() const { return buffer(); }
+        char* begin() { return wbuffer(); }
+        char* end() { return wbuffer() + length(); }
         const char* begin() const { return c_str(); }
         const char* end() const { return c_str() + length(); }
 
@@ -224,7 +224,7 @@ class String {
         int lastIndexOf(const String &str) const;
         int lastIndexOf(const String &str, unsigned int fromIndex) const;
         String substring(unsigned int beginIndex) const {
-            return substring(beginIndex, len);
+            return substring(beginIndex, len());
         }
         ;
         String substring(unsigned int beginIndex, unsigned int endIndex) const;
@@ -243,9 +243,34 @@ class String {
         float toFloat(void) const;
 
     protected:
-        char *buffer;	        // the actual char array
-        unsigned int capacity;  // the array length minus one (for the '\0')
-        unsigned int len;       // the String length (not counting the '\0')
+        // Contains the string info when we're not in SSO mode
+        struct _ptr { 
+            char *   buff;
+            uint16_t cap;
+            uint16_t len;
+        };
+
+        // SSO is handled by checking the last byte of sso_buff.
+        // When not in SSO mode, that byte is set to 0xff, while when in SSO mode it is always 0x00 (so it can serve as the string terminator as well as a flag)
+        // This allows strings up up to 12 (11 + \0 termination) without any extra space.
+        enum { SSOSIZE = sizeof(struct _ptr) + 4 }; // Characters to allocate space for SSO, must be 12 or more
+        enum { CAPACITY_MAX = 65535 }; // If size of capacity changed, be sure to update this enum
+        union {
+            struct _ptr ptr;
+            char sso_buf[SSOSIZE];
+        };
+        // Accessor functions
+        inline bool sso() const { return sso_buf[SSOSIZE - 1] == 0; }
+        inline unsigned int len() const { return sso() ? strlen(sso_buf) : ptr.len; }
+        inline unsigned int capacity() const { return sso() ? SSOSIZE - 1 : ptr.cap; }
+        inline void setSSO(bool sso) { sso_buf[SSOSIZE - 1] = sso ? 0x00 : 0xff; }
+        inline void setLen(int len) { if (!sso()) ptr.len = len; }
+        inline void setCapacity(int cap) { if (!sso()) ptr.cap = cap; }
+	inline void setBuffer(char *buff) { if (!sso()) ptr.buff = buff; }
+        // Buffer accessor functions
+        inline const char *buffer() const { return (const char *)(sso() ? sso_buf : ptr.buff); }
+        inline char *wbuffer() const { return sso() ? const_cast<char *>(sso_buf) : ptr.buff; } // Writable version of buffer
+
     protected:
         void init(void);
         void invalidate(void);

commit db88c828824f38db55e863274137d0f599011511

Fix Small String Optimization misplaced 0 terminator
A fix for some issue introduced in PR #5690
See discussion in https://github.com/esp8266/Arduino/issues/5883

cores/esp8266/WString.cpp
@@ -129,6 +129,7 @@ String::~String() {
 // /*********************************************/
 
 inline void String::init(void) {
+    memset(sso_buf, 0, sizeof(sso_buf));
     setSSO(false);
     setCapacity(0);
     setLen(0);
@@ -161,11 +162,11 @@ unsigned char String::changeBuffer(unsigned int maxStrLen) {
             return 1;
         } else { // if bufptr && !sso()
             // Using bufptr, need to shrink into sso_buff
-            char temp[sizeof(sso_buf)];
+            char temp[sizeof(sso_buf)] = {0};
             memcpy(temp, buffer(), maxStrLen);
             free(wbuffer());
             setSSO(true);
-            memcpy(wbuffer(), temp, maxStrLen);
+            memcpy(wbuffer(), temp, sizeof(sso_buf));
             return 1;
         }
     }

tests/host/core/test_string.cpp
@@ -83,6 +83,12 @@ TEST_CASE("String constructors", "[core][String]")
     REQUIRE(ssh == "3.14159_abcd");
     String flash = (F("hello from flash"));
     REQUIRE(flash == "hello from flash");
+    const char textarray[6] = {'h', 'e', 'l', 'l', 'o', 0};
+    String hello(textarray);
+    REQUIRE(hello == "hello");
+    String hello2;
+    hello2 = textarray;
+    REQUIRE(hello2 == "hello");
 }
 
 TEST_CASE("String concantenation", "[core][String]")
@@ -360,4 +366,8 @@ TEST_CASE("String SSO works", "[core][String]")
     REQUIRE(s == "0123456789abcdefghijklmnopqrstu");
     REQUIRE(s.length() == 31);
   }
+  s = "0123456789abcde";
+  s = s.substring(s.indexOf('a'));
+  REQUIRE(s == "abcde");
+  REQUIRE(s.length() == 5);
 }