66beb4a6bd3df7af7774468cf6b8084737ae74a2
Allman <47007540+Allman-astyler@users.noreply.github.com>
2019-05-13 00:50:07 +0200

    switch restyle script for CI

--- tests/restyle.sh
+++ tests/restyle.sh
@@ -8,12 +8,24 @@ pwd
 test -d cores/esp8266
 test -d libraries
 
-# in a near future, restyle-all.sh will be renamed to restyle.sh
-# and will be checked against CI
+# this warning question will be removed after restyle-all.sh is renamed to restyle.sh
+echo "This is dangerous if you have modified your local repository"
+echo "type iknowwhatido to continue"
+read ans
+test "$ans" = iknowwhatido || exit 1
+
+for d in cores/esp8266 libraries; do
+    for e in c cpp h; do
+        find $d -name "*.$e" -exec \
+            astyle \
+                --suffix=none \
+                --options=${org}/astyle_core.conf {} \;
+        done
+done
 
 for d in libraries; do
-	find $d -name "*.ino" -exec \
-	    astyle \
-	        --suffix=none \
-                --options=${org}/astyle_examples.conf {} \;
+    find $d -name "*.ino" -exec \
+        astyle \
+            --suffix=none \
+            --options=${org}/astyle_examples.conf {} \;
 done
