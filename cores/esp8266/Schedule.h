c042640f163392dba00d519cf472e25ef040bfe7
david gauchard <gauchard@laas.fr>
2019-05-21 01:01:53 +0200

    restore FIFO

--- cores/esp8266/Schedule.h
+++ cores/esp8266/Schedule.h
@@ -6,9 +6,8 @@
 #define SCHEDULED_FN_MAX_COUNT 32
 #define SCHEDULED_FN_INITIAL_COUNT 4
 
-// Warning
-// This API is not considered stable.
-// Function signatures will change.
+// This API was not considered stable but is now fossilizing.
+// Function signatures may change, queue must stay FIFO.
 // You have been warned.
 
 // Run given function ONCE next time `loop` function returns,
@@ -20,7 +19,7 @@
 // Returns false if the number of scheduled functions exceeds SCHEDULED_FN_MAX_COUNT.
 bool schedule_function(std::function<void(void)> fn);
 
-// run given function every at least <repeat_us> microseconds until it returns false
+// Run given function every at least <repeat_us> microseconds until it returns false
 bool schedule_function_us(std::function<bool(void)> fn, uint32_t repeat_us);
 
 // Run all scheduled functions.
