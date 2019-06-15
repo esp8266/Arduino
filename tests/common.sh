74b84ff8ab121c14d2e0bac1665458bc67ead3b2
Ruan <ruandv@gmail.com>
2019-06-15 19:11:04 +0200

    Updated References to ArduinoJson 6.11.0

--- tests/common.sh
+++ tests/common.sh
@@ -135,7 +135,7 @@ function install_libraries()
     pushd $HOME/Arduino/libraries
 
     # install ArduinoJson library
-    { test -r ArduinoJson-v4.6.1.zip || wget https://github.com/bblanchon/ArduinoJson/releases/download/v4.6.1/ArduinoJson-v4.6.1.zip; } && unzip ArduinoJson-v4.6.1.zip
+    { test -r ArduinoJson-v6.11.0 || wget https://github.com/bblanchon/ArduinoJson/releases/download/v6.11.0/ArduinoJson-v6.11.0.zip; } && unzip ArduinoJson-v6.11.0
 
     popd
 }
