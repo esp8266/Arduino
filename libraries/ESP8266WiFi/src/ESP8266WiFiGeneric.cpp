c841cce3f9c4d924a1e7ea79913a5bafb4230467
ruggi99 <ruggerotomaselli@gmail.com>
2019-07-10 09:22:14 +0200

    Update ESP8266WiFiGeneric.cpp

--- libraries/ESP8266WiFi/src/ESP8266WiFiGeneric.cpp
+++ libraries/ESP8266WiFi/src/ESP8266WiFiGeneric.cpp
@@ -647,23 +647,23 @@ void wifi_dns_found_callback_async(const char* name, CONST ip_addr_t* ipaddr, vo
 
 //meant to be called from user-defined preinit()
 void ESP8266WiFiGenericClass::preinitWiFiOff() {
-    // https://github.com/esp8266/Arduino/issues/2111#issuecomment-224251391
-    // WiFi.persistent(false);
-    // WiFi.mode(WIFI_OFF);
-    // WiFi.forceSleepBegin();
+  // https://github.com/esp8266/Arduino/issues/2111#issuecomment-224251391
+  // WiFi.persistent(false);
+  // WiFi.mode(WIFI_OFF);
+  // WiFi.forceSleepBegin();
 
-    //WiFi.mode(WIFI_OFF) equivalent:
-    // datasheet:
-    // Set Wi-Fi working mode to Station mode, SoftAP
-    // or Station + SoftAP, and do not update flash
-    // (not persistent)
-    wifi_set_opmode_current(WIFI_OFF);
+  //WiFi.mode(WIFI_OFF) equivalent:
+  // datasheet:
+  // Set Wi-Fi working mode to Station mode, SoftAP
+  // or Station + SoftAP, and do not update flash
+  // (not persistent)
+  wifi_set_opmode_current(WIFI_OFF);
 
-    //WiFi.forceSleepBegin(/*default*/0) equivalent:
-    // sleep forever until wifi_fpm_do_wakeup() is called
-    wifi_fpm_set_sleep_type(MODEM_SLEEP_T);
-    wifi_fpm_open();
-    wifi_fpm_do_sleep(0xFFFFFFF);
+  //WiFi.forceSleepBegin(/*default*/0) equivalent:
+  // sleep forever until wifi_fpm_do_wakeup() is called
+  wifi_fpm_set_sleep_type(MODEM_SLEEP_T);
+  wifi_fpm_open();
+  wifi_fpm_do_sleep(0xFFFFFFF);
 
-    // use WiFi.forceSleepWake() to wake WiFi up
+  // use WiFi.forceSleepWake() to wake WiFi up
 }
