33fee67c29eee96470611ca098f569cecfd71746
Earle F. Philhower, III <earlephilhower@yahoo.com>
2019-07-13 09:02:03 -0700

    Remove gdb stub warnings w/a pragma

--- cores/esp8266/gdb_hooks.cpp
+++ cores/esp8266/gdb_hooks.cpp
@@ -31,6 +31,11 @@ static bool ICACHE_RAM_ATTR __gdb_no_op()
     return false;
 }
 
+// To save space, don't create a dummy no-op for each GCC, just point to the no-op
+// Need to turn off GCC's checking of parameter types or we'll get many warnings
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattribute-alias"
+#pragma GCC diagnostic ignored "-Wmissing-attributes"
 void gdb_init(void) __attribute__ ((weak, alias("__gdb_no_op")));
 void gdb_do_break(void) __attribute__ ((weak, alias("__gdb_no_op")));
 bool gdb_present(void) __attribute__ ((weak, alias("__gdb_no_op")));
@@ -40,5 +45,6 @@ bool gdbstub_has_uart_isr_control(void) __attribute__ ((weak, alias("__gdb_no_op
 void gdbstub_set_uart_isr_callback(void (*func)(void*, uint8_t), void* arg) __attribute__ ((weak, alias("__gdb_no_op")));
 void gdbstub_write_char(char c) __attribute__ ((weak, alias("__gdb_no_op")));
 void gdbstub_write(const char* buf, size_t size) __attribute__ ((weak, alias("__gdb_no_op")));
+#pragma GCC diagnostic pop
 
 };
