b19070a858ff0a554414ecb853c34a82f314dc5e
ruggi99 <ruggerotomaselli@gmail.com>
2019-07-15 13:38:28 +0200

    Update WString.h

--- cores/esp8266/WString.h
+++ cores/esp8266/WString.h
@@ -82,7 +82,7 @@ class String {
                 return 0;
             }
         }
-        inline void empty(void) const {
+        inline void empty(void) {
             setLen(0);
         }
         inline bool isEmpty(void) const {
