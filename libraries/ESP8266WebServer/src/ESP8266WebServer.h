5cb82c16104b103411e2fb1888e1ec9429a8cf9f
david gauchard <gauchard@laas.fr>
2019-07-20 08:29:02 +0200

    webserver: restore legacy request handler (#6321)

--- libraries/ESP8266WebServer/src/ESP8266WebServer.h
+++ libraries/ESP8266WebServer/src/ESP8266WebServer.h
@@ -216,6 +216,7 @@ protected:
 
 
 using ESP8266WebServer = esp8266webserver::ESP8266WebServerTemplate<WiFiServer>;
+using RequestHandler = esp8266webserver::RequestHandler<WiFiServer>;
 
 
 #endif //ESP8266WEBSERVER_H
