5ad6ff9d96f4b947c3c18b49339ad027bc83df8e
Mehdi Beyk Mohamadi <mbm_greenway@yahoo.com>
2019-08-15 02:20:03 +0430

    Added ability to set start and end ip for softAP
    Base on #3562 and discussed in #6031

--- libraries/ESP8266WiFi/src/ESP8266WiFiAP.h
+++ libraries/ESP8266WiFi/src/ESP8266WiFiAP.h
@@ -38,6 +38,7 @@ class ESP8266WiFiAPClass {
 
         bool softAP(const char* ssid, const char* passphrase = NULL, int channel = 1, int ssid_hidden = 0, int max_connection = 4);
         bool softAP(const String& ssid,const String& passphrase = emptyString,int channel = 1,int ssid_hidden = 0,int max_connection = 4);
+        bool softAPConfig(IPAddress local_ip, IPAddress gateway, IPAddress subnet, IPAddress dhcp_start, IPAddress dhcp_end);
         bool softAPConfig(IPAddress local_ip, IPAddress gateway, IPAddress subnet);
         bool softAPdisconnect(bool wifioff = false);
 
