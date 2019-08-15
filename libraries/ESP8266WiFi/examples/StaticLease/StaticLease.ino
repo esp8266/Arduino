9f1b343b09b3d710bd526c201ee65beb83d0e919
Mehdi Beyk Mohamadi <mbm_greenway@yahoo.com>
2019-08-15 15:48:58 +0430

    Fix Static DHCP Lease
    Fix #6031

--- libraries/ESP8266WiFi/examples/StaticLease/StaticLease.ino
+++ libraries/ESP8266WiFi/examples/StaticLease/StaticLease.ino
@@ -23,6 +23,10 @@ void loop() {
 const char *ssid = "ESPap";
 const char *password = "thereisnospoon";
 
+/* List of mac address for static lease */
+uint8 mac_CAM[6] = { 0x00, 0x0C, 0x43, 0x01, 0x60, 0x15 };
+uint8 mac_PC[6] = { 0xb4, 0x52, 0x7e, 0x9a, 0x19, 0xa5 };
+
 ESP8266WebServer server(80);
 
 /* Set the IP Address you want for your AP */
@@ -44,7 +48,6 @@ void handleRoot() {
   result += String(number_client);
   result += "</h1></br>";
   while (stat_info != NULL) {
-
     result += "Client ";
     result += String(i);
     result += " = ";
@@ -53,7 +56,6 @@ void handleRoot() {
     sprintf(wifiClientMac, "%02X:%02X:%02X:%02X:%02X:%02X", MAC2STR(stat_info->bssid));
     result += wifiClientMac;
     result += "</br>";
-
     stat_info = STAILQ_NEXT(stat_info, next);
     i++;
   }
@@ -63,10 +65,6 @@ void handleRoot() {
 }
 
 void setup() {
-  /* List of mac address for static lease */
-  uint8 mac_CAM[6] = { 0x00, 0x0C, 0x43, 0x01, 0x60, 0x15 };
-  uint8 mac_PC[6] = { 0xb4, 0x52, 0x7e, 0x9a, 0x19, 0xa5 };
-
   Serial.begin(115200);
   Serial.println();
   Serial.println("Configuring access point...");
@@ -75,15 +73,18 @@ void setup() {
   WiFi.persistent(false);
 
   WiFi.mode(WIFI_AP);
+
   /* Configure AP with IP = 192.168.0.1 / Gateway = 192.168.0.1 / Subnet = 255.255.255.0
      if you specify the ESP8266's IP-address with 192.168.0.1, the function softAPConfig() sets the DHCP-range as 192.168.0.100 - 192.168.0.200
   */
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+
+  /* Start Access Point. You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAP(ssid, password);
+
   /* Setup your static leases.
-
-     As it depend from your first address, and need to be done BEFORE any request from client,
+      As it depend from your first address, and need to be done BEFORE any request from client,
      this need to be specified after WiFi.softAPConfig() and before WiFi.softAP().
-
      first call to wifi_softap_add_dhcps_lease() will setup first IP address of the range
      second call to wifi_softap_add_dhcps_lease() will setup second IP address of the range
      ...
@@ -91,10 +92,10 @@ void setup() {
   */
   wifi_softap_add_dhcps_lease(mac_CAM);  // always 192.168.0.100
   wifi_softap_add_dhcps_lease(mac_PC);   // always 192.168.0.101
-  /* Start Access Point. You can remove the password parameter if you want the AP to be open. */
-  WiFi.softAP(ssid, password);
+
   Serial.print("AP IP address: ");
   Serial.println(WiFi.softAPIP());
+
   /* Setup HTTP Server */
   server.on("/", handleRoot);
   server.begin();
