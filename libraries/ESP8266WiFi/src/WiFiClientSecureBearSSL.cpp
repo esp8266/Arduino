5b2ae10ebf64593b8af76ff4b3b4d0113bc56527
johnm545 <52096239+johnm545@users.noreply.github.com>
2019-08-26 22:32:45 +1000

    Fix WiFiClientSecure::available blocking
    Added a check of WiFiClient::availableForWrite to prevent blocking writes when the _run_until blocking flag is false

--- libraries/ESP8266WiFi/src/WiFiClientSecureBearSSL.cpp
+++ libraries/ESP8266WiFi/src/WiFiClientSecureBearSSL.cpp
@@ -437,12 +437,9 @@ int WiFiClientSecure::_run_until(unsigned target, bool blocking) {
     DEBUG_BSSL("_run_until: Not connected\n");
     return -1;
   }
-  for (int no_work = 0; blocking || no_work < 2;) {
-    if (blocking) {
-      // Only for blocking operations can we afford to yield()
-      optimistic_yield(100);
-    }
-
+  for (int no_work = 0; blocking || no_work < 2;) {    
+    optimistic_yield(100);
+    
     int state;
     state = br_ssl_engine_current_state(_eng);
     if (state & BR_SSL_CLOSED) {
@@ -461,8 +458,19 @@ int WiFiClientSecure::_run_until(unsigned target, bool blocking) {
       unsigned char *buf;
       size_t len;
       int wlen;
+      int availForWrite;
 
       buf = br_ssl_engine_sendrec_buf(_eng, &len);
+      availForWrite = WiFiClient::availableForWrite();
+      
+      if (!blocking && len > availForWrite) {
+        /* 
+           writes on WiFiClient will block if len > availableForWrite()
+           this is needed to prevent available() calls from blocking
+           on dropped connections 
+        */
+        len = availForWrite;
+      }	  
       wlen = WiFiClient::write(buf, len);
       if (wlen <= 0) {
         /*
