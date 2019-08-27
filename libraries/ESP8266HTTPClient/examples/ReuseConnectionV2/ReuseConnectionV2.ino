3981b18a095fb67fe3ebec3056f36a571bbf4d9b
Jeroen88 <info@el-mago.nl>
2019-08-27 21:17:16 +0200

    Fix Travis formatting issue for example ReuseConnectionV2.ino

--- libraries/ESP8266HTTPClient/examples/ReuseConnectionV2/ReuseConnectionV2.ino
+++ libraries/ESP8266HTTPClient/examples/ReuseConnectionV2/ReuseConnectionV2.ino
@@ -47,30 +47,30 @@ void setup() {
 }
 
 void loop() {
-    for(int i = 0; i < 10; i++) {
-        Serial.printf("Reuse connection example, GET url for the %d time\n", i + 1);
-        int httpCode = http.GET();
-        if (httpCode > 0) {
-            Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+  for(int i = 0; i < 10; i++) {
+    Serial.printf("Reuse connection example, GET url for the %d time\n", i + 1);
+    int httpCode = http.GET();
+    if (httpCode > 0) {
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
 
-            // file found at server
-            if (httpCode == HTTP_CODE_OK) {
-                http.writeToStream(&Serial);
-            }
-        } else {
-            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-            // Something went wrong with the connection, try to reconnect
-            http.end();
-            http.begin(client, "http://jigsaw.w3.org/HTTP/connection.html");
-            //http.begin(client, "jigsaw.w3.org", 80, "/HTTP/connection.html");
-        }
-
-        Serial.println("\n\n\nWait 5 second...\n");
-        delay(5000);
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        http.writeToStream(&Serial);
+      }
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      // Something went wrong with the connection, try to reconnect
+      http.end();
+      http.begin(client, "http://jigsaw.w3.org/HTTP/connection.html");
+      //http.begin(client, "jigsaw.w3.org", 80, "/HTTP/connection.html");
     }
 
-    http.end();
+    Serial.println("\n\n\nWait 5 second...\n");
+    delay(5000);
+  }
 
-    Serial.println("Done testing, now wait forever");
-    for(;;) delay(100); // Wait forever
+  http.end();
+
+  Serial.println("Done testing, now wait forever");
+  for(;;) delay(100); // Wait forever
 }
