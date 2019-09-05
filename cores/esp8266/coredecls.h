6e1c44e5455be68a35d760a6371badc723504189
david gauchard <gauchard@laas.fr>
2019-09-05 02:14:23 +0200

    unify crc32 calls

--- cores/esp8266/coredecls.h
+++ cores/esp8266/coredecls.h
@@ -21,7 +21,7 @@ void settimeofday_cb (void (*cb)(void));
 void disable_extra4k_at_link_time (void) __attribute__((noinline));
 
 uint32_t sqrt32 (uint32_t n);
-uint32_t crc32 (const void* data, size_t length);
+uint32_t crc32 (const void* data, size_t length, uint32_t crc = 0xffffffff);
 
 #ifdef __cplusplus
 }
--- cores/esp8266/crc32.cpp
+++ cores/esp8266/crc32.cpp
@@ -1,11 +1,30 @@
+/*
+ crc32.cpp
+
+ Copyright (c) 2015 Ivan Grokhotkov. All rights reserved.
+ This file is part of the esp8266 core for Arduino environment.
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #include "coredecls.h"
 
-// taken from esp8266/examples/RTCUserMemory/RTCUserMemory.ino
-uint32_t crc32 (const void* data, size_t length)
+// moved from core_esp8266_eboot_command.cpp
+uint32_t crc32 (const void* data, size_t length, uint32_t crc /*= 0xffffffff*/)
 {
     const uint8_t* ldata = (const uint8_t*)data;
-    uint32_t crc = 0xffffffff;
     while (length--)
     {
         uint8_t c = *ldata++;
