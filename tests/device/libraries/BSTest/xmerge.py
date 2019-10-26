df9993ee16394a29400ed5e0166727b3746d3b37
Earle F. Philhower, III <earlephilhower@yahoo.com>
2019-10-26 11:18:41 -0700

    Add appropriate attribution, license to xunitmerge
    Add like to the original sources with the author's license to the
    copied/fixed xunitmerge files.

--- tests/device/libraries/BSTest/xmerge.py
+++ tests/device/libraries/BSTest/xmerge.py
@@ -1,4 +1,28 @@
-from __future__ import unicode_literals, print_function
+# Cloned from https://github.com/miki725/xunitmerge
+# to fix a Python3 error.
+#
+# xunitmerge is MIT licensed by Miroslav Shubernetskiy https://github.com/miki725
+#
+# The MIT License (MIT)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 from contextlib import contextmanager
 from xml.etree import ElementTree as etree
 from xml.sax.saxutils import quoteattr
