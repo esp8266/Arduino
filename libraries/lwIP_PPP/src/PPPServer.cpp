574be1310c063de1f339770081d652c3c6e3813b
david gauchard <gauchard@laas.fr>
2019-10-26 11:20:32 +0200

    ppp server updates

--- libraries/lwIP_PPP/src/PPPServer.cpp
+++ libraries/lwIP_PPP/src/PPPServer.cpp
@@ -1,45 +1,50 @@
 
-// testing on linux:
-// sudo /usr/sbin/pppd /dev/ttyUSB1 4800 noipdefault nocrtscts local defaultroute noauth nodetach debug dump
-// sudo /usr/sbin/pppd /dev/ttyUSB1 4800 noipdefault nocrtscts local defaultroute noauth
+// This is still beta / a work in progress
 
-// proxy arp needed
+// testing on linux:
+// sudo /usr/sbin/pppd /dev/ttyUSB1 38400 noipdefault nocrtscts local defaultroute noauth nodetach debug dump
+// sudo /usr/sbin/pppd /dev/ttyUSB1 38400 noipdefault nocrtscts local defaultroute noauth
+
+// proxy arp is needed but we don't have it
 // http://lwip.100.n7.nabble.com/PPP-proxy-arp-support-tp33286p33345.html
+// using NAT instead (see in example)
 
 #include <Arduino.h>
 #include <Schedule.h>
 #include <IPAddress.h>
 #include <lwip/dns.h>
 
-#include "../PPPServer.h"
+#include "PPPServer.h"
 
-PPP::PPP (Stream* sio): _sio(sio)
+PPPServer::PPPServer(Stream* sio): _sio(sio), _cb(netif_status_cb_s), _enabled(false)
 {
 }
 
-err_t PPP::handlePackets ()
+bool PPPServer::handlePackets()
 {
     int avail;
     if ((avail = _sio->available()) > 0)
     {
-        //XXX USE BSTREAM
-        if (avail > 128)
-            avail = 128;
-        uint8_t buffer[avail];
-        avail = _sio->readBytes(buffer, avail);
-        pppos_input(_ppp, buffer, avail);
+        // XXX block peeking would be useful here
+        if (avail > _bufsize)
+        {
+            avail = _bufsize;
+        }
+        avail = _sio->readBytes(_buf, avail);
+        pppos_input(_ppp, _buf, avail);
     }
-    return ERR_OK;
+    return _enabled;
 }
 
-void PPP::link_status_cb_s (ppp_pcb* pcb, int err_code, void* ctx)
+void PPPServer::link_status_cb_s(ppp_pcb* pcb, int err_code, void* ctx)
 {
     //PPP* This = static_cast<PPP*>(ctx);
     netif* nif = ppp_netif(pcb);
 
-    switch(err_code) {
+    switch (err_code)
+    {
     case PPPERR_NONE:               /* No error. */
-        {
+    {
 #if LWIP_DNS
         const ip_addr_t *ns;
 #endif /* LWIP_DNS */
@@ -62,77 +67,74 @@ void PPP::link_status_cb_s (ppp_pcb* pcb, int err_code, void* ctx)
 #if PPP_IPV6_SUPPORT
         ets_printf("   our6_ipaddr = %s\n\r", ip6addr_ntoa(netif_ip6_addr(nif, 0)));
 #endif /* PPP_IPV6_SUPPORT */
-        }
-        break;
+    }
+    break;
 
     case PPPERR_PARAM:             /* Invalid parameter. */
-       ets_printf("ppp_link_status_cb: PPPERR_PARAM\n");
+        ets_printf("ppp_link_status_cb: PPPERR_PARAM\n");
         break;
 
     case PPPERR_OPEN:              /* Unable to open PPP session. */
-       ets_printf("ppp_link_status_cb: PPPERR_OPEN\n");
+        ets_printf("ppp_link_status_cb: PPPERR_OPEN\n");
         break;
 
     case PPPERR_DEVICE:            /* Invalid I/O device for PPP. */
-       ets_printf("ppp_link_status_cb: PPPERR_DEVICE\n");
+        ets_printf("ppp_link_status_cb: PPPERR_DEVICE\n");
         break;
 
     case PPPERR_ALLOC:             /* Unable to allocate resources. */
-       ets_printf("ppp_link_status_cb: PPPERR_ALLOC\n");
+        ets_printf("ppp_link_status_cb: PPPERR_ALLOC\n");
         break;
 
     case PPPERR_USER:              /* User interrupt. */
-       ets_printf("ppp_link_status_cb: PPPERR_USER\n");
+        ets_printf("ppp_link_status_cb: PPPERR_USER\n");
         break;
 
     case PPPERR_CONNECT:           /* Connection lost. */
-       ets_printf("ppp_link_status_cb: PPPERR_CONNECT\n");
+        ets_printf("ppp_link_status_cb: PPPERR_CONNECT\n");
         break;
 
     case PPPERR_AUTHFAIL:          /* Failed authentication challenge. */
-       ets_printf("ppp_link_status_cb: PPPERR_AUTHFAIL\n");
+        ets_printf("ppp_link_status_cb: PPPERR_AUTHFAIL\n");
         break;
 
     case PPPERR_PROTOCOL:          /* Failed to meet protocol. */
-       ets_printf("ppp_link_status_cb: PPPERR_PROTOCOL\n");
+        ets_printf("ppp_link_status_cb: PPPERR_PROTOCOL\n");
         break;
 
     case PPPERR_PEERDEAD:          /* Connection timeout. */
-       ets_printf("ppp_link_status_cb: PPPERR_PEERDEAD\n");
+        ets_printf("ppp_link_status_cb: PPPERR_PEERDEAD\n");
         break;
 
     case PPPERR_IDLETIMEOUT:       /* Idle Timeout. */
-       ets_printf("ppp_link_status_cb: PPPERR_IDLETIMEOUT\n");
+        ets_printf("ppp_link_status_cb: PPPERR_IDLETIMEOUT\n");
         break;
 
     case PPPERR_CONNECTTIME:       /* PPPERR_CONNECTTIME. */
-       ets_printf("ppp_link_status_cb: PPPERR_CONNECTTIME\n");
+        ets_printf("ppp_link_status_cb: PPPERR_CONNECTTIME\n");
         break;
 
     case PPPERR_LOOPBACK:          /* Connection timeout. */
-       ets_printf("ppp_link_status_cb: PPPERR_LOOPBACK\n");
+        ets_printf("ppp_link_status_cb: PPPERR_LOOPBACK\n");
         break;
 
     default:
-       ets_printf("ppp_link_status_cb: unknown errCode %d\n", err_code);
+        ets_printf("ppp_link_status_cb: unknown errCode %d\n", err_code);
         break;
     }
 }
 
-u32_t PPP::output_cb_s (ppp_pcb* pcb, u8_t* data, u32_t len, void* ctx)
+u32_t PPPServer::output_cb_s(ppp_pcb* pcb, u8_t* data, u32_t len, void* ctx)
 {
     (void)pcb;
-    return static_cast<PPP*>(ctx)->_sio->write(data, len);
+    (void)ctx;
+    return static_cast<PPPServer*>(ctx)->_sio->write(data, len);
 }
 
-void PPP::netif_status_cb_s (netif* nif)
+void PPPServer::netif_status_cb_s(netif* nif)
 {
-//ip4_addr_get_u32(ip_2_ip4(&nif->ip_addr)) = IPAddress(192,168,0,1).v4();
-//ip4_addr_get_u32(ip_2_ip4(&nif->netmask)) = IPAddress(255,255,255,0).v4();
-//ip4_addr_get_u32(ip_2_ip4(&nif->gw)) = IPAddress(192,168,0,2).v4();
-
     ets_printf("PPPNETIF: %c%c%d is %s\n", nif->name[0], nif->name[1], nif->num,
-              netif_is_up(nif) ? "UP" : "DOWN");
+               netif_is_up(nif) ? "UP" : "DOWN");
 #if LWIP_IPV4
     ets_printf("IPV4: Host at %s ", ip4addr_ntoa(netif_ip4_addr(nif)));
     ets_printf("mask %s ", ip4addr_ntoa(netif_ip4_netmask(nif)));
@@ -144,36 +146,45 @@ void PPP::netif_status_cb_s (netif* nif)
     ets_printf("FQDN: %s\n", netif_get_hostname(nif));
 }
 
-bool PPP::begin ()
+bool PPPServer::begin(const IPAddress& ourAddress, const IPAddress& peer)
 {
     // lwip2-src/doc/ppp.txt
-    
-    _ppp = pppos_create(&_netif, PPP::output_cb_s, PPP::link_status_cb_s, this);
-    if (!_ppp)
-        return false;
 
-    ip4_addr_t addr;
-    
-    // PPP server
-    IP4_ADDR(&addr, 10,0,1,225);
-    ppp_set_ipcp_ouraddr(_ppp, &addr);
-    IP4_ADDR(&addr, 10,0,1,129);
-    ppp_set_ipcp_hisaddr(_ppp, &addr);
-    IP4_ADDR(&addr, 10,0,1,254);
-    ppp_set_ipcp_dnsaddr(_ppp, 0, &addr);
+    _ppp = pppos_create(&_netif, PPPServer::output_cb_s, PPPServer::link_status_cb_s, this);
+    if (!_ppp)
+    {
+        return false;
+    }
+
+    ppp_set_ipcp_ouraddr(_ppp, ip_2_ip4((const ip_addr_t*)ourAddress));
+    ppp_set_ipcp_hisaddr(_ppp, ip_2_ip4((const ip_addr_t*)peer));
+
+    //ip4_addr_t addr;
+    //IP4_ADDR(&addr, 10,0,1,254);
+    //ppp_set_ipcp_dnsaddr(_ppp, 0, &addr);
+
     //ppp_set_auth(_ppp, PPPAUTHTYPE_ANY, "login", "password");
     //ppp_set_auth_required(_ppp, 1);
-    //ppp_set_silent(_ppp, 1);
-    
-    ppp_listen(_ppp);
 
-    //netif_set_status_callback(&_netif, PPP::netif_status_cb_s);
-    
-    if (!schedule_recurrent_function_us([&]() { this->handlePackets(); return true; }, 1000))
+    ppp_set_silent(_ppp, 1);
+    ppp_listen(_ppp);
+    netif_set_status_callback(&_netif, _cb);
+
+    _enabled = true;
+    if (!schedule_recurrent_function_us([&]()
+{
+    return this->handlePackets();
+    }, 1000))
     {
         netif_remove(&_netif);
         return false;
     }
 
     return true;
+}
+
+void PPPServer::stop()
+{
+    _enabled = false;
+    ppp_close(_ppp, 0);
 }
--- libraries/lwIP_PPP/src/PPPServer.h
+++ libraries/lwIP_PPP/src/PPPServer.h
@@ -1,63 +1,77 @@
 /*
- * Redistribution and use in source and binary forms, with or without modification, 
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission. 
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED 
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
- * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
- * OF SUCH DAMAGE.
- *
- * This file is part of the lwIP TCP/IP stack.
- * 
- * Author: Dirk Ziegelmeier <dziegel@gmx.de>
- *
- */
+    Redistribution and use in source and binary forms, with or without modification,
+    are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    3. The name of the author may not be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+    SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+    OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+    IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+    OF SUCH DAMAGE.
+
+    This file is part of the lwIP TCP/IP stack.
+
+    Author: Dirk Ziegelmeier <dziegel@gmx.de>
+
+*/
 
 
-#ifndef __PPPIF_H
-#define __PPPIF_H
+#ifndef __PPPSERVER_H
+#define __PPPSERVER_H
 
 #include <Arduino.h>
-
+#include <IPAddress.h>
 #include <lwip/netif.h>
 #include <netif/ppp/ppp.h>
 #include <netif/ppp/pppos.h>
 
-class PPP
+class PPPServer
 {
 public:
 
-    PPP (Stream* sio);
-    
-    bool begin ();
-    
+    PPPServer(Stream* sio);
+
+    bool begin(const IPAddress& ourAddress, const IPAddress& peer = IPAddress(172, 31, 255, 254));
+    void stop();
+
+    void ifUpCb(void (*cb)(netif*))
+    {
+        _cb = cb;
+    }
+    const ip_addr_t* getPeerAddress() const
+    {
+        return &_netif.gw;
+    }
+
 protected:
 
+    static constexpr size_t _bufsize = 128;
     Stream* _sio;
     ppp_pcb* _ppp;
     netif _netif;
-    
-    // called on a regular basis or on interrupt
-    err_t handlePackets ();
-    
-    static u32_t output_cb_s (ppp_pcb* pcb, u8_t* data, u32_t len, void* ctx);
-    static void link_status_cb_s (ppp_pcb* pcb, int err_code, void* ctx);
-    static void netif_status_cb_s (netif* nif);
+    void (*_cb)(netif*);
+    uint8_t _buf[_bufsize];
+    bool _enabled;
+
+    // feed ppp from stream - to call on a regular basis or on interrupt
+    bool handlePackets();
+
+    static u32_t output_cb_s(ppp_pcb* pcb, u8_t* data, u32_t len, void* ctx);
+    static void link_status_cb_s(ppp_pcb* pcb, int err_code, void* ctx);
+    static void netif_status_cb_s(netif* nif);
 
 };
 
-#endif // __PPPIF_H
+#endif // __PPPSERVER_H
