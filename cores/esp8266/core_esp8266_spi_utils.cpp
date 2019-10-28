63cbbf72ed1f3964232e74aac802d9edd1cca4a6
Mike Nix <mnix@wanm.com.au>
2019-10-28 23:29:41 +0800

    Comments, formatting and variable renames
    Added a number of comments to better explain the code and improved the
    formatting.
    
    Also renamed some variables for consistency.

--- cores/esp8266/core_esp8266_spi_utils.cpp
+++ cores/esp8266/core_esp8266_spi_utils.cpp
@@ -40,23 +40,29 @@ extern "C" uint32_t Wait_SPI_Idle(SpiFlashChip *fc);
  * Kept in a separate function to aid with precaching
  * PRECACHE_* saves having to make the function IRAM_ATTR.
  *
+ * spiIfNum needs to be volatile to keep the optimiser from
+ * deciding it can be treated as a constant (due to this being a
+ * static function only called with spiIfNum set to 0)
+ *
  * Note: if porting to ESP32 mosi/miso bits are set in 2 registers, not 1.
  */
 static SpiOpResult PRECACHE_ATTR
 _SPICommand(volatile uint32_t spiIfNum,
             uint32_t spic,uint32_t spiu,uint32_t spiu1,uint32_t spiu2,
-            uint32_t *data,uint32_t write_words,uint32_t read_words)
+            uint32_t *data,uint32_t writeWords,uint32_t readWords)
 { 
-  if (spiIfNum>1) return SPI_RESULT_ERR;
+  if (spiIfNum>1)
+     return SPI_RESULT_ERR;
 
   // force SPI register access via base+offest. 
   // Prevents loading individual address constants from flash.
   uint32_t *spibase = (uint32_t*)(spiIfNum ? &(SPI1CMD) : &(SPI0CMD));
-  #define SPIREG(reg) *((volatile uint32_t *)(spibase+(&(reg) - &(SPI0CMD))))
+  #define SPIREG(reg) (*((volatile uint32_t *)(spibase+(&(reg) - &(SPI0CMD)))))
 
   // preload any constants and functions we need into variables
-  // Everything must be volatile or the optimizer can treat them as 
-  // constants, resulting in the flash reads we're trying to avoid
+  // Everything defined here must be volatile or the optimizer can
+  // treat them as constants, resulting in the flash reads we're
+  // trying to avoid
   void    *(* volatile memcpyp)(void *,const void *, size_t) = memcpy;
   uint32_t (* volatile Wait_SPI_Idlep)(SpiFlashChip *) = Wait_SPI_Idle;
   volatile SpiFlashChip *fchip=flashchip;
@@ -68,9 +74,11 @@ _SPICommand(volatile uint32_t spiIfNum,
      Wait_SPI_Idlep((SpiFlashChip *)fchip);
   }
   
-  uint32_t old_spi_usr = SPIREG(SPI0U);
-  uint32_t old_spi_usr2= SPIREG(SPI0U2);
-  uint32_t old_spi_c   = SPIREG(SPI0C);
+  // preserve essential controller state such as incoming/outgoing
+  // data lengths and IO mode.
+  uint32_t oldSPI0U = SPIREG(SPI0U);
+  uint32_t oldSPI0U2= SPIREG(SPI0U2);
+  uint32_t oldSPI0C = SPIREG(SPI0C);
 
   //SPI0S &= ~(SPISE|SPISBE|SPISSE|SPISCD);
   SPIREG(SPI0C) = spic;
@@ -78,26 +86,27 @@ _SPICommand(volatile uint32_t spiIfNum,
   SPIREG(SPI0U1)= spiu1;
   SPIREG(SPI0U2)= spiu2;
 
-  if (write_words>0) {
+  if (writeWords>0) {
      // copy the outgoing data to the SPI hardware
-     memcpyp((void*)&(SPIREG(SPI0W0)),data,write_words*4);
+     memcpyp((void*)&(SPIREG(SPI0W0)),data,writeWords*4);
   }
 
   // Start the transfer
   SPIREG(SPI0CMD) = spicmdusr;
 
-  // wait for the command to complete
+  // wait for the command to complete (typically only 1-3 iterations)
   uint32_t timeout = 1000;
-  while ((SPIREG(SPI0CMD) & spicmdusr) && timeout--) {}
+  while ((SPIREG(SPI0CMD) & spicmdusr) && timeout--);
 
-  if ((read_words>0) && (timeout>0)) {
+  if ((readWords>0) && (timeout>0)) {
      // copy the response back to the buffer
-     memcpyp(data,(void *)&(SPIREG(SPI0W0)),read_words*4);
+     memcpyp(data,(void *)&(SPIREG(SPI0W0)),readWords*4);
   }
 
-  SPIREG(SPI0U) = old_spi_usr;
-  SPIREG(SPI0U2)= old_spi_usr2;
-  SPIREG(SPI0C) = old_spi_c;
+  // Restore saved registers
+  SPIREG(SPI0U) = oldSPI0U;
+  SPIREG(SPI0U2)= oldSPI0U2;
+  SPIREG(SPI0C) = oldSPI0C;
   
   PRECACHE_END();
   return (timeout>0 ? SPI_RESULT_OK : SPI_RESULT_TIMEOUT);
@@ -106,29 +115,54 @@ _SPICommand(volatile uint32_t spiIfNum,
 
 /*  SPI0Command: send a custom SPI command.
  *  This part calculates register values and passes them to _SPI0Command().
+ *  Parameters:
+ *	cmd	The command byte (first 8 bits) to send to the SPI device
+ *	*data	The buffer containing the outgoing data for the SPI bus.
+ *		The data is expected to be mosi_bits long, and the buffer
+ *		is overwritten by the incoming bus data, which will be
+ *		miso_bits long.
+ *	mosi_bits
+ *		Number of bits to be sent after the command byte.
+ *	miso_bits
+ *		Number of bits to read from the SPI bus after the outgoing
+ *		data has been sent.
  */
 SpiOpResult SPI0Command(uint8_t cmd, uint32_t *data, uint32_t mosi_bits, uint32_t miso_bits) {
-  if (mosi_bits>(64*8)) return SPI_RESULT_ERR;
-  if (miso_bits>(64*8)) return SPI_RESULT_ERR;
+  if (mosi_bits>(64*8))
+     return SPI_RESULT_ERR;
+  if (miso_bits>(64*8))
+     return SPI_RESULT_ERR;
 
+  // Calculate the number of data words (aka registers) that need to be copied
+  // to/from the SPI controller.
   uint32_t mosi_words=mosi_bits/32;
   uint32_t miso_words=miso_bits/32;
-  if (mosi_bits % 32 != 0) mosi_words++;
-  if (miso_bits % 32 != 0) miso_words++;
+  if (mosi_bits % 32 != 0)
+     mosi_words++;
+  if (miso_bits % 32 != 0)
+     miso_words++;
 
+  // Select user defined command mode in the controller
   uint32_t spiu=SPIUCOMMAND; //SPI_USR_COMMAND
+
+  // Set the command byte to send
   uint32_t spiu2 = ((7 & SPIMCOMMAND)<<SPILCOMMAND) | cmd;
+
   uint32_t spiu1 = 0;
   if (mosi_bits>0) {
+     // set the number of outgoing data bits to send
      spiu1 |= ((mosi_bits-1) & SPIMMOSI) << SPILMOSI;
      spiu  |= SPIUMOSI;  // SPI_USR_MOSI
   }
   if (miso_bits>0) {
+     // set the number of incoming bits to read
      spiu1 |= ((miso_bits-1) & SPIMMISO) << SPILMISO;
      spiu  |= SPIUMISO;  // SPI_USR_MISO
   }
 
   uint32_t spic = SPI0C;
+  // Select the most basic IO mode for maximum compatibility
+  // Some flash commands are only available in this mode.
   spic &= ~(SPICQIO | SPICDIO | SPICQOUT | SPICDOUT | SPICAHB | SPICFASTRD);
   spic |= (SPICRESANDRES | SPICSHARE | SPICWPR | SPIC2BSE);
 
