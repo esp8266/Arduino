dfb6a0057cb85ff3370ab818794d1165c305f816
David Gauchard <gauchard@laas.fr>
2019-10-30 11:24:12 +0100

    fix emulation on host

--- tests/host/common/flash_hal_mock.h
+++ tests/host/common/flash_hal_mock.h
@@ -5,15 +5,17 @@
 
 extern "C"
 {
-    extern uint32_t s_phys_addr;
-    extern uint32_t s_phys_size;
-    extern uint32_t s_phys_page;
-    extern uint32_t s_phys_block;
-    extern uint8_t* s_phys_data;
-}
+
+extern uint32_t s_phys_addr;
+extern uint32_t s_phys_size;
+extern uint32_t s_phys_page;
+extern uint32_t s_phys_block;
+extern uint8_t* s_phys_data;
 
 extern int32_t flash_hal_read(uint32_t addr, uint32_t size, uint8_t *dst);
 extern int32_t flash_hal_write(uint32_t addr, uint32_t size, const uint8_t *src);
 extern int32_t flash_hal_erase(uint32_t addr, uint32_t size);
+
+}
 
 #endif
