5eedc90ee474ef33d3610b125f376e62f0da780e
gnu-emu <57782560+gnu-emu@users.noreply.github.com>
2019-11-15 12:24:08 +0100

    Update upload.py

--- tools/upload.py
+++ tools/upload.py
@@ -20,53 +20,70 @@ except:
     sys.stderr.write("pyserial or esptool directories not found next to this upload.py tool.\n")
     sys.exit(1)
 
+fakeargs = [];
 cmdline = []
 write_option = ''
 erase_addr = ''
 erase_len = ''
 
-while len(sys.argv):
-    thisarg = sys.argv.pop(0)
+if sys.argv[len(sys.argv)-1] == '--end':
 
-    # We silently replace the 921kbaud setting with 460k to enable backward
-    # compatibility with the old esptool-ck.exe.  Esptool.py doesn't seem
-    # work reliably at 921k, but is still significantly faster at 460kbaud.
-    if thisarg == "921600":
-        thisarg = "460800"
+    while len(sys.argv):
+        if sys.argv[0] == '--end':
+            esptool.main(fakeargs)
+            sys.argv.pop(0) # Remove --end
+            fakeargs = []
+        else:
+            # We silently replace the 921kbaud setting with 460k to enable backward
+            # compatibility with the old esptool-ck.exe.  Esptool.py doesn't seem
+            # work reliably at 921k, but is still significantly faster at 460kbaud.
+            thisarg = sys.argv.pop(0)
+            if thisarg == "921600":
+                thisarg = "460800"
+            fakeargs = fakeargs + [thisarg]
+else:
 
-    # 'erase_flash' command is translated to the write_flash --erase-all option
-    # https://github.com/esp8266/Arduino/issues/6755#issuecomment-553208688
-    if thisarg == "erase_flash":
-        write_option = '--erase-all'
-        thisarg = ''
+    while len(sys.argv):
+        thisarg = sys.argv.pop(0)
 
-    if thisarg == 'erase_region':
-        erase_addr = sys.argv.pop(0)
-        erase_len = sys.argv.pop(0)
-        thisarg = ''
+        # We silently replace the 921kbaud setting with 460k to enable backward
+        # compatibility with the old esptool-ck.exe.  Esptool.py doesn't seem
+        # work reliably at 921k, but is still significantly faster at 460kbaud.
+        if thisarg == "921600":
+            thisarg = "460800"
 
-    if os.path.isfile(thisarg):
-        binary = thisarg
-        thisarg = ''
+        # 'erase_flash' command is translated to the write_flash --erase-all option
+        # https://github.com/esp8266/Arduino/issues/6755#issuecomment-553208688
+        if thisarg == "erase_flash":
+            write_option = '--erase-all'
+            thisarg = ''
 
-    if len(thisarg):
-        cmdline = cmdline + [thisarg]
+        if thisarg == 'erase_region':
+            erase_addr = sys.argv.pop(0)
+            erase_len = sys.argv.pop(0)
+            thisarg = ''
 
-cmdline = cmdline + ['write_flash']
-if len(write_option):
-    cmdline = cmdline + [write_option]
-cmdline = cmdline + ['0x0', binary]
+        if os.path.isfile(thisarg):
+            binary = thisarg
+            thisarg = ''
 
-erase_file = ''
-if len(erase_addr):
-    # generate temporary empty (0xff) file
-    eraser = tempfile.mkstemp()
-    erase_file = eraser[1]
-    os.write(eraser[0], bytearray([255] * int(erase_len, 0)))
-    os.close(eraser[0])
-    cmdline = cmdline + [ erase_addr, erase_file ]
+        if len(thisarg):
+            cmdline = cmdline + [thisarg]
 
-esptool.main(cmdline)
+    cmdline = cmdline + ['write_flash']
+    if len(write_option):
+        cmdline = cmdline + [write_option]
+    cmdline = cmdline + ['0x0', binary]
 
-if len(erase_file):
-    os.remove(erase_file)
+    erase_file = ''
+    if len(erase_addr):
+        # generate temporary empty (0xff) file
+        eraser = tempfile.mkstemp()
+        erase_file = eraser[1]
+        os.write(eraser[0], bytearray([255] * int(erase_len, 0)))
+        os.close(eraser[0])
+        cmdline = cmdline + [ erase_addr, erase_file ]
+    esptool.main(cmdline)
+
+    if len(erase_file):
+        os.remove(erase_file)
