85bdc6ebca68f6b94ac464a95b5a1ad0af78be5b
david gauchard <gauchard@laas.fr>
2019-12-02 23:56:23 +0100

    Update PostHttpClient.ino
    style, comments

--- libraries/ESP8266HTTPClient/examples/PostHttpClient/PostHttpClient.ino
+++ libraries/ESP8266HTTPClient/examples/PostHttpClient/PostHttpClient.ino
@@ -1,72 +1,84 @@
 /**
- * PostHTTPClient.ino
- *
- *  Created on: 21.11.2016
- *
- */
+   PostHTTPClient.ino
 
-#include <Arduino.h>
+    Created on: 21.11.2016
+
+*/
 
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-
 #include <ESP8266HTTPClient.h>
 
 #define USE_SERIAL Serial
 
-ESP8266WiFiMulti WiFiMulti;
+/* this can be run with an emulated server on host:
+        cd esp8266-core-root-dir
+        cd tests/host
+        make ../../libraries/ESP8266WebServer/examples/PostServer/PostServer
+        bin/PostServer/PostServer
+   then put your PC's IP address in SERVER_IP below, port 9080 (instead of default 80):
+*/
+//#define SERVER_IP "10.0.1.7:9080" // PC address with emulation on host
+#define SERVER_IP "192.168.1.42"
+
+#ifndef STASSID
+#define STASSID "your-ssid"
+#define STAPSK  "your-password"
+#endif
 
 void setup() {
 
-    USE_SERIAL.begin(115200);
-   // USE_SERIAL.setDebugOutput(true);
+  USE_SERIAL.begin(115200);
 
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-    USE_SERIAL.println();
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+  USE_SERIAL.println();
 
-    for(uint8_t t = 4; t > 0; t--) {
-        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
-        USE_SERIAL.flush();
-        delay(1000);
-    }
+  WiFi.begin(STASSID, STAPSK);
 
-    WiFiMulti.addAP("SSID", "PASSWORD");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    USE_SERIAL.print(".");
+  }
+  USE_SERIAL.println("");
+  USE_SERIAL.print("Connected! IP address: ");
+  USE_SERIAL.println(WiFi.localIP());
 
 }
 
 void loop() {
-    // wait for WiFi connection
-    if((WiFiMulti.run() == WL_CONNECTED)) {
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
 
-        WiFiClient client;
-        HTTPClient http;
+    WiFiClient client;
+    HTTPClient http;
 
-        USE_SERIAL.print("[HTTP] begin...\n");
-        // configure traged server and url
-        http.begin(client, "http://192.168.1.12/post/"); //HTTP
-        http.addHeader("Content-Type", "application/json");
+    USE_SERIAL.print("[HTTP] begin...\n");
+    // configure traged server and url
+    http.begin(client, "http://" SERVER_IP "/postplain/"); //HTTP
+    http.addHeader("Content-Type", "application/json");
 
-        USE_SERIAL.print("[HTTP] POST...\n");
-        // start connection and send HTTP header and body
-        int httpCode = http.POST("{\"hello\":\"world\"}");
+    USE_SERIAL.print("[HTTP] POST...\n");
+    // start connection and send HTTP header and body
+    int httpCode = http.POST("{\"hello\":\"world\"}");
 
-        // httpCode will be negative on error
-        if(httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
-            USE_SERIAL.printf("[HTTP] POST... code: %d\n", httpCode);
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      USE_SERIAL.printf("[HTTP] POST... code: %d\n", httpCode);
 
-            // file found at server
-            if(httpCode == HTTP_CODE_OK) {
-                String payload = http.getString();
-                USE_SERIAL.println(payload);
-            }
-        } else {
-            USE_SERIAL.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
-
-        http.end();
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        const String& payload = http.getString();
+        USE_SERIAL.println("received payload:\n<<");
+        USE_SERIAL.println(payload);
+        USE_SERIAL.println(">>");
+      }
+    } else {
+      USE_SERIAL.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
 
-    delay(10000);
+    http.end();
+  }
+
+  delay(10000);
 }
