f5f767383fa6030e5fdbdc733085084b0ccf39ab
Earle F. Philhower, III <earlephilhower@yahoo.com>
2019-12-11 07:47:58 -0800

    Remove using std::abs which conflicted with C lib headers

--- cores/esp8266/Arduino.h
+++ cores/esp8266/Arduino.h
@@ -240,7 +240,6 @@ const int TIM_DIV265 __attribute__((deprecated, weak)) = TIM_DIV256;
 #include "Updater.h"
 #include "debug.h"
 
-using std::abs;
 using std::min;
 using std::max;
 using std::round;
