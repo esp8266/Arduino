0827917ff694f582c1a2cee8c2aaf98b4adbf28b
Dirk O. Kaar <dok@dok-net.net>
2020-01-05 01:13:03 +0100

    The default timings were chosen vastly outside specification. On COTS servos, this causes much confusion for the user, because a wide range of angle starting from 0 degress, and a just a large range of angle leading up to 180 degrees, is dead - in fact, the servo doesn't move at all, from any position.
    Not investigated further for obvious reasons, as this may have also destroyed servos that ran hot trying to abide by the PWM signal but could not mechanically!

--- libraries/Servo/src/Servo.h
+++ libraries/Servo/src/Servo.h
@@ -29,7 +29,7 @@
 //
 //   attach(pin )  - Attaches a servo motor to an i/o pin.
 //   attach(pin, min, max  ) - Attaches to a pin setting min and max values in microseconds
-//   default min is 544, max is 2400
+//   default min is 800, max is 2190
 //
 //   write()     - Sets the servo angle in degrees.  (invalid angle that is valid as pulse in microseconds is treated as microseconds)
 //   writeMicroseconds() - Sets the servo pulse width in microseconds
@@ -46,10 +46,10 @@
 
 // the following are in us (microseconds)
 //
-#define MIN_PULSE_WIDTH       544     // the shortest pulse sent to a servo  
-#define MAX_PULSE_WIDTH      2400     // the longest pulse sent to a servo 
-#define DEFAULT_PULSE_WIDTH  1500     // default pulse width when servo is attached
-#define REFRESH_INTERVAL    20000     // minumim time to refresh servos in microseconds 
+#define MIN_PULSE_WIDTH       800     // the shortest duty cycle sent to a servo  
+#define MAX_PULSE_WIDTH      2190     // the longest duty cycle sent to a servo 
+#define DEFAULT_PULSE_WIDTH  1500     // default duty cycle when servo is attached
+#define REFRESH_INTERVAL    20000     // classic default period to refresh servos in microseconds 
 #define MAX_SERVOS             12
 
 #if !defined(ESP8266)
