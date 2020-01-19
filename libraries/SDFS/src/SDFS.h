61a0dc8723970caf78ef0d7b4cbb73dbcbde9ac1
Earle F. Philhower, III <earlephilhower@yahoo.com>
2020-01-19 10:32:01 -0800

    Correct DOS year/month offset in dateTime callback

--- libraries/SDFS/src/SDFS.h
+++ libraries/SDFS/src/SDFS.h
@@ -212,7 +212,7 @@ public:
     static void dateTimeCB(uint16_t *dosYear, uint16_t *dosTime) {
         time_t now = time(nullptr);
         struct tm *tiempo = localtime(&now);
-        *dosYear = ((tiempo->tm_year - 1980) << 9) | (tiempo->tm_mon << 5) | tiempo->tm_mday;
+        *dosYear = ((tiempo->tm_year - 80) << 9) | ((tiempo->tm_mon + 1) << 5) | tiempo->tm_mday;
         *dosTime = (tiempo->tm_hour << 11) | (tiempo->tm_min << 5) | tiempo->tm_sec;
     }
 
