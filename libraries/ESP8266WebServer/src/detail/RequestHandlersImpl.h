esp8266/Arduino

commit 9f2cfb82184176a0930303be05791d0aaac1e918

fallback onto index.html if index.htm fails (#2614)

libraries/ESP8266WebServer/src/detail/RequestHandlersImpl.h
@@ -101,6 +101,10 @@ public:
 
             // Append whatever follows this URI in request to get the file path.
             path += requestUri.substring(_baseUriLength);
+
+            if (!_fs.exists(path) && path.endsWith(".htm")) {
+                path += "l";
+            }
         }
         DEBUGV("StaticRequestHandler::handle: path=%s, isFile=%d\r\n", path.c_str(), _isFile);
 

commit 96e3d5ff4a16da5476e6f1fdd43c77b2b18b2fea

Fix serach order for index.htm(l)(.gz) files
Fixes #6984

When a directory index is requested with an explicit index.html, follow
the original webserver order and check for: index.htm, index.htm.gz,
index.html, index.html.gz, in order.

Fixes the regressions introduced in 9f2cfb8 and 6768116

libraries/ESP8266WebServer/src/detail/RequestHandlersImpl.h
@@ -110,7 +110,9 @@ public:
             // Append whatever follows this URI in request to get the file path.
             path += requestUri.substring(_baseUriLength);
 
-            if (!_fs.exists(path) && path.endsWith(".htm") && _fs.exists(path + "l")) {
+            // If neither <blah> nor <blah>.gz exist, and <blah> is a file.htm, try it with file.html instead
+            // For the normal case this will give a search order of index.htm, index.htm.gz, index.html, index.html.gz
+            if (!_fs.exists(path) && !_fs.exists(path + ".gz") && path.endsWith(".htm")) {
                 path += "l";
             }
         }