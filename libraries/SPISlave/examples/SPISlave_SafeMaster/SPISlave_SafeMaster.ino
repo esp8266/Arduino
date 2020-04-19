4fbe402dd49ca0588dd8965c2243792a027b23a9
zharmand <zharmand@163.com>
2020-04-19 17:24:27 +0800

    Update SPISlave_SafeMaster.ino
    Modified some comments. ESP work in master mode in this example, so the device connected to it is a slave.

--- libraries/SPISlave/examples/SPISlave_SafeMaster/SPISlave_SafeMaster.ino
+++ libraries/SPISlave/examples/SPISlave_SafeMaster/SPISlave_SafeMaster.ino
@@ -1,6 +1,6 @@
 /*
     SPI Safe Master Demo Sketch
-    Connect the SPI Master device to the following pins on the esp8266:
+    Connect the SPI Slave device to the following pins on the esp8266:
 
     GPIO    NodeMCU   Name  |   Uno
    ===================================
@@ -9,7 +9,7 @@
      12       D6      MISO  |   D12
      14       D5      SCK   |   D13
 
-    Note: If the ESP is booting at a moment when the SPI Master has the Select line HIGH (deselected)
+    Note: If a SPI slave is booting at a moment when the SPI Master has the Select line HIGH (deselected)
     the ESP8266 WILL FAIL to boot!
     This sketch tries to go around this issue by only pulsing the Slave Select line to reset the command
     and keeping the line LOW all other time.
