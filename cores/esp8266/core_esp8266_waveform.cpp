e0fbc99ffa75c72a43715a04a0b81ed792d0abdc
Dirk O. Kaar <dok@dok-net.net>
2020-04-29 20:39:19 +0200

    Updating "now" in the inner loop should lessen interference

--- cores/esp8266/core_esp8266_waveform.cpp
+++ cores/esp8266/core_esp8266_waveform.cpp
@@ -389,10 +389,9 @@ static ICACHE_RAM_ATTR void timer1Interrupt() {
         nextTimerCcy = wave.nextEventCcy;
         nextPin = pin;
       }
-
+      now = ESP.getCycleCount();
     } while ((pin = (pin < endPin) ? pin + 1 : startPin, pin != stopPin));
 
-    now = ESP.getCycleCount();
     const int32_t timerMarginCcys = isrTimeoutCcy - nextTimerCcy;
     busy = timerMarginCcys > 0;
     if (busy) {
