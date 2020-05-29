71e6e2c0ea3b8aa3572c8303be7187bfa8ac4008
M Hightower <27247790+mhightower83@users.noreply.github.com>
2020-05-29 09:32:27 -0700

    Fixed block count for debug build. This resolves OOM events for debug build. Correct overstepping array when freeing.

--- libraries/esp8266/examples/HeapMetric/HeapMetric.ino
+++ libraries/esp8266/examples/HeapMetric/HeapMetric.ino
@@ -3,6 +3,12 @@
 // released to public domain
 
 #include <ESP8266WiFi.h>
+#include <umm_malloc/umm_malloc.h>
+
+// A macro to handle printing w/o using the heap.
+#ifndef ETS_PRINTF
+#define ETS_PRINTF(a, ...) ets_uart_printf(a, ##__VA_ARGS__)
+#endif
 
 void stats(const char* what) {
   // we could use getFreeHeap() getMaxFreeBlockSize() and getHeapFragmentation()
@@ -21,8 +27,21 @@ void tryit(int blocksize) {
   void** p;
   int blocks;
 
+  if (umm_get_oom_count()) {
+    return;
+  }
   // heap-used ~= blocks*sizeof(void*) + blocks*blocksize
-  blocks = ((ESP.getMaxFreeBlockSize() / (blocksize + sizeof(void*))) + 3) & ~3; // rounded up, multiple of 4
+  // This calculation gets deep into how umm_malloc divides up memory and understanding it is not important to this example.
+  // Internally the umm_malloc works with memory in 8 byte increments and aligned to 8 bytes.
+  // The creation of an allocation adds about 4 bytes of overhead plus alignment to the allocation size and more for debug builds.
+  // This complicates the calculation of `blocks` a little.
+  // With Core Version 2.7.2 ESP.getMaxFreeBlockSize() returns a value that has already been adjusted for umm_malloc overhead.
+  // It reports a value that could have been allocated at the time of the call.
+  // The overhead size is different for the debug build.
+  // umm_get_alloc_overhead() provides a value that can be used to adjust calculations when trying to dividing up memory as we are here.
+  blocks = ESP.getMaxFreeBlockSize() / (((blocksize + umm_get_alloc_overhead() + 7) & ~7) + sizeof(void*)); // rounded up, multiple of 8
+  // Note we did not include any adjustment for the overhead of blocks*blocksize.
+  // It was already accounted for in the value returned by ESP.getMaxFreeBlockSize().
 
   Serial.printf("\nFilling memory with blocks of %d bytes each\n", blocksize);
   stats("before");
@@ -30,6 +49,10 @@ void tryit(int blocksize) {
   p = (void**)malloc(sizeof(void*) * blocks);
   for (int i = 0; i < blocks; i++) {
     p[i] = malloc(blocksize);
+    if (umm_get_oom_count()) {
+      ETS_PRINTF("\n\nOOM at block index %d of possible %d.\n\n",  i, blocks);
+      return;
+    }
   }
   stats("array and blocks allocation");
 
@@ -41,7 +64,7 @@ void tryit(int blocksize) {
   }
   stats("freeing every other blocks");
 
-  for (int i = 0; i < blocks; i += 4) {
+  for (int i = 0; i < (blocks - 1); i += 4) {
     if (p[i + 1]) {
       free(p[i + 1]);
     }
