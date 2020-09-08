2933852c6c8b2267607a83be9644fb8f836e9f22
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2020-09-08 16:40:39 +0200

    Check also if IP is not IPADDR_NONE

--- cores/esp8266/IPAddress.cpp
+++ cores/esp8266/IPAddress.cpp
@@ -32,7 +32,7 @@ IPAddress::IPAddress() {
 }
 
 bool IPAddress::isSet () const {
-    return !ip_addr_isany(&_ip);
+    return !ip_addr_isany(&_ip) && ((*this) != IPADDR_NONE);
 }
 
 IPAddress::IPAddress(uint8_t first_octet, uint8_t second_octet, uint8_t third_octet, uint8_t fourth_octet) {
