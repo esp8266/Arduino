15dc740fe1c3c9bfbfe599ae6e0edbc87dae67df
hreintke <hreintke@tauri.nl>
2020-10-15 00:02:39 +0200

    Move strlcat & strlcpy to __cplusplus

--- tests/host/common/mock.h
+++ tests/host/common/mock.h
@@ -62,12 +62,14 @@ extern "C" {
 // TODO: #include <stdlib_noniso.h> ?
 char* itoa (int val, char *s, int radix);
 char* ltoa (long val, char *s, int radix);
-#ifdef __cplusplus
-}
-#endif
+
 
 size_t strlcat(char *dst, const char *src, size_t size);
 size_t strlcpy(char *dst, const char *src, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
 
 // exotic typedefs used in the sdk
 
