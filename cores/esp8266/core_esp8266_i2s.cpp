183f2459e61d2cb2b57d9d34506fb9808420eb95
david gauchard <gauchard@laas.fr>
2020-12-08 22:28:55 +0100

    i2s:adds i2s_rxtxdac_begin(rx,tx,dac):
    A dac parameter is added to allow using only i2s-out-data pin.
    This is necessary in no-DAC mode when i2so-bck and i2so-ws are repurposed
    especially because they overlap with SPI GPIO.

--- cores/esp8266/core_esp8266_i2s.cpp
+++ cores/esp8266/core_esp8266_i2s.cpp
@@ -64,6 +64,7 @@ typedef struct i2s_state {
   // Callback function should be defined as 'void ICACHE_RAM_ATTR function_name()',
   // and be placed in IRAM for faster execution. Avoid long computational tasks in this
   // function, use it to set flags and process later.
+  bool             txDAC;
 } i2s_state_t;
 
 // RX = I2S receive (i.e. microphone), TX = I2S transmit (i.e. DAC)
@@ -493,6 +494,10 @@ float i2s_get_real_rate(){
 }
 
 bool i2s_rxtx_begin(bool enableRx, bool enableTx) {
+  return i2s_rxtxdac_begin(enableRx, enableTx, true);
+}
+
+bool i2s_rxtxdac_begin(bool enableRx, bool enableTx, bool txDAC) {
   if (tx || rx) {
     i2s_end(); // Stop and free any ongoing stuff
   }
@@ -503,9 +508,11 @@ bool i2s_rxtx_begin(bool enableRx, bool enableTx) {
       // Nothing to clean up yet
       return false; // OOM Error!
     }
-    pinMode(I2SO_WS, FUNCTION_1);
     pinMode(I2SO_DATA, FUNCTION_1);
-    pinMode(I2SO_BCK, FUNCTION_1);
+    if ((tx->txDAC = txDAC)) {
+      pinMode(I2SO_WS, FUNCTION_1);
+      pinMode(I2SO_BCK, FUNCTION_1);
+    }
   }
   if (enableRx) {
     rx = (i2s_state_t*)calloc(1, sizeof(*rx));
@@ -579,8 +586,10 @@ void i2s_end() {
 
   if (tx) {
     pinMode(I2SO_DATA, INPUT);
-    pinMode(I2SO_BCK, INPUT);
-    pinMode(I2SO_WS, INPUT);
+    if (tx->txDAC) {
+      pinMode(I2SO_BCK, INPUT);
+      pinMode(I2SO_WS, INPUT);
+    }
     free(tx);
     tx = NULL;
   }
--- cores/esp8266/i2s.h
+++ cores/esp8266/i2s.h
@@ -21,6 +21,8 @@
 #ifndef I2S_h
 #define I2S_h
 
+#define I2S_HAS_BEGIN_RXTXDAC 1
+
 /*
 How does this work? Basically, to get sound, you need to:
 - Connect an I2S codec to the I2S pins on the ESP.
@@ -41,6 +43,7 @@ extern "C" {
 #endif
 
 void i2s_begin(); // Enable TX only, for compatibility
+bool i2s_rxtxdac_begin(bool enableRx, bool enableTx, bool txDAC); // Allow TX and/or RX, returns false on OOM error
 bool i2s_rxtx_begin(bool enableRx, bool enableTx); // Allow TX and/or RX, returns false on OOM error
 void i2s_end();
 void i2s_set_rate(uint32_t rate);//Sample Rate in Hz (ex 44100, 48000)
