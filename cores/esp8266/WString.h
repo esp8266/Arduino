6dbcfa9cc35957fb550410926d4bb1afa8f2bf25
Maxim Prokhorov <prokhorov.max@outlook.com>
2020-12-23 02:01:08 +0300

    trying to fix arduinojson
    based on the implementation, we only need to specify that this symbol is a class

--- cores/esp8266/WString.h
+++ cores/esp8266/WString.h
@@ -39,6 +39,10 @@ class __FlashStringHelper;
 #define FPSTR(pstr_pointer) (reinterpret_cast<const __FlashStringHelper *>(pstr_pointer))
 #define F(string_literal) (FPSTR(PSTR(string_literal)))
 
+// support libraries that expect this name to be available
+// replace with `using StringSumHelper = String;` in case something wants this constructible
+class StringSumHelper;
+
 // The string class
 class String {
         // use a function pointer to allow for "if (s)" without the
