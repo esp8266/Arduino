abca00b2bdfeebc0df98e094c40430d18f01c128
david gauchard <gauchard@laas.fr>
2021-01-29 22:26:29 +0100

    fix licences

--- cores/esp8266/StreamDev.h
+++ cores/esp8266/StreamDev.h
@@ -15,8 +15,6 @@
     You should have received a copy of the GNU Lesser General Public
     License along with this library; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-    parsing functions based on TextFinder library by Michael Margolis
 */
 
 #ifndef __STREAMDEV_H
