c7efc3545034e6345bfe5afe5a002dae1910e3d4
Timo Wischer <twischer@freenet.de>
2021-01-31 19:21:40 +0100

    Support to reboot into UART download mode
    without any external wiring.
    
    This patch introduces the new method
    Esp.rebootIntoUartDownloadMode()
    
    When the user calls this method the ESP8266 reboots into the UART
    download mode. In this mode the user can use esptool.py to flash a new
    firmware file. The following command was used to test it:
    $ esptool.py --before no_reset --after soft_reset --chip esp8266 \
        --port /dev/ttyUSB0 --baud 460800 write_flash 0x0 firmware.bin
    
    The implementation is based on the original implementation in the
    boot ROM. Some parts of the original implementation can be found in
    [1]. This patch is a squashed and simplified version of [2]. The non
    squashed version might be helpful in case of debugging issues.
    
    [1] https://github.com/twischer/xtensa-subjects/blob/master/reversed/bootrom.c
    [2] https://github.com/twischer/Arduino/tree/reboot_uart_download_full
    
    Signed-off-by: Timo Wischer <twischer@freenet.de>

---
+++ cores/esp8266/reboot_uart_dwnld.h
@@ -0,0 +1,23 @@
+/*
+ ESP8266-specific implementation of the UART download mode
+ Copyright (c) 2021 Timo Wischer <twischer@freenet.de>
+ All rights reserved.
+ This file is part of the esp8266 core for Arduino environment.
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+#include <stdnoreturn.h>
+
+[[noreturn]] void esp8266RebootIntoUartDownloadMode();
