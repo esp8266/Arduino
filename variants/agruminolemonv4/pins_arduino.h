48527fc25e52e31f4d37278b42259b389c68ff7a
david gauchard <gauchard@laas.fr>
2021-02-17 12:54:21 +0100

    update variant

--- variants/agruminolemonv4/pins_arduino.h
+++ variants/agruminolemonv4/pins_arduino.h
@@ -26,25 +26,27 @@
 #ifndef Pins_Arduino_h
 #define Pins_Arduino_h
 
-#define PIN_WIRE_SDA (4)
-#define PIN_WIRE_SCL (5)
-
-static const uint8_t SDA = PIN_WIRE_SDA;
-static const uint8_t SCL = PIN_WIRE_SCL;
-
 #define LED_BUILTIN 2
 
-static const uint8_t D0   = 16;
-static const uint8_t D1   = 5;
-static const uint8_t D2   = 4;
-static const uint8_t D3   = 0;
-static const uint8_t D4   = 2;
-static const uint8_t D5   = 14;
-static const uint8_t D6   = 12;
-static const uint8_t D7   = 13;
-static const uint8_t D8   = 15;
-static const uint8_t RX   = 3;
-static const uint8_t TX   = 1;
+// PINOUT Agrumino Implemented
+#define PIN_SDA 2 // [X] BOOT: Must be HIGH at boot
+#define PIN_SCL 14 // [X]
+#define PIN_PUMP 12 // [X]
+#define PIN_BTN_S1 4 // [X] Same as Internal WT8266 LED
+#define PIN_USB_DETECT 5 // [X]
+#define PIN_MOSFET 15 // [X] BOOT: Must be LOW at boot
+#define PIN_BATT_STAT 13 // [X]
+#define PIN_LEVEL 0 // [ ] BOOT: HIGH for Running and LOW for Program
+
+static constexpr uint8_t D0   = 16;
+static constexpr uint8_t RX   = 3;
+static constexpr uint8_t TX   = 1;
+
+#define PIN_WIRE_SDA PIN_SDA
+#define PIN_WIRE_SCL PIN_SCL
+
+static constexpr uint8_t SDA = PIN_WIRE_SDA;
+static constexpr uint8_t SCL = PIN_WIRE_SCL;
 
 #include "../generic/common.h"
 
