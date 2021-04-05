c9b3efb19d65e4ef7d822c85ab581b7859cb4dd1
david gauchard <gauchard@laas.fr>
2021-04-05 14:52:26 +0200

    remove `const`, add comment

--- libraries/ESP8266WiFi/src/include/ClientContext.h
+++ libraries/ESP8266WiFi/src/include/ClientContext.h
@@ -129,8 +129,11 @@ public:
         }
     }
 
-    int connect(const ip_addr_t* addr, uint16_t port)
+    int connect(ip_addr_t* addr, uint16_t port)
     {
+        // note: not using `const ip_addr_t* addr` because
+        // - `ip6_addr_assign_zone()` below modifies `*addr`
+        // - caller's parameter `WiFiClient::connect` is a local copy
 #if LWIP_IPV6
         // Set zone so that link local addresses use the default interface
         if (IP_IS_V6(addr) && ip6_addr_lacks_zone(ip_2_ip6(addr), IP6_UNKNOWN)) {
