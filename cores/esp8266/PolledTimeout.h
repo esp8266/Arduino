8a9d25bd78fd6de7b06cb8e673cb66adb19ee889
Dirk O. Kaar <dok@dok-net.net>
2021-04-05 11:45:59 +0200

    Somewhat easier on the human reader.

--- cores/esp8266/PolledTimeout.h
+++ cores/esp8266/PolledTimeout.h
@@ -175,7 +175,7 @@ public:
 
   bool canWait () const
   {
-    return _timeout != 0 && !_oneShotExpired;
+    return !(_timeout == 0 || _oneShotExpired);
   }
 
   // Resets, will trigger after this new timeout.
