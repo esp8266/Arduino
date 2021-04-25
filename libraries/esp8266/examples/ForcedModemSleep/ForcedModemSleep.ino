c0326f0dd2d25cb526cc101ec5f4307000fda701
Dirk O. Kaar <dok@dok-net.net>
2021-04-25 12:45:46 +0200

    Copyright notices and clarifying introductory comments in example code.

--- libraries/esp8266/examples/ForcedModemSleep/ForcedModemSleep.ino
+++ libraries/esp8266/examples/ForcedModemSleep/ForcedModemSleep.ino
@@ -1,3 +1,24 @@
+/*
+  ESP8266 forced modem sleep mode example
+
+  Copyright (c) 2021 Dirk O. Kaar. All rights reserved.
+  This file is part of the esp8266 core for Arduino environment.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #include <Schedule.h>
 #include <PolledTimeout.h>
 
