c1147e100b422ea0cb21587442749d258a7d4085
david gauchard <gauchard@laas.fr>
2021-06-02 23:18:23 +0200

    makeword: sync header and implementation

--- cores/esp8266/WMath.cpp
+++ cores/esp8266/WMath.cpp
@@ -77,10 +77,10 @@ long map(long x, long in_min, long in_max, long out_min, long out_max) {
     return (delta * dividend + (divisor / 2)) / divisor + out_min;
 }
 
-unsigned int makeWord(unsigned int w) {
+uint16_t makeWord(uint16_t w) {
     return w;
 }
 
-unsigned int makeWord(unsigned char h, unsigned char l) {
+uint16_t makeWord(byte h, byte l) {
     return (h << 8) | l;
 }
