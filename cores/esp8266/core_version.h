39c102c0255ce9b39e898bd8ff89601f32ce3510
sascha <sascha_lammers@gmx.de>
2021-06-17 23:31:23 -0700

    typo

--- cores/esp8266/core_version.h
+++ cores/esp8266/core_version.h
@@ -18,7 +18,7 @@
 #define ARDUINO_ESP8266_REVISION  0
 #endif
 
-#define ARDUINO_ESP8266_RELEASE_EX "Extedend Version"
+#define ARDUINO_ESP8266_RELEASE_EX "Extendend Version"
 
 // ARDUINO_ESP8266_RELEASE is defined for released versions as a string containing the version name, i.e. "2_3_0_RC1"
 // ARDUINO_ESP8266_RELEASE is used in the core internally. Please use ESP.getCoreVersion() function instead.
