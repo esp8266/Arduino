d03a2ecde0e1c0e439d2c785f3ef9d5c5bb83496
Jonas Scharpf <jonas@brainelectronics.de>
2021-06-21 10:30:46 +0200

    modify build opt time only if required and file available

--- tools/prebuild.py
+++ tools/prebuild.py
@@ -17,7 +17,6 @@ import sys
 def create_sketch_dir(build_path):
     """
     Create the sketch directory
-
     :param      build_path:  The path to the build directory
     :type       build_path:  str
     """
@@ -30,11 +29,9 @@ def create_sketch_dir(build_path):
 def create_build_options_file(source_path, build_path, build_opt_name="build_opt.h"):
     """
     Create the build options file in the build directory.
-
     The modification time of the build options file is set to the sketch
     modification time in case the file did not exist or the users build options
     modification time to reflect changes which require a recompilation
-
     :param      source_path:     The path to the source directory
     :type       source_path:     str
     :param      build_path:      The path to the build directory
@@ -45,32 +42,49 @@ def create_build_options_file(source_path, build_path, build_opt_name="build_opt
     build_opt_source_path = os.path.join(source_path, build_opt_name)
     build_opt_build_path = os.path.join(build_path, "sketch", build_opt_name)
 
-    if not os.path.exists(build_opt_source_path):
-        # create empty file
+    # check for an existing build options file in the source directory
+    if os.path.exists(build_opt_source_path):
+        # user does have/use an build options file in it's sketch directory
+
+        if os.path.exists(build_opt_build_path):
+            # set build options file modification time to the same time as the
+            # sketch modification time to avoid rebuilding libraries
+            set_file_time_to_sketch_time(source_path, build_opt_build_path)
+        else:
+            # the build options file does not yet exist in the build directory
+            # just continue as it will be copied by the IDE automatically on
+            # time. This section is only entered on the very first run
+            pass
+    else:
+        # user does not have a build options file, create empty one to avoid
+        # compilation error due to missing file for compilation
         open(build_opt_build_path, 'a').close()
 
-        # set build_opt.h file modification time to the same time as the sketch
-        # modification time to avoid rebuilding libraries
-        sketch_path = get_full_sketch_path(source_path)
-        sketch_mtime = get_modification_time(sketch_path)
-        build_opt_ctime = get_creation_time(build_opt_build_path)
-        os.utime(build_opt_build_path, (build_opt_ctime, sketch_mtime))
-    else:
-        # set build option file modification time to the same time as the users
-        # build option file modification time to reflect changes which require
-        # a recompilation
-        build_opt_ctime = get_creation_time(build_opt_build_path)
-        build_opt_mtime = get_modification_time(build_opt_source_path)
-        os.utime(build_opt_build_path, (build_opt_ctime, build_opt_mtime))
+        # set build options file modification time to the same time as the
+        # sketch modification time to avoid rebuilding libraries
+        set_file_time_to_sketch_time(source_path, build_opt_build_path)
+
+
+def set_file_time_to_sketch_time(source_path, destination_path):
+    """
+    Set the file ctime and mtime to the ctime and mtime of the sketch.
+    :param      source_path:       The source path to the sketch
+    :type       source_path:       str
+    :param      destination_path:  The destination path to the file to modify
+    :type       destination_path:  str
+    """
+    sketch_path = get_full_sketch_path(source_path)
+    sketch_mtime = get_modification_time(sketch_path)
+    destination_ctime = get_creation_time(destination_path)
+
+    os.utime(destination_path, (destination_ctime, sketch_mtime))
 
 
 def get_full_sketch_path(source_path):
     """
     Get the full sketch path.
-
     :param      source_path:  The source path
     :type       source_path:  str
-
     :returns:   The full sketch path including the '.ino' extension.
     :rtype:     str
     """
@@ -84,24 +98,19 @@ def get_full_sketch_path(source_path):
 def get_modification_time(file_path):
     """
     Get the modification time of a file.
-
     :param      file_path:  The file path
     :type       file_path:  str
-
     :returns:   The modification time.
     :rtype:     float
     """
-
     return os.path.getmtime(file_path)
 
 
 def get_creation_time(file_path):
     """
     Gets the creation time if a file.
-
     :param      file_path:  The file path
     :type       file_path:  str
-
     :returns:   The creation time.
     :rtype:     float
     """
