9571f54e70ab51c9bc9b609944298146574c8ec8
Paulo <paulosanz@poli.ufrj.br>
2021-06-26 22:33:50 -0300

    Move location of stack variable

--- libraries/ESP8266HTTPClient/src/ESP8266HTTPClient.cpp
+++ libraries/ESP8266HTTPClient/src/ESP8266HTTPClient.cpp
@@ -847,12 +847,12 @@ bool HTTPClient::hasHeader(const char* name)
  */
 bool HTTPClient::connect(void)
 {
-    #if defined(NO_GLOBAL_INSTANCES)
-    StreamNull devnull;
-    #endif
-
     if(_reuse && _canReuse && connected()) {
         DEBUG_HTTPCLIENT("[HTTP-Client] connect: already connected, reusing connection\n");
+
+        #if defined(NO_GLOBAL_INSTANCES)
+        StreamNull devnull;
+        #endif
         _client->sendAvailable(devnull); // clear _client's output (all of it, no timeout)
         return true;
     }
