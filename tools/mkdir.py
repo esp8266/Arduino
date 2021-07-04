73eb2c81aeb8a3026a5a7084777adb94e585c8ae
gneiss15 <gneiss@web.de>
2021-07-04 12:20:45 +0200

    Make makdir.py work unter python3 versions below 3.5 Because early versions of python3 did not have the optional arg "exist_ok" for "pathlib.Path(...).mkdir(...)" a build under this versions will abort with an error message. This PR will modify the python script so that it works even under python 3.4 (and below).

--- tools/mkdir.py
+++ tools/mkdir.py
@@ -6,14 +6,33 @@ import argparse
 import pathlib
 import sys
 
+def IsPythonVersionBelow3_5():
+  v = sys.version_info
+  if v[ 0 ] > 3 or ( v[ 0 ] == 3 and v[ 1 ] > 4 ):
+    return False
+  return True
+
+def mkdir_3_4( p, parents_ ):
+  try:
+    pathlib.Path( p ).mkdir( parents = parents_ )
+  except FileExistsError:
+    pass
+
+def mkdir_3_5( p, parents_ ):
+  pathlib.Path( p ).mkdir( parents = parents_, exist_ok = True )
+
 def main():
-    parser = argparse.ArgumentParser(description='Platform-independent `mkdir`')
-    parser.add_argument('-p', '--parents', action='store_true', required=False, help='no error if existing, make parent directories as needed')
-    parser.add_argument('dir', action='store', nargs='+')
-    ns = parser.parse_args()
-    for p in ns.dir:
-        pathlib.Path(p).mkdir(parents=ns.parents, exist_ok=True)
-    return 0
+  parser = argparse.ArgumentParser(description='Platform-independent `mkdir`')
+  parser.add_argument('-p', '--parents', action='store_true', required=False, help='no error if existing, make parent directories as needed')
+  parser.add_argument('dir', action='store', nargs='+')
+  ns = parser.parse_args()
+  if IsPythonVersionBelow3_5():
+    mkdir_ = mkdir_3_4
+  else:
+    mkdir_ = mkdir_3_5
+  for p in ns.dir:
+    mkdir_( p, ns.parents )
+  return 0
 
 if __name__ == '__main__':
-    sys.exit(main())
+  sys.exit(main())
