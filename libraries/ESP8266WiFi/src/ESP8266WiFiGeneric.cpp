a69d4d60aaf3234e1c81e5066bb259f0f1b992f3
david gauchard <gauchard@laas.fr>
2021-09-23 14:32:30 +0200

    further simplification

--- libraries/ESP8266WiFi/src/ESP8266WiFiGeneric.cpp
+++ libraries/ESP8266WiFi/src/ESP8266WiFiGeneric.cpp
@@ -622,7 +622,7 @@ int ESP8266WiFiGenericClass::hostByName(const char* aHostname, IPAddress& aResul
         aResult = IPAddress(&addr);
     } else if(err == ERR_INPROGRESS) {
         _dns_lookup_pending = true;
-        if (yieldUntil([pIp = &aResult](){ return pIp->isSet(); }, timeout_ms))
+        if (yieldUntil([&](){ return aResult.isSet(); }, timeout_ms))
             err = ERR_OK;
         _dns_lookup_pending = false;
         // will return here when dns_found_callback fires (or timeout)
@@ -669,7 +669,7 @@ int ESP8266WiFiGenericClass::hostByName(const char* aHostname, IPAddress& aResul
         aResult = IPAddress(&addr);
     } else if(err == ERR_INPROGRESS) {
         _dns_lookup_pending = true;
-        if (yieldUntil([pIp = &aResult](){ return pIp->isSet(); }, timeout_ms))
+        if (yieldUntil([&](){ return aResult.isSet(); }, timeout_ms))
             err = ERR_OK;
         // will resume on timeout or when wifi_dns_found_callback fires
         _dns_lookup_pending = false;
