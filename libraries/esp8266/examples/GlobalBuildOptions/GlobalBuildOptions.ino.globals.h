b6cc66dfd142b38ddd6d5aa4bb48c10791a08eee
M Hightower <27247790+mhightower83@users.noreply.github.com>
2022-03-30 11:49:23 -0700

    Update CI for build option and global support
    Added "mkbuildoptglobals.extra_flags=--cache_core" to platform.loca.txt
    Update "-ide-version=10802" this version number indicates aggressive caching support
    Added example to test global .h support

---
+++ libraries/esp8266/examples/GlobalBuildOptions/GlobalBuildOptions.ino.globals.h
@@ -0,0 +1,39 @@
+/*@create-file:build.opt@
+  // An embedded build.opt file using a "C" block comment. The starting signature
+  // must be on a line by itself. The closing block comment pattern should be on a
+  // line by itself. Each line within the block comment will be space trimmed and
+  // written to build.opt, skipping blank lines and lines starting with '//', '*'
+  // or '#'.
+  -DMYTITLE1="\"Running on \""
+ * this line is ignored
+  *@create-file:build.opt@
+  # this line is ignored
+   -O3
+//  -fanalyzer
+  -DUMM_STATS_FULL=1
+*/
+
+#ifndef GLOBALBUILDOPTIONS_INO_GLOBALS_H
+#define GLOBALBUILDOPTIONS_INO_GLOBALS_H
+
+#if !defined(__ASSEMBLER__)
+// Defines kept away from assembler modules
+// i.e. Defines for .cpp, .ino, .c ... modules
+#endif
+
+#if defined(__cplusplus)
+// Defines kept private to .cpp and .ino modules
+//#pragma message("__cplusplus has been seen")
+#define MYTITLE2 "Empty"
+#endif
+
+#if !defined(__cplusplus) && !defined(__ASSEMBLER__)
+// Defines kept private to .c modules
+#define MYTITLE2 "~Full"
+#endif
+
+#if defined(__ASSEMBLER__)
+// Defines kept private to assembler modules
+#endif
+
+#endif
