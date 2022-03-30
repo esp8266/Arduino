a192705533147aa6a2f07597a645b5c14b83154e
Maxim Prokhorov <prokhorov.max@outlook.com>
2022-03-31 01:29:09 +0300

    cannot assign to uninitialized obj, try to placement-new replace the ctor

--- cores/esp8266/WString.cpp
+++ cores/esp8266/WString.cpp
@@ -202,48 +202,50 @@ String::String(String &&rval) noexcept {
     move(rval);
 }
 
+// placement new is safe enough, but be wary when `this` has something in the hierarchy
+
 String::String(unsigned char value, unsigned char base) {
-    *this = toString(value, base);
+    new (this) String(toString(value, base));
 }
 
 String::String(int value, unsigned char base) {
-    *this = toString(value, base);
+    new (this) String(toString(value, base));
 }
 
 String::String(unsigned int value, unsigned char base) {
-    *this = toString(value, base);
+    new (this) String(toString(value, base));
 }
 
 String::String(long value, unsigned char base) {
-    *this = toString(value, base);
+    new (this) String(toString(value, base));
 }
 
 String::String(unsigned long value, unsigned char base) {
-    *this = toString(value, base);
+    new (this) String(toString(value, base));
 }
 
 String::String(long long value, unsigned char base) {
-    *this = toString(value, base);
+    new (this) String(toString(value, base));
 }
 
 String::String(long long value) {
-    *this = toString(value);
+    new (this) String(toString(value));
 }
 
 String::String(unsigned long long value, unsigned char base) {
-    *this = toString(value, base);
+    new (this) String(toString(value, base));
 }
 
 String::String(unsigned long long value) {
-    *this = toString(value);
+    new (this) String(toString(value));
 }
 
 String::String(float value, unsigned char decimalPlaces) {
-    *this = toString(value, decimalPlaces);
+    new (this) String(toString(value, decimalPlaces));
 }
 
 String::String(double value, unsigned char decimalPlaces) {
-    *this = toString(value, decimalPlaces);
+    new (this) String(toString(value, decimalPlaces));
 }
 
 /*********************************************/
