f149d7b70e9190468d82da86e361346e983d9d95
M Hightower <27247790+mhightower83@users.noreply.github.com>
2022-04-30 08:17:13 -0700

    Correction to run_CI_locall.sh (#8552)
    `tests/ci/check_restyle.sh` does not exist; however, `tests/ci/style_check.sh` does.
    Changed run_CI_locally.sh to run style_check.sh from ci directory.

--- tests/run_CI_locally.sh
+++ tests/run_CI_locally.sh
@@ -115,7 +115,7 @@ elif [ "$BUILD_TYPE" = host ]; then
     tests/ci/host_test.sh
 
 elif [ "$BUILD_TYPE" = style ]; then
-    tests/ci/check_restyle.sh
+    tests/ci/style_check.sh
     tests/restyle.sh
 
 else
