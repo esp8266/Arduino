ac63f6153e4264628c8d3e39251fbb47b91277f8
david gauchard <gauchard@laas.fr>
2022-07-09 12:34:28 +0200

    add missing license headers

--- cores/esp8266/LwipIntf.h
+++ cores/esp8266/LwipIntf.h
@@ -1,3 +1,25 @@
+/*
+  LwipIntf.h
+
+  Arduino interface for lwIP generic callbacks and functions
+
+  Original Copyright (c) 2020 esp8266 Arduino All rights reserved.
+  This file is part of the esp8266 Arduino core environment.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 #ifndef _LWIPINTF_H
 #define _LWIPINTF_H
@@ -28,16 +50,21 @@ public:
                                  IPAddress& netmask, IPAddress& dns1);
 
     String hostname();
+
     bool   hostname(const String& aHostname)
     {
         return hostname(aHostname.c_str());
     }
+
     bool hostname(const char* aHostname);
+
     // ESP32 API compatibility
     bool setHostname(const char* aHostName)
     {
         return hostname(aHostName);
     }
+
+    // ESP32 API compatibility
     const char* getHostname();
 
 protected:
