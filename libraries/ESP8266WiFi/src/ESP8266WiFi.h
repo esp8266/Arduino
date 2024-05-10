d29ee2d1ee0dd75a4aa87592b06c1fa34c50acb9
SanjanaMops <147549505+SanjanaMops@users.noreply.github.com>
2024-05-10 19:18:29 +0530

    Update ESP8266WiFi.h

--- libraries/ESP8266WiFi/src/ESP8266WiFi.h
+++ libraries/ESP8266WiFi/src/ESP8266WiFi.h
@@ -6,6 +6,7 @@
 
  This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
+ 
  License as published by the Free Software Foundation; either
  version 2.1 of the License, or (at your option) any later version.
 
