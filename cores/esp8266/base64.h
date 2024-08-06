e221e87478445634834ec51f32913d3d85d90d0b
Benjamin Cance <49796265+rowingdude@users.noreply.github.com>
2024-08-06 09:53:31 -0400

    Update base64.h
    a. Header files should not mandate whether to inline or not, that's the compiler's job.
    
    b. Setting compatible defaults ensures we can reduce code size and maintain functionality.
    
    c. Removed comments that should be in documentation.

--- cores/esp8266/base64.h
+++ cores/esp8266/base64.h
@@ -26,32 +26,16 @@
 #define CORE_BASE64_H_
 
 #include <WString.h>
+#include <cstdint>
 
-class base64
-{
+class Base64 {
 public:
-    // NOTE: The default behaviour of backend (lib64)
-    // is to add a newline every 72 (encoded) characters output.
-    // This may 'break' longer uris and json variables
-    static String encode(const uint8_t * data, size_t length, bool doNewLines);
-    static inline String encode(const String& text, bool doNewLines)
-    {
-        return encode( (const uint8_t *) text.c_str(), text.length(), doNewLines );
-    }
 
-    // esp32 compat:
+    static String encode(const uint8_t* data, size_t length, bool doNewLines = false);
+    static String encode(const String& text, bool doNewLines = false);
 
-    static inline String encode(const uint8_t * data, size_t length)
-    {
-        return encode(data, length, false);
-    }
-
-    static inline String encode(const String& text)
-    {
-        return encode(text, false);
-    }
 private:
+    Base64() = delete;  // Prevent instantiation
 };
-
 
 #endif /* CORE_BASE64_H_ */
