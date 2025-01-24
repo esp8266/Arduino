ea26398c9fed677a0a5013b24964a21e14bcf2a5
Maxim Prokhorov <prokhorov.max@outlook.com>
2025-01-24 06:03:01 +0300

    Pin clang-format in CI recipe not the script itself

--- tests/restyle.sh
+++ tests/restyle.sh
@@ -8,8 +8,7 @@ test -d ${root}/cores/esp8266
 test -d ${root}/libraries
 
 # allow `env CLANG_FORMAT=clang-format-N`, or some other version
-# default to v15, latest stable version from ubuntu-latest Github Actions image
-CLANG_FORMAT=${CLANG_FORMAT:-clang-format-15}
+CLANG_FORMAT=${CLANG_FORMAT:-clang-format}
 
 cd $root
 python $root/tests/restyle.py format --clang-format=$CLANG_FORMAT preset --include core --include arduino
