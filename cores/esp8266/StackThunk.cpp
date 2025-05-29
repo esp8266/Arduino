b43c4fb1e316d69a54fc2bb63938c4487c5a0db2
Maxim Prokhorov <prokhorov.max@outlook.com>
2025-05-29 03:44:18 +0300

    skip invalid a1 load, but not yielding
    correctly cherry-picked mcspr/esp8266-Arduino#653ee92eb83e48868960ca0f6c44966dfaa5937d

--- cores/esp8266/StackThunk.cpp
+++ cores/esp8266/StackThunk.cpp
@@ -179,17 +179,16 @@ asm(
     "movi         a2, stack_thunk_save\n\t"
 /* But, only when inside of bssl stack (saved a1 != 0) */
     "l32i.n       a3, a2, 0\n\t"
-    "beqz         a3, stack_thunk_yield_restore\n\t"
+    "beqz         a3, stack_thunk_yield_do_yield\n\t"
     "l32i.n       a1, a2, 0\n\t"
 /* optimistic_yield(10000) without extra l32r */
+"stack_thunk_yield_do_yield:\n\t"
     "movi         a2, 0x10\n\t"
     "addmi        a2, a2, 0x2700\n\t"
     "call0        optimistic_yield\n\t"
 /* Swap bearssl <-> cont stacks, again */
-"stack_thunk_yield_restore:\n\t"
     "movi         a2, stack_thunk_yield_save\n\t"
     "l32i.n       a1, a2, 0\n\t"
-    "\n"
 /* Restore caller */
     "l32i.n       a0, a1, 12\n\t"
     "addi         a1, a1, 16\n\t"
